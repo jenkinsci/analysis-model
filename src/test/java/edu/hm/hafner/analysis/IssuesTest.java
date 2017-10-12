54d5428293983d7586426bce4d73e69a616a5af4
Tom Maier <maier14@hm.edu>
2017-10-12 16:19:48 +0200

    add test class for issues

---
+++ src/test/java/edu/hm/hafner/analysis/IssuesTest.java
@@ -0,0 +1,15 @@
+package edu.hm.hafner.analysis;
+
+import static org.junit.jupiter.api.Assertions.*;
+/*
+ * Hochschule Muenchen Fakultaet 07 Informatik und Mathematik
+ * Praktikum Softwareentwicklung II, IF2B, SS2016
+ * PROJEKTNAME
+ *
+ * Software: Mac OS X 10.11, oracle Java SE 1.8.0_25
+ * System: Intel Core i5,8 GByte RAM
+ */
+
+class IssuesTest {
+
+}
