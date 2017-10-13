3b984768118104a1c7a820a19ef07c8f1c89d35e
Tom <weissbier.weissbier@googlemail.com>
2017-10-13 11:17:57 +0200

    added custom assertion for Issues and implemented to the soft assertions
    @Author Johannes Arzt
    @Author Tom Maier

--- src/test/java/edu/hm/hafner/analysis/IssuesAssert.java
+++ src/test/java/edu/hm/hafner/analysis/IssuesAssert.java
@@ -1,5 +1,7 @@
 package edu.hm.hafner.analysis;
 
+import java.util.SortedSet;
+
 import org.assertj.core.api.AbstractAssert;
 
 public class IssuesAssert extends AbstractAssert<IssuesAssert, Issues> {
@@ -8,5 +10,77 @@ public class IssuesAssert extends AbstractAssert<IssuesAssert, Issues> {
         super(issues, selfType);
     }
 
+    public IssuesAssert hasSize(int size) {
+        isNotNull();
+
+        if (actual.getSize() != size) {
+            failWithMessage("Expected size to be <%s> but was <%s>", size, actual.getSize());
+        }
+        return this;
+    }
+
+    public IssuesAssert hasHighPrioritySize(int highPrioritySize) {
+        isNotNull();
+
+        if (actual.getHighPrioritySize() != highPrioritySize) {
+            failWithMessage("Expected highPrioritySize to be <%s> but was <%s>", highPrioritySize, actual.getHighPrioritySize());
+        }
+        return this;
+    }
+
+    public IssuesAssert hasNormalPrioritySize(int normalPrioritySize) {
+        isNotNull();
+
+        if (actual.getNormalPrioritySize() != normalPrioritySize) {
+            failWithMessage("Expected normalPrioritySize to be <%s> but was <%s>", normalPrioritySize, actual.getNormalPrioritySize());
+        }
+        return this;
+    }
+
+    public IssuesAssert hasLowPrioritySize(int lowPrioritySize) {
+        isNotNull();
+
+        if (actual.getLowPrioritySize() != lowPrioritySize) {
+            failWithMessage("Expected lowPrioritySize to be <%s> but was <%s>", lowPrioritySize, actual.getLowPrioritySize());
+        }
+        return this;
+    }
+
+    public IssuesAssert hasNumberOfFiles(int numberOfFiles) {
+        isNotNull();
+
+        if (actual.getNumberOfFiles() != numberOfFiles) {
+            failWithMessage("Expected numberOfFiles to be <%s> but was <%s>", numberOfFiles, actual.getNumberOfFiles());
+        }
+        return this;
+    }
+
+    public IssuesAssert hasIssueonIndex(Issue issue, int index) {
+        isNotNull();
+
+        if (actual.get(index) != issue) {
+            failWithMessage("Expected issue to be <%s> on the index %s but was <%s>", issue, index, actual.get(index));
+        }
+        return this;
+    }
+
+    public IssuesAssert hasFiles(SortedSet<String> files) {
+        isNotNull();
+
+        if (actual.getFiles().equals(files)) {
+            failWithMessage("Expected sets are not the same");
+        }
+        return this;
+    }
+
+    /*
+    public IssuesAssert hasProperties(<R> SortedSet<R> properties) {
+        isNotNull();
+
+        if (actual.getFiles().equals(files)) {
+            failWithMessage("Expected sets are not the same");
+        }
+        return this;
+    }*/
 
 }
--- src/test/java/edu/hm/hafner/analysis/MySoftAssertions.java
+++ src/test/java/edu/hm/hafner/analysis/MySoftAssertions.java
@@ -2,10 +2,14 @@ package edu.hm.hafner.analysis;
 
 import org.assertj.core.api.SoftAssertions;
 
-public class MySoftAssertions extends SoftAssertions{
+public class MySoftAssertions extends SoftAssertions {
 
     public IssueAssert assertThat(Issue actual) {
         return proxy(IssueAssert.class, Issue.class, actual);
+    }
+
+    public IssuesAssert assertThat(Issues actual) {
+        return proxy(IssuesAssert.class, Issues.class, actual);
     }
 
 }
