d729307bf3d67a055398fc6f7b02d849ac7168f1
Tom <weissbier.weissbier@googlemail.com>
2017-10-13 10:45:22 +0200

    added IssuesAssert class

---
+++ src/test/java/edu/hm/hafner/analysis/IssuesAssert.java
@@ -0,0 +1,12 @@
+package edu.hm.hafner.analysis;
+
+import org.assertj.core.api.AbstractAssert;
+
+public class IssuesAssert extends AbstractAssert<IssuesAssert, Issues> {
+
+    public IssuesAssert(final Issues issues, final Class<?> selfType) {
+        super(issues, selfType);
+    }
+
+
+}
