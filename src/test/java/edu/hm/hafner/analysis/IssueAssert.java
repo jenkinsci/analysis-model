42d29d5d7bff4e2ffb9b49e01fc2d43f79336e62
Tom Maier <maier14@hm.edu>
2017-10-13 10:26:13 +0200

    added test methods - getType - getMessage - getFingerprint - getLineEnd - getColumnEnd - getPriority (equals enum values, not sure if its the right way)

--- src/test/java/edu/hm/hafner/analysis/IssueAssert.java
+++ src/test/java/edu/hm/hafner/analysis/IssueAssert.java
@@ -4,21 +4,21 @@ import java.util.Objects;
 
 import org.assertj.core.api.AbstractAssert;
 
-public class IssueAssert extends AbstractAssert <IssueAssert,Issue>{
+public class IssueAssert extends AbstractAssert<IssueAssert, Issue> {
 
   /*  public IssueAssert(final Issue issue, final Class<?> selfType) {
         super(issue, selfType);
     }*/
 
-    public IssueAssert(Issue actual){
-        super(actual,IssueAssert.class);
+    public IssueAssert(Issue actual) {
+        super(actual, IssueAssert.class);
     }
 
-    public static IssueAssert assertThat(Issue actual){
-        return  new IssueAssert(actual);
+    public static IssueAssert assertThat(Issue actual) {
+        return new IssueAssert(actual);
     }
 
-    public IssueAssert hasFileName(String fileName){
+    public IssueAssert hasFileName(String fileName) {
         isNotNull();
 
         if (!Objects.equals(actual.getFileName(), fileName)) {
@@ -27,7 +27,7 @@ public class IssueAssert extends AbstractAssert <IssueAssert,Issue>{
         return this;
     }
 
-    public IssueAssert hasCategory(String category){
+    public IssueAssert hasCategory(String category) {
         isNotNull();
 
         if (!Objects.equals(actual.getCategory(), category)) {
@@ -36,4 +36,57 @@ public class IssueAssert extends AbstractAssert <IssueAssert,Issue>{
         return this;
     }
 
+    public IssueAssert hasType(String type) {
+        isNotNull();
+
+        if (!Objects.equals(actual.getType(), type)) {
+            failWithMessage("Expected Type to be <%s> but was <%s>", type, actual.getType());
+        }
+        return this;
+    }
+
+    public IssueAssert hasMessage(String message) {
+        isNotNull();
+
+        if (!Objects.equals(actual.getMessage(), message)) {
+            failWithMessage("Expected Message to be <%s> but was <%s>", message, actual.getMessage());
+        }
+        return this;
+    }
+
+    public IssueAssert hasFingerprint(String fingerprint) {
+        isNotNull();
+
+        if (!Objects.equals(actual.getFingerprint(), fingerprint)) {
+            failWithMessage("Expected Fingerprint to be <%s> but was <%s>", fingerprint, actual.getFingerprint());
+        }
+        return this;
+    }
+
+    public IssueAssert hasLineEnd(int lineEnd) {
+        isNotNull();
+
+        if (actual.getLineEnd() != lineEnd) {
+            failWithMessage("Expected LineEnd to be <%s> but was <%s>", lineEnd, actual.getLineEnd());
+        }
+        return this;
+    }
+
+    public IssueAssert hasColumnEnd(int columnEnd) {
+        isNotNull();
+
+        if (actual.getColumnEnd() != columnEnd) {
+            failWithMessage("Expected ColumnEnd to be <%s> but was <%s>", columnEnd, actual.getColumnEnd());
+        }
+        return this;
+    }
+
+    public IssueAssert hasPriority(Priority priority) {
+        isNotNull();
+
+        if (actual.getPriority() != priority) {
+            failWithMessage("Expected Priority to be <%s> but was <%s>", priority, actual.getPriority());
+        }
+        return this;
+    }
 }
