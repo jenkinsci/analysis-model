2d2a1a082914bcc406895775207b14b76275d6f3
Johdeath <johannes.arzt@gmx.net>
2017-10-17 08:41:02 +0200

    add a short sample with soft assertion and assertion builder

--- src/test/java/edu/hm/hafner/analysis/IssuesTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssuesTest.java
@@ -1,5 +1,29 @@
 package edu.hm.hafner.analysis;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
 class IssuesTest {
+
+    @Test
+    void SampleTest(){
+
+        IssueBuilder builder = new IssueBuilder();
+        builder.setMessage("Hello again!");
+        builder.setLineStart(123);
+        Issue testissue = builder.build();
+
+        MySoftAssertions softly = new MySoftAssertions();
+        softly.assertThat(testissue)
+                .hasMessage("Hello again!")
+                .hasLineStart(123);
+
+        softly.assertAll();
+
+
+
+
+
+    }
 
 }
