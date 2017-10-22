65542c01dacfb2e82193c6d6124ad9870f2a67ac
Michael Schmid <m.schmid@hm.edu>
2017-10-22 23:01:57 +0200

    IssuesTest

--- src/test/java/edu/hm/hafner/analysis/IssuesTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssuesTest.java
@@ -1,7 +1,75 @@
 package edu.hm.hafner.analysis;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static edu.hm.hafner.analysis.IssuesAssert.assertThat;
 
 class IssuesTest {
+
+    @Test
+    void addIssueCheckContains() {
+        final Issues sut = new Issues();
+        final Issue issue = new IssueBuilder().build();
+        sut.add(issue);
+        assertThat(sut)
+                .assertContains(issue)
+                .hasSize(1)
+                .hasSizeOfPriorityHigh(0)
+                .hasSizeOfPriorityLow(0)
+                .hasSizeOfPriorityNormal(1)
+                .hasToString("1 issues");
+    }
+
+    @Test
+    void noAddIssueCheckNotContains() {
+        final Issues sut = new Issues();
+        final Issue issue = new IssueBuilder().build();
+        assertThat(sut)
+                .assertDoesNotContain(issue)
+                .hasSize(0)
+                .hasSizeOfPriorityHigh(0)
+                .hasSizeOfPriorityLow(0)
+                .hasSizeOfPriorityNormal(0)
+                .hasToString("0 issues");
+    }
+
+    @Test
+    void addAndRemoveIssue() {
+        final Issues sut = new Issues();
+        final Issue issue = new IssueBuilder().build();
+        sut.add(issue);
+        assertThat(sut)
+                .assertContains(issue)
+                .hasSize(1)
+                .hasSizeOfPriorityHigh(0)
+                .hasSizeOfPriorityLow(0)
+                .hasSizeOfPriorityNormal(1)
+                .hasToString("1 issues");
+        sut.findById(issue.getId());
+        sut.remove(issue.getId());
+        assertThat(sut)
+                .assertDoesNotContain(issue)
+                .hasSize(0)
+                .hasSizeOfPriorityHigh(0)
+                .hasSizeOfPriorityLow(0)
+                .hasSizeOfPriorityNormal(0)
+                .hasToString("0 issues");
+    }
+
+    @Test
+    void removeIssueIssuesDoesNotContain() {
+        final Issues sut = new Issues();
+        final Issue issue = new IssueBuilder().build();
+        assertThat(sut)
+                .assertDoesNotContain(issue)
+                .hasSize(0)
+                .hasSizeOfPriorityHigh(0)
+                .hasSizeOfPriorityLow(0)
+                .hasSizeOfPriorityNormal(0)
+                .hasToString("0 issues");
+        sut.findById(issue.getId());
+        sut.remove(issue.getId());
+    }
 
 }
