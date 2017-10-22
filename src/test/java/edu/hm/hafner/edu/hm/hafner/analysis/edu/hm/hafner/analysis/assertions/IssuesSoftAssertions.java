4aa0e17d4112060f01a7eaf654ec632f661269ed
Raphael Furch <raphael@familie-furch.de>
2017-10-22 16:13:18 +0200

    issues.class is finished

---
+++ src/test/java/edu/hm/hafner/edu/hm/hafner/analysis/edu/hm/hafner/analysis/assertions/IssuesSoftAssertions.java
@@ -0,0 +1,11 @@
+package edu.hm.hafner.edu.hm.hafner.analysis.edu.hm.hafner.analysis.assertions;
+
+import org.assertj.core.api.SoftAssertions;
+
+import edu.hm.hafner.analysis.Issues;
+
+public class IssuesSoftAssertions extends SoftAssertions {
+    public IssuesAssertions assertThat(Issues actual) {
+        return proxy(IssuesAssertions.class, Issues.class, actual);
+    }
+}
