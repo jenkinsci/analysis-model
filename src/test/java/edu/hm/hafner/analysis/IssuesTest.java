3f9857d4f2b51e9a2e5ced371457007ea758496e
Michael Schmid <m.schmid@hm.edu>
2017-10-22 23:19:12 +0200

    Issues Test on Exceptions Issues Fix

--- src/test/java/edu/hm/hafner/analysis/IssuesTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssuesTest.java
@@ -2,7 +2,9 @@ package edu.hm.hafner.analysis;
 
 import org.junit.jupiter.api.Test;
 
+import edu.hm.hafner.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static edu.hm.hafner.analysis.IssuesAssert.assertThat;
 
 class IssuesTest {
@@ -68,8 +70,13 @@ class IssuesTest {
                 .hasSizeOfPriorityLow(0)
                 .hasSizeOfPriorityNormal(0)
                 .hasToString("0 issues");
-        sut.findById(issue.getId());
-        sut.remove(issue.getId());
+
+        assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> sut.findById(issue.getId()))
+                .withMessageContaining(issue.getId().toString());
+
+        assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> sut.remove(issue.getId()))
+                .withMessageContaining(issue.getId().toString());
+
     }
 
 }
