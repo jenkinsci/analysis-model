72e7470f9fc187ebb7347c556c581a8be2190e0e
Michael Schmid <m.schmid@hm.edu>
2017-10-25 00:34:50 +0200

    Issues test all() findByProperty()

--- src/test/java/edu/hm/hafner/analysis/IssuesTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssuesTest.java
@@ -1,9 +1,12 @@
 package edu.hm.hafner.analysis;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableSet;
 
 import edu.hm.hafner.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,10 +15,11 @@ import static edu.hm.hafner.analysis.IssuesAssert.assertThat;
 
 class IssuesTest {
 
+    @SuppressWarnings("JUnitTestMethodWithNoAssertions")
     @Test
     void addIssueCheckContains() {
-        final Issues sut = new Issues();
-        final Issue issue = new IssueBuilder().build();
+        Issues sut = new Issues();
+        Issue issue = new IssueBuilder().build();
         sut.add(issue);
         assertThat(sut)
                 .assertContains(issue)
@@ -26,10 +30,11 @@ class IssuesTest {
                 .hasToString("1 issues");
     }
 
+    @SuppressWarnings("JUnitTestMethodWithNoAssertions")
     @Test
     void noAddIssueCheckNotContains() {
-        final Issues sut = new Issues();
-        final Issue issue = new IssueBuilder().build();
+        Issues sut = new Issues();
+        Issue issue = new IssueBuilder().build();
         assertThat(sut)
                 .assertDoesNotContain(issue)
                 .hasSize(0)
@@ -39,10 +44,11 @@ class IssuesTest {
                 .hasToString("0 issues");
     }
 
+    @SuppressWarnings("JUnitTestMethodWithNoAssertions")
     @Test
     void addAndRemoveIssue() {
-        final Issues sut = new Issues();
-        final Issue issue = new IssueBuilder().build();
+        Issues sut = new Issues();
+        Issue issue = new IssueBuilder().build();
         sut.add(issue);
         assertThat(sut)
                 .assertContains(issue)
@@ -64,8 +70,8 @@ class IssuesTest {
 
     @Test
     void removeIssueIssuesDoesNotContain() {
-        final Issues sut = new Issues();
-        final Issue issue = new IssueBuilder().build();
+        Issues sut = new Issues();
+        Issue issue = new IssueBuilder().build();
         assertThat(sut)
                 .assertDoesNotContain(issue)
                 .hasSize(0)
@@ -82,11 +88,12 @@ class IssuesTest {
 
     }
 
+    @SuppressWarnings("JUnitTestMethodWithNoAssertions")
     @Test
     void addAllTest() {
-        final Issues sut = new Issues();
-        final List<Issue> issueList = new ArrayList<>();
-        final int testObjects = 10;
+        Issues sut = new Issues();
+        List<Issue> issueList = new ArrayList<>();
+        int testObjects = 10;
         for (int index = 0; index < testObjects; index++) {
             issueList.add(new IssueBuilder()
                     .setMessage("Issue " + index)
@@ -120,6 +127,113 @@ class IssuesTest {
         Issue removedIssue = issueList.get(0);
         issueList.remove(0);
         IssueAssert.assertThat(sut.get(0)).isEqualTo(removedIssue);
+
+    }
+
+    @Test
+    void allTest() {
+        Issues sut = new Issues();
+        int testObjects = 10;
+        for (int index = 0; index < testObjects; index++) {
+            sut.add(new IssueBuilder()
+                    .setMessage("Issue " + index)
+                    .setLineStart(index)
+                    .setLineEnd(index * index)
+                    .setDescription("Issue build while testing the addAll method of Issues")
+                    .setPriority(index % 3 == 0 ? Priority.LOW : index % 3 == 1 ? Priority.NORMAL : Priority.HIGH)
+                    .build());
+        }
+
+        IssuesAssert asserter = assertThat(sut)
+                .hasSize(testObjects)
+                .hasSizeOfPriorityLow((testObjects + 2) / 3)
+                .hasSizeOfPriorityNormal((testObjects + 1) / 3)
+                .hasSizeOfPriorityHigh(testObjects / 3);
+
+        // Check if all() delivers all issues
+        ImmutableSet<Issue> setOfIssues = sut.all();
+        for (Issue issue: setOfIssues) {
+            asserter.assertContains(issue);
+        }
+        for (Iterator<Issue> it = sut.iterator(); it.hasNext(); ) {
+            org.assertj.core.api.Assertions.assertThat(setOfIssues).contains(it.next());
+        }
+
+    }
+
+    @Test
+    void copyTest() {
+        Issues sut = new Issues();
+        int testObjects = 10;
+        for (int index = 0; index < testObjects; index++) {
+            sut.add(new IssueBuilder()
+                    .setMessage("Issue " + index)
+                    .setLineStart(index)
+                    .setLineEnd(index * index)
+                    .setDescription("Issue build while testing the addAll method of Issues")
+                    .setPriority(index % 3 == 0 ? Priority.LOW : index % 3 == 1 ? Priority.NORMAL : Priority.HIGH)
+                    .build());
+        }
+
+        IssuesAssert asserter = assertThat(sut)
+                .hasSize(testObjects)
+                .hasSizeOfPriorityLow((testObjects + 2) / 3)
+                .hasSizeOfPriorityNormal((testObjects + 1) / 3)
+                .hasSizeOfPriorityHigh(testObjects / 3);
+
+        Issues copy = sut.copy();
+        ImmutableSet<Issue> sutSet = sut.all();
+        ImmutableSet<Issue> copySet = copy.all();
+        org.assertj.core.api.Assertions.assertThat(sutSet).isEqualTo(copySet).as("The issues object and the copy of it does not contains the equal elements");
+
+        // Check if the issues object and it's copy have the same references to the issue objects
+        for (Iterator<Issue> it = copy.iterator(); it.hasNext(); ) {
+            it.next().setFingerprint("modified copy");
+        }
+
+        for (Iterator<Issue> it = sut.iterator(); it.hasNext(); ) {
+            IssueAssert.assertThat(it.next()).hasFingerprint("modified copy");
+        }
+
+        // Check if the issues object is independed of it's copy
+        copy.remove(copy.get(0).getId());
+        for (Issue issue: sutSet) {
+            asserter.assertContains(issue).as("The issues object and it's copy are not independend");
+        }
+
+    }
+
+    @Test
+    void findByPropertyTest() {
+        Issues sut = new Issues();
+        int testObjects = 10;
+        for (int index = 0; index < testObjects; index++) {
+            sut.add(new IssueBuilder()
+                    .setMessage("Issue " + index)
+                    .setLineStart(index)
+                    .setLineEnd(index * index)
+                    .setDescription("Issue build while testing the addAll method of Issues")
+                    .setPriority(index % 3 == 0 ? Priority.LOW : index % 3 == 1 ? Priority.NORMAL : Priority.HIGH)
+                    .build());
+        }
+
+        IssuesAssert asserter = assertThat(sut)
+                .hasSize(testObjects)
+                .hasSizeOfPriorityLow((testObjects + 2) / 3)
+                .hasSizeOfPriorityNormal((testObjects + 1) / 3)
+                .hasSizeOfPriorityHigh(testObjects / 3);
+
+        List<Issue> allIssues = sut.findByProperty(issue -> true);
+        Iterator<Issue> sutIterator = sut.iterator();
+        for (Issue issue: allIssues) {
+            org.assertj.core.api.Assertions.assertThat(issue).isEqualTo(sutIterator.next()).as("findByProperty with true as predicate doesn't deliver the whole list");
+        }
+
+        org.assertj.core.api.Assertions.assertThat(sut.findByProperty(issue -> false)).hasSize(0).as("findByProperty with false as predicate doesn't deliver a empty list");
+
+        org.assertj.core.api.Assertions.assertThat(sut.findByProperty(issue -> issue.getPriority() == Priority.LOW)).hasSize((testObjects + 2) / 3).as("findByProperty doesn't work with predicate issue.getPriority() == Priority.LOW");
+        org.assertj.core.api.Assertions.assertThat(sut.findByProperty(issue -> issue.getPriority() == Priority.NORMAL)).hasSize((testObjects + 1) / 3).as("findByProperty doesn't work with predicate issue.getPriority() == Priority.NORMAL");
+        org.assertj.core.api.Assertions.assertThat(sut.findByProperty(issue -> issue.getPriority() == Priority.HIGH)).hasSize(testObjects / 3).as("findByProperty doesn't work with predicate issue.getPriority() == Priority.HIGH");
 
     }
 
