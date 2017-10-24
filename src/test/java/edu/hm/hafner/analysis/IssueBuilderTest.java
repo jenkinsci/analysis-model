ed71808ade327c35488662ed025119382cfd709f
Tom <weissbier.weissbier@googlemail.com>
2017-10-24 16:32:39 +0200

    add test to IssueBuilderTest
    - negative values
    - null values
    - max values
    
    @Author Johannes Arzt
    @Author Tom Maier

--- src/test/java/edu/hm/hafner/analysis/IssueBuilderTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssueBuilderTest.java
@@ -104,4 +104,108 @@ class IssueBuilderTest {
         assertThat(copy).isEqualTo(basicIssue);
     }
 
+    @Test
+    void createIssueWithNullValuesTest() {
+        IssueBuilder builder = new IssueBuilder();
+        Issue basicIssue = builder
+                .setLineStart(0)
+                .setLineEnd(0)
+                .setPriority(null)
+                .setMessage(null)
+                .setFileName(null)
+                .setCategory(null)
+                .setType(null)
+                .setColumnStart(0)
+                .setColumnEnd(0)
+                .setPackageName(null)
+                .setDescription(null)
+                .build();
+
+        IssueSoftAssertion.assertIssueSoftly(softly -> {
+            softly.assertThat(basicIssue)
+                    .hasLineStart(0)
+                    .hasLineEnd(0)
+                    .hasPriority(Priority.NORMAL)
+                    .hasMessage("")
+                    .hasCategory("")
+                    .hasFileName("-")
+                    .hasType("-")
+                    .hasColumnStart(0)
+                    .hasColumnEnd(0)
+                    .hasPackageName("-")
+                    .hasDescription("")
+                    .hasToString(String.format("%s(%d,%d): %s: %s: %s", "-", 0, 0, "-", "", ""));
+        });
+
+    }
+
+    @Test
+    void createIssueWithNegativeValuesTest() {
+        IssueBuilder builder = new IssueBuilder();
+        Issue basicIssue = builder
+                .setLineStart(-1)
+                .setLineEnd(1)
+                .setPriority(Priority.NORMAL)
+                .setMessage("Hello I am a basic issue")
+                .setFileName("basic.txt")
+                .setCategory("basic")
+                .setType("basic")
+                .setColumnStart(-1)
+                .setColumnEnd(1)
+                .setPackageName("basic")
+                .setDescription("basic issue")
+                .build();
+
+
+        IssueSoftAssertion.assertIssueSoftly(softly -> {
+            softly.assertThat(basicIssue)
+                    .hasLineStart(0)
+                    .hasLineEnd(0)
+                    .hasPriority(Priority.NORMAL)
+                    .hasMessage("Hello I am a basic issue")
+                    .hasCategory("basic")
+                    .hasFileName("basic.txt")
+                    .hasType("basic")
+                    .hasColumnStart(0)
+                    .hasColumnEnd(1)
+                    .hasPackageName("basic")
+                    .hasDescription("basic issue")
+                    .hasToString(String.format("%s(%d,%d): %s: %s: %s", "basic.txt", 0, 0, "basic", "basic", "Hello I am a basic issue"));
+        });
+    }
+
+    @Test
+    void createIssueWithMaxValuesTest() {
+        IssueBuilder builder = new IssueBuilder();
+        Issue basicIssue = builder
+                .setLineStart(1)
+                .setLineEnd(Integer.MAX_VALUE)
+                .setPriority(Priority.NORMAL)
+                .setMessage("Hello I am a basic issue")
+                .setFileName("basic.txt")
+                .setCategory("basic")
+                .setType("basic")
+                .setColumnStart(-1)
+                .setColumnEnd(1)
+                .setPackageName("basic")
+                .setDescription("basic issue")
+                .build();
+
+
+        IssueSoftAssertion.assertIssueSoftly(softly -> {
+            softly.assertThat(basicIssue)
+                    .hasLineStart(1)
+                    .hasLineEnd(Integer.MAX_VALUE)
+                    .hasPriority(Priority.NORMAL)
+                    .hasMessage("Hello I am a basic issue")
+                    .hasCategory("basic")
+                    .hasFileName("basic.txt")
+                    .hasType("basic")
+                    .hasColumnStart(0)
+                    .hasColumnEnd(1)
+                    .hasPackageName("basic")
+                    .hasDescription("basic issue")
+                    .hasToString(String.format("%s(%d,%d): %s: %s: %s", "basic.txt", 1, Integer.MAX_VALUE, "basic", "basic", "Hello I am a basic issue"));
+        });
+    }
 }
