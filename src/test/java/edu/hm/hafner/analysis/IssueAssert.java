6169ec82b79edecac6c2f44a279e76324d770853
Michael Schmid <m.schmid@hm.edu>
2017-10-30 13:45:25 +0100

    Use default EXPECTED_BUT_WAS_MESSAGE

--- src/test/java/edu/hm/hafner/analysis/IssueAssert.java
+++ src/test/java/edu/hm/hafner/analysis/IssueAssert.java
@@ -12,6 +12,7 @@ import org.assertj.core.api.AbstractAssert;
 @SuppressWarnings("NonBooleanMethodNameMayNotStartWithQuestion")
 public class IssueAssert extends AbstractAssert<IssueAssert, Issue> {
 
+    private static final String EXPECTED_BUT_WAS_MESSAGE = "%nExpecting:%n <%s>%nto be equal to:%n <%s>%nbut was not.";
 
     public IssueAssert(final Issue actual) {
         super(actual, IssueAssert.class);
@@ -22,79 +23,79 @@ public class IssueAssert extends AbstractAssert<IssueAssert, Issue> {
     }
 
     public IssueAssert hasFileName(final String fileName) {
-        checkString("filename", fileName, actual.getFileName());
+        checkString(fileName, actual.getFileName());
         return this;
     }
 
     public IssueAssert hasCategory(final String category) {
-        checkString("category", category, actual.getCategory());
+        checkString(category, actual.getCategory());
         return this;
     }
 
     public IssueAssert hasType(final String type) {
-        checkString("type", type, actual.getType());
+        checkString(type, actual.getType());
         return this;
     }
 
     public IssueAssert hasPriority(final Priority priority) {
         isNotNull();
         if (!Objects.equals(actual.getPriority(), priority)) {
-            failWithMessage("Expected issue's priority to be <%s> but was <%s>", priority, actual.getPriority());
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, priority, actual.getPriority());
         }
         return this;
     }
 
     public IssueAssert hasMessage(final String message) {
-        checkString("message", message, actual.getMessage());
+        checkString(message, actual.getMessage());
         return this;
     }
 
     public IssueAssert hasDescription(final String description) {
-        checkString("description", description, actual.getDescription());
+        checkString(description, actual.getDescription());
         return this;
     }
 
     public IssueAssert hasPackageName(final String packageName) {
-        checkString("packageName", packageName, actual.getPackageName());
+        checkString(packageName, actual.getPackageName());
         return this;
     }
 
     public IssueAssert hasFingerprint(final String fingerprint) {
-        checkString("fingerprint", fingerprint, actual.getFingerprint());
+        checkString(fingerprint, actual.getFingerprint());
         return this;
     }
 
     public IssueAssert hasLineStart(final int lineStart) {
-        checkInt("lineStart", lineStart, actual.getLineStart());
+        checkInt(lineStart, actual.getLineStart());
         return this;
     }
 
     public IssueAssert hasLineEnd(final int lineEnd) {
-        checkInt("lineEnd", lineEnd, actual.getLineEnd());
+        checkInt(lineEnd, actual.getLineEnd());
         return this;
     }
 
     public IssueAssert hasColumnStart(final int columnStart) {
-        checkInt("columnStart", columnStart, actual.getColumnStart());
+        checkInt(columnStart, actual.getColumnStart());
         return this;
     }
 
     public IssueAssert hasColumnEnd(final int columnEnd) {
-        checkInt("columnEnd", columnEnd, actual.getColumnEnd());
+        checkInt(columnEnd, actual.getColumnEnd());
         return this;
     }
 
-    private void checkString(final String usage, final String expected, final String actualString) {
+    private void checkString(final String expected, final String actualString) {
         isNotNull();
         if (!Objects.equals(actualString, expected)) {
-            failWithMessage("Expected issue's %s to be <%s> but was <%s>", usage, expected, actualString);
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE,expected, actualString);
         }
     }
 
-    private void checkInt(final String usage, final int expected, final int actualInt) {
+    private void checkInt(final int expected, final int actualInt) {
         isNotNull();
         if (actualInt != expected) {
-            failWithMessage("Expected issue's %s to be <%s> but was <%s>", usage, expected, actualInt);
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, expected, actualInt);
         }
     }
 }
