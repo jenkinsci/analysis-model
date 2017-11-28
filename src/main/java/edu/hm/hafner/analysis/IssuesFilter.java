260da7fbefee6816a296573bbc826f0572252e6a
aykut <aykut.yilmaz0@hm.edu>
2017-11-28 12:11:44 +0100

    + IssuesFilter CheckStyle clean

--- src/main/java/edu/hm/hafner/analysis/IssuesFilter.java
+++ src/main/java/edu/hm/hafner/analysis/IssuesFilter.java
@@ -13,16 +13,20 @@ import static java.util.Collections.singletonList;
 
 /**
  * Filters issues by the given filters that are set.
+ *
+ * @author Aykut Yilmaz
  */
 public class IssuesFilter {
+
     private final Issues issues;
+
     private final Collection<String> includeFileNames;
     private final Collection<String> includePackageNames;
     private final Collection<String> includeModuleNames;
     private final Collection<String> includeCategories;
     private final Collection<String> includeTypes;
 
-    private final Collection<String> excludeFilenames;
+    private final Collection<String> excludeFileNames;
     private final Collection<String> excludePackageNames;
     private final Collection<String> excludeModuleNames;
     private final Collection<String> excludeCategories;
@@ -34,36 +38,40 @@ public class IssuesFilter {
      *
      * @param issues that shall be filtered.
      */
-    public IssuesFilter(Issues issues) {
+    public IssuesFilter(final Issues issues) {
         this.issues = issues;
 
-        this.includeFileNames = new HashSet<>();
-        this.includePackageNames = new HashSet<>();
-        this.includeModuleNames = new HashSet<>();
-        this.includeCategories = new HashSet<>();
-        this.includeTypes = new HashSet<>();
+        includeFileNames = new HashSet<>();
+        includePackageNames = new HashSet<>();
+        includeModuleNames = new HashSet<>();
+        includeCategories = new HashSet<>();
+        includeTypes = new HashSet<>();
 
-        this.excludeFilenames = new HashSet<>();
-        this.excludePackageNames = new HashSet<>();
-        this.excludeModuleNames = new HashSet<>();
-        this.excludeCategories = new HashSet<>();
-        this.excludeTypes = new HashSet<>();
+        excludeFileNames = new HashSet<>();
+        excludePackageNames = new HashSet<>();
+        excludeModuleNames = new HashSet<>();
+        excludeCategories = new HashSet<>();
+        excludeTypes = new HashSet<>();
     }
 
     /**
      * Filters the issues according to the given filters.
      *
-     * @return if the issues were empty or no filters were given, a copy of the issues will be returned,
-     * otherwise a filtered issues will be returned.
+     * @return if the issues were empty or no filters were given, a copy of the issues will be returned, otherwise a
+     * filtered issues will be returned.
      */
     public Issues filter() {
-        if (this.issues.isEmpty() || !hasFilters()) return issues.copy();
+        if (issues.isEmpty() || !hasFilters()) {
+            return issues.copy();
+        }
 
         ensureDisjointRegexps();
 
-        final Collection<Issues> includeWarnings = hasIncludeFilters() ? includeWarnings() : singletonList(this.issues);
-        final Issues includeWarningsMerged = mergeIssuesDistinct(includeWarnings);
-        if (!hasExcludeFilters()) return includeWarningsMerged;
+        Collection<Issues> includeWarnings = hasIncludeFilters() ? includeWarnings() : singletonList(issues);
+        Issues includeWarningsMerged = mergeIssuesDistinct(includeWarnings);
+        if (!hasExcludeFilters()) {
+            return includeWarningsMerged;
+        }
 
         mergeIssuesDistinct(excludeWarnings()).forEach(issue -> includeWarningsMerged.remove(issue.getId()));
 
@@ -71,77 +79,84 @@ public class IssuesFilter {
     }
 
     private void ensureDisjointRegexps() {
-        final String filterConflict = "cannot include and exclude same ";
-        Ensure.that(Collections.disjoint(includeFileNames, excludeFilenames))
-                .isTrue(filterConflict + "fileName");
+        String filterConflict = "cannot include and exclude same";
+        Ensure.that(Collections.disjoint(includeFileNames, excludeFileNames))
+                .isTrue(filterConflict + " " + "fileName");
 
         Ensure.that(Collections.disjoint(includePackageNames, excludePackageNames))
-                .isTrue(filterConflict + "packageName");
+                .isTrue(filterConflict + " " + "packageName");
 
         Ensure.that(Collections.disjoint(includeModuleNames, excludeModuleNames))
-                .isTrue(filterConflict + "moduleName");
+                .isTrue(filterConflict + " " + "moduleName");
 
         Ensure.that(Collections.disjoint(includeCategories, excludeCategories))
-                .isTrue(filterConflict + "category");
+                .isTrue(filterConflict + " " + "category");
 
         Ensure.that(Collections.disjoint(includeTypes, excludeTypes))
-                .isTrue(filterConflict + "type");
+                .isTrue(filterConflict + " " + "type");
     }
 
-    private Issues mergeIssuesDistinct(Collection<Issues> warnings) {
+    private Issues mergeIssuesDistinct(final Collection<Issues> warnings) {
         return new Issues(stream(Issues.merge(warnings)).distinct().collect(Collectors.toList()));
     }
 
-    private Collection<Issues> filterWarnings(Collection<String> fileNamesCollection,
-                                              Collection<String> packageNamesCollection,
-                                              Collection<String> moduleNamesCollection,
-                                              Collection<String> categoriesCollection,
-                                              Collection<String> typesCollection) {
+    private Collection<Issues> filterWarnings(final Collection<String> fileNamesCollection,
+                                              final Collection<String> packageNamesCollection,
+                                              final Collection<String> moduleNamesCollection,
+                                              final Collection<String> categoriesCollection,
+                                              final Collection<String> typesCollection) {
         Collection<Issues> warnings = new ArrayList<>();
 
-        final String fileNames = mergeRegex(fileNamesCollection);
-        final String packageNames = mergeRegex(packageNamesCollection);
-        final String moduleNames = mergeRegex(moduleNamesCollection);
-        final String categories = mergeRegex(categoriesCollection);
-        final String types = mergeRegex(typesCollection);
+        String fileNames = mergeRegex(fileNamesCollection);
+        String packageNames = mergeRegex(packageNamesCollection);
+        String moduleNames = mergeRegex(moduleNamesCollection);
+        String categories = mergeRegex(categoriesCollection);
+        String types = mergeRegex(typesCollection);
 
-        if (!fileNames.isEmpty())
-            warnings.add(this.issues.filter(issue -> issue.getFileName().matches(fileNames)));
-        if (!packageNames.isEmpty())
-            warnings.add(this.issues.filter(issue -> issue.getPackageName().matches(packageNames)));
-        if (!moduleNames.isEmpty())
-            warnings.add(this.issues.filter(issue -> issue.getModuleName().matches(moduleNames)));
-        if (!categories.isEmpty())
-            warnings.add(this.issues.filter(issue -> issue.getCategory().matches(categories)));
-        if (!types.isEmpty())
-            warnings.add(this.issues.filter(issue -> issue.getType().matches(types)));
+        if (!fileNames.isEmpty()) {
+            warnings.add(issues.filter(issue -> issue.getFileName().matches(fileNames)));
+        }
+        if (!packageNames.isEmpty()) {
+            warnings.add(issues.filter(issue -> issue.getPackageName().matches(packageNames)));
+        }
+        if (!moduleNames.isEmpty()) {
+            warnings.add(issues.filter(issue -> issue.getModuleName().matches(moduleNames)));
+        }
+        if (!categories.isEmpty()) {
+            warnings.add(issues.filter(issue -> issue.getCategory().matches(categories)));
+        }
+        if (!types.isEmpty()) {
+            warnings.add(issues.filter(issue -> issue.getType().matches(types)));
+        }
         return warnings;
     }
 
     private Collection<Issues> includeWarnings() {
         return filterWarnings(
-                this.includeFileNames,
-                this.includePackageNames,
-                this.includeModuleNames,
-                this.includeCategories,
-                this.includeTypes);
+                includeFileNames,
+                includePackageNames,
+                includeModuleNames,
+                includeCategories,
+                includeTypes);
     }
 
     private Collection<Issues> excludeWarnings() {
         return filterWarnings(
-                this.excludeFilenames,
-                this.excludePackageNames,
-                this.excludeModuleNames,
-                this.excludeCategories,
-                this.excludeTypes);
+                excludeFileNames,
+                excludePackageNames,
+                excludeModuleNames,
+                excludeCategories,
+                excludeTypes);
     }
 
-    private String mergeRegex(Collection<String> regexps) {
-        if (regexps.isEmpty()) return "";
+    private String mergeRegex(final Collection<String> regexps) {
+        if (regexps.isEmpty()) {
+            return "";
+        }
 
         StringBuilder builder = new StringBuilder();
 
-        regexps.forEach(regex -> builder.append(regex).append('|'));
+        regexps.forEach(regex -> builder.append('(').append(regex).append(')').append('|'));
         builder.deleteCharAt(builder.length() - 1);
 
         return builder.toString();
@@ -152,118 +167,156 @@ public class IssuesFilter {
     }
 
     private boolean hasIncludeFilters() {
-        return !(includeFileNames.isEmpty()
-                && includePackageNames.isEmpty()
-                && includeModuleNames.isEmpty()
-                && includeCategories.isEmpty()
-                && includeTypes.isEmpty());
+        if (hasIncludeFileNames() || hasIncludePackageNames()) {
+            return true;
+        }
+
+        if (hasIncludeModuleNames() || hasIncludeCategories()) {
+            return true;
+        }
+
+        return hasIncludeTypes();
+    }
+
+    private boolean hasIncludeTypes() {
+        return !includeTypes.isEmpty();
+    }
+
+    private boolean hasIncludeCategories() {
+        return !includeCategories.isEmpty();
+    }
+
+    private boolean hasIncludeModuleNames() {
+        return !includeModuleNames.isEmpty();
+    }
+
+    private boolean hasIncludePackageNames() {
+        return !includePackageNames.isEmpty();
+    }
+
+    private boolean hasIncludeFileNames() {
+        return !includeFileNames.isEmpty();
     }
 
     private boolean hasExcludeFilters() {
-        return !(excludeFilenames.isEmpty()
-                && excludePackageNames.isEmpty()
-                && excludeModuleNames.isEmpty()
-                && excludeCategories.isEmpty()
-                && excludeTypes.isEmpty());
+        if (hasExcludeFileNames() || hasExcludePackageNames()) {
+            return true;
+        }
+
+        if (hasExcludeModuleNames() || hasExcludeCategories()) {
+            return true;
+        }
+
+        return hasExcludeTypes();
+    }
+
+    private boolean hasExcludeTypes() {
+        return !excludeTypes.isEmpty();
+    }
+
+    private boolean hasExcludeCategories() {
+        return !excludeCategories.isEmpty();
+    }
+
+    private boolean hasExcludeModuleNames() {
+        return !excludeModuleNames.isEmpty();
+    }
+
+    private boolean hasExcludePackageNames() {
+        return !excludePackageNames.isEmpty();
+    }
+
+    private boolean hasExcludeFileNames() {
+        return !excludeFileNames.isEmpty();
     }
 
     /**
-     * Specify files by the filename that will be included additionally.
-     * The regex will be added to a collection.
+     * Specify files by the filename that will be included additionally. The regex will be added to a collection.
      *
      * @param regex specifies the filenames.
      */
-    public void includeFileNames(String regex) {
-        this.includeFileNames.add(regex);
+    public void includeFileNames(final String regex) {
+        includeFileNames.add(regex);
     }
 
     /**
-     * Specify files by the package name that will be included additionally.
-     * The regex will be added to a collection.
+     * Specify files by the package name that will be included additionally. The regex will be added to a collection.
      *
      * @param regex specifies the package names.
      */
-    public void includePackageNames(String regex) {
-        this.includePackageNames.add(regex);
+    public void includePackageNames(final String regex) {
+        includePackageNames.add(regex);
     }
 
     /**
-     * Specify files by the module name that will be included additionally.
-     * The regex will be added to a collection.
+     * Specify files by the module name that will be included additionally. The regex will be added to a collection.
      *
      * @param regex specifies the module names.
      */
-    public void includeModuleNames(String regex) {
-        this.includeModuleNames.add(regex);
+    public void includeModuleNames(final String regex) {
+        includeModuleNames.add(regex);
     }
 
     /**
-     * Specify files by the category that will be included additionally.
-     * The regex will be added to a collection.
+     * Specify files by the category that will be included additionally. The regex will be added to a collection.
      *
      * @param regex specifies the categories.
      */
-    public void includeCategories(String regex) {
-        this.includeCategories.add(regex);
+    public void includeCategories(final String regex) {
+        includeCategories.add(regex);
     }
 
     /**
-     * Specify files by the type that will be included additionally.
-     * The regex will be added to a collection.
+     * Specify files by the type that will be included additionally. The regex will be added to a collection.
      *
      * @param regex specifies the types.
      */
-    public void includeType(String regex) {
-        this.includeTypes.add(regex);
+    public void includeType(final String regex) {
+        includeTypes.add(regex);
     }
 
     /**
-     * Specify files by the filename that will be excluded.
-     * The regex will be added to a collection.
+     * Specify files by the filename that will be excluded. The regex will be added to a collection.
      *
      * @param regex specifies the filenames.
      */
-    public void excludeFileNames(String regex) {
-        this.excludeFilenames.add(regex);
+    public void excludeFileNames(final String regex) {
+        excludeFileNames.add(regex);
     }
 
     /**
-     * Specify files by the package name that will be excluded.
-     * The regex will be added to a collection.
+     * Specify files by the package name that will be excluded. The regex will be added to a collection.
      *
      * @param regex specifies the package names.
      */
-    public void excludePackageNames(String regex) {
-        this.excludePackageNames.add(regex);
+    public void excludePackageNames(final String regex) {
+        excludePackageNames.add(regex);
     }
 
     /**
-     * Specify files by the module name that will be excluded.
-     * The regex will be added to a collection.
+     * Specify files by the module name that will be excluded. The regex will be added to a collection.
      *
      * @param regex specifies the module names.
      */
-    public void excludeModuleNames(String regex) {
-        this.excludeModuleNames.add(regex);
+    public void excludeModuleNames(final String regex) {
+        excludeModuleNames.add(regex);
     }
 
     /**
-     * Specify files by the category that will be excluded.
-     * The regex will be added to a collection.
+     * Specify files by the category that will be excluded. The regex will be added to a collection.
      *
      * @param regex specifies the categories.
      */
-    public void excludeCategories(String regex) {
-        this.excludeCategories.add(regex);
+    public void excludeCategories(final String regex) {
+        excludeCategories.add(regex);
     }
 
     /**
-     * Specify files by the type that will be excluded.
-     * The regex will be added to a collection.
+     * Specify files by the type that will be excluded. The regex will be added to a collection.
      *
      * @param regex specifies the types.
      */
-    public void excludeTypes(String regex) {
-        this.excludeTypes.add(regex);
+    public void excludeTypes(final String regex) {
+        excludeTypes.add(regex);
     }
 }
