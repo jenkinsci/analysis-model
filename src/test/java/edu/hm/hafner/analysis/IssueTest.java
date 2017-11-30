e3fb9883ee83784bb91eb289137a505cd7cb8f15
Ulli Hafner <ullrich.hafner@gmail.com>
2017-12-01 00:03:31 +0100

    Added serialization tests for Issue and BuildIssue.

--- src/test/java/edu/hm/hafner/analysis/IssueTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssueTest.java
@@ -1,5 +1,18 @@
 package edu.hm.hafner.analysis;
 
+import javax.annotation.CheckForNull;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 import org.junit.jupiter.api.Test;
 
 import static edu.hm.hafner.analysis.assertj.Assertions.*;
@@ -10,7 +23,7 @@ import static edu.hm.hafner.analysis.assertj.SoftAssertions.*;
  *
  * @author Marcel Binder
  */
-class IssueTest {
+public class IssueTest {
     static final String FILE_NAME = "C:/users/tester/file-name";
     static final String FILE_NAME_WITH_BACKSLASHES = "C:\\users\\tester/file-name";
     static final int LINE_START = 1;
@@ -30,6 +43,53 @@ class IssueTest {
     static final String UNDEFINED = "-";
     static final String FINGERPRINT = "fingerprint";
     static final String ORIGIN = "origin";
+    private static final String SERIALIZATION_NAME = "issue.ser";
+
+    /**
+     * Creates a new subject under test (i.e. a sub-type of {@link Issue}) using the specified properties.
+     *
+     * @param fileName
+     *         the name of the file that contains this issue
+     * @param lineStart
+     *         the first line of this issue (lines start at 1; 0 indicates the whole file)
+     * @param lineEnd
+     *         the last line of this issue (lines start at 1)
+     * @param columnStart
+     *         the first column of this issue (columns start at 1, 0 indicates the whole line)
+     * @param columnEnd
+     *         the last column of this issue (columns start at 1)
+     * @param category
+     *         the category of this issue (depends on the available categories of the static analysis tool)
+     * @param type
+     *         the type of this issue (depends on the available types of the static analysis tool)
+     * @param packageName
+     *         the name of the package (or name space) that contains this issue
+     * @param moduleName
+     *         the name of the moduleName (or project) that contains this issue
+     * @param priority
+     *         the priority of this issue
+     * @param message
+     *         the detail message of this issue
+     * @param description
+     *         the description for this issue
+     * @param origin
+     *         the ID of the tool that did report this issue
+     * @param fingerprint
+     *         the finger print for this issue
+     *
+     * @return the subject under test
+     */
+    @SuppressWarnings("ParameterNumber")
+    protected Issue createIssue(@CheckForNull final String fileName,
+            final int lineStart, final int lineEnd, final int columnStart, final int columnEnd,
+            @CheckForNull final String category, @CheckForNull final String type,
+            @CheckForNull final String packageName, @CheckForNull final String moduleName,
+            @CheckForNull final Priority priority,
+            @CheckForNull final String message, @CheckForNull final String description,
+            @CheckForNull final String origin, @CheckForNull final String fingerprint) {
+        return new Issue(fileName, lineStart, lineEnd, columnStart, columnEnd, category, type, packageName,
+                moduleName, priority, message, description, origin, fingerprint);
+    }
 
     @Test
     void testIssue() {
@@ -57,16 +117,15 @@ class IssueTest {
 
     @Test
     void testDefaultIssueNullStringsNegativeIntegers() {
-        Issue issue = new Issue(null, 0, 0, 0, 0, null, null,
-                null, null, PRIORITY, null, null, null, null);
+        Issue issue = createIssue(null, 0, 0, 0, 0, null, null, null, null, PRIORITY, null, null, null, null);
 
         assertIsDefaultIssue(issue);
     }
 
     @Test
     void testDefaultIssueEmptyStringsNegativeIntegers() {
-        Issue issue = new Issue(EMPTY, -1, -1, -1, -1,
-                EMPTY, EMPTY, EMPTY, EMPTY, PRIORITY, EMPTY, EMPTY, EMPTY, EMPTY);
+        Issue issue = createIssue(EMPTY, -1, -1, -1, -1, EMPTY, EMPTY, EMPTY, EMPTY, PRIORITY, EMPTY, EMPTY, EMPTY,
+                EMPTY);
 
         assertIsDefaultIssue(issue);
     }
@@ -91,8 +150,8 @@ class IssueTest {
 
     @Test
     void testZeroLineColumnEndsDefaultToLineColumnStarts() {
-        Issue issue = new Issue(FILE_NAME, LINE_START, 0, COLUMN_START, 0, CATEGORY, TYPE, PACKAGE_NAME,
-                MODULE_NAME, PRIORITY, MESSAGE, DESCRIPTION, ORIGIN, FINGERPRINT);
+        Issue issue = createIssue(FILE_NAME, LINE_START, 0, COLUMN_START, 0, CATEGORY, TYPE, PACKAGE_NAME, MODULE_NAME,
+                PRIORITY, MESSAGE, DESCRIPTION, ORIGIN, FINGERPRINT);
 
         assertSoftly(softly -> {
             softly.assertThat(issue)
@@ -105,8 +164,8 @@ class IssueTest {
 
     @Test
     void testNullPriorityDefaultsToNormal() {
-        Issue issue = new Issue(FILE_NAME, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY, TYPE, PACKAGE_NAME,
-                MODULE_NAME, null, MESSAGE, DESCRIPTION, ORIGIN, FINGERPRINT);
+        Issue issue = createIssue(FILE_NAME, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY, TYPE,
+                PACKAGE_NAME, MODULE_NAME, null, MESSAGE, DESCRIPTION, ORIGIN, FINGERPRINT);
 
         assertThat(issue.getPriority()).isEqualTo(Priority.NORMAL);
     }
@@ -119,8 +178,8 @@ class IssueTest {
         assertThat(one.getId()).isNotEqualTo(another.getId());
     }
 
-    private Issue createFilledIssue() {
-        return new Issue(FILE_NAME, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY, TYPE, PACKAGE_NAME,
+    protected Issue createFilledIssue() {
+        return createIssue(FILE_NAME, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY, TYPE, PACKAGE_NAME,
                 MODULE_NAME, PRIORITY, MESSAGE, DESCRIPTION, ORIGIN, FINGERPRINT);
     }
 
@@ -140,7 +199,7 @@ class IssueTest {
 
     @Test
     void testFileNameBackslashConversion() {
-        Issue issue = new Issue(FILE_NAME_WITH_BACKSLASHES, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY,
+        Issue issue = createIssue(FILE_NAME_WITH_BACKSLASHES, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY,
                 TYPE, PACKAGE_NAME, MODULE_NAME, PRIORITY, MESSAGE, DESCRIPTION, ORIGIN, FINGERPRINT);
 
         assertThat(issue).hasFileName(FILE_NAME);
@@ -148,7 +207,7 @@ class IssueTest {
 
     @Test
     void testMessageDescriptionStripped() {
-        Issue issue = new Issue(FILE_NAME_WITH_BACKSLASHES, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY,
+        Issue issue = createIssue(FILE_NAME_WITH_BACKSLASHES, LINE_START, LINE_END, COLUMN_START, COLUMN_END, CATEGORY,
                 TYPE, PACKAGE_NAME, MODULE_NAME, PRIORITY, MESSAGE_NOT_STRIPPED, DESCRIPTION_NOT_STRIPPED, ORIGIN,
                 FINGERPRINT);
 
@@ -157,5 +216,89 @@ class IssueTest {
                     .hasMessage(MESSAGE)
                     .hasDescription(DESCRIPTION);
         });
+    }
+
+    /**
+     * Ensures that an issue instance can be serialized and deserialized.
+     */
+    @Test
+    void shouldBeSerializable() {
+        Issue createdWithConstructor = createFilledIssue();
+
+        byte[] bytes = asBytes(createdWithConstructor);
+
+        assertThatIssueCanBeRestoredFrom(bytes);
+    }
+
+    /**
+     * Verifies that saved serialized format (from a previous release) still can be resolved with the current
+     * implementation of {@link Issue}.
+     */
+    @Test
+    void shouldReadIssueFromOldSerialization() {
+        byte[] restored = readResource(SERIALIZATION_NAME);
+
+        assertThatIssueCanBeRestoredFrom(restored);
+    }
+
+    private void assertThatIssueCanBeRestoredFrom(final byte[] restored) {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new ByteArrayInputStream(restored))) {
+            Object issue = inputStream.readObject();
+
+            assertThat(issue).isInstanceOf(Issue.class);
+            assertThat(issue).isEqualTo(createFilledIssue());
+        }
+        catch (IOException | ClassNotFoundException e) {
+            throw new AssertionError("Can' resolve issue from byte array", e);
+        }
+    }
+
+    protected byte[] readResource(final String name) {
+        try {
+            URL resource = getClass().getResource(name);
+            if (resource == null) {
+                throw new AssertionError("Can't find resource " + name);
+            }
+            return Files.readAllBytes(Paths.get(resource.toURI()));
+        }
+        catch (IOException | URISyntaxException e) {
+            throw new AssertionError("Can't read resource " + name, e);
+        }
+    }
+
+    private byte[] asBytes(final Serializable object) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (ObjectOutputStream stream = new ObjectOutputStream(out)) {
+            stream.writeObject(object);
+            stream.close();
+        }
+        catch (IOException exception) {
+            throw new IllegalStateException(exception);
+        }
+        return out.toByteArray();
+    }
+
+    /**
+     * Serializes an issues to a file. Use this method in case the issue properties have been changed and the
+     * readResolve method has been adapted accordingly so that the old serialization still can be read.
+     *
+     * @param args
+     *         not used
+     *
+     * @throws IOException
+     *         if the file could not be written
+     */
+    public static void useIfSerializationChanges(final String... args) throws IOException {
+        new IssueTest().createSerialization();
+    }
+
+    /**
+     * Serializes an issue using an {@link ObjectOutputStream } to the file /tmp/issue.ser.
+     *
+     * @throws IOException
+     *         if the file could not be created
+     */
+    protected void createSerialization() throws IOException {
+        Files.write(Paths.get("/tmp/issue.ser"), asBytes(createFilledIssue()), StandardOpenOption.CREATE_NEW);
     }
 }
