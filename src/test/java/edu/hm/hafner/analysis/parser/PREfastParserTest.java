8620358a2b3f9ebfa797ea141364c84ae6d98061
Johannes <johannesbruehl@googlemail.com>
2017-12-09 15:07:44 +0100

    Converted four ParserTests to Abstract Test Pattern

--- src/test/java/edu/hm/hafner/analysis/parser/PREfastParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/PREfastParserTest.java
@@ -2,61 +2,66 @@ package edu.hm.hafner.analysis.parser;
 
 import java.util.Iterator;
 
-import org.junit.jupiter.api.Test;
-
+import edu.hm.hafner.analysis.AbstractParser;
+import edu.hm.hafner.analysis.AbstractParserTest;
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.Issues;
 import edu.hm.hafner.analysis.Priority;
+import edu.hm.hafner.analysis.assertj.SoftAssertions;
 import static edu.hm.hafner.analysis.assertj.IssuesAssert.*;
-import static edu.hm.hafner.analysis.assertj.SoftAssertions.*;
 
 /**
  * Tests the class {@link PREfastParser}.
  *
  * @author Charles Chan
  */
-public class PREfastParserTest extends ParserTester {
-    /**
-     * Tests the Puppet-Lint parsing.
-     */
-    @Test
-    public void testParse() {
+public class PREfastParserTest extends AbstractParserTest {
+
+    PREfastParserTest() {
+        super("PREfast.xml");
+    }
+
+
+    protected String getWarningsFile() {
+        return "PREfast.xml";
+    }
+
+    @Override
+    protected void assertThatIssuesArePresent(final Issues<Issue> issues, final SoftAssertions softly) {
         Issues<Issue> results = new PREfastParser().parse(openFile());
         assertThat(results).hasSize(11);
 
         Iterator<Issue> iterator = results.iterator();
 
-        assertSoftly(softly -> {
-            softly.assertThat(iterator.next())
-                    .hasPriority(Priority.NORMAL)
-                    .hasCategory("28101")
-                    .hasLineStart(102)
-                    .hasLineEnd(102)
-                    .hasMessage(
-                            "The Drivers module has inferred that the current function is a DRIVER_INITIALIZE function:  This is informational only. No problem has been detected.")
-                    .hasFileName("sys.c");
+        softly.assertThat(iterator.next())
+                .hasPriority(Priority.NORMAL)
+                .hasCategory("28101")
+                .hasLineStart(102)
+                .hasLineEnd(102)
+                .hasMessage(
+                        "The Drivers module has inferred that the current function is a DRIVER_INITIALIZE function:  This is informational only. No problem has been detected.")
+                .hasFileName("sys.c");
 
-            softly.assertThat(iterator.next())
-                    .hasPriority(Priority.NORMAL)
-                    .hasCategory("6014")
-                    .hasLineStart(116)
-                    .hasLineEnd(116)
-                    .hasMessage("(PFD)Leaking memory 'device'.")
-                    .hasFileName("sys.c");
+        softly.assertThat(iterator.next())
+                .hasPriority(Priority.NORMAL)
+                .hasCategory("6014")
+                .hasLineStart(116)
+                .hasLineEnd(116)
+                .hasMessage("(PFD)Leaking memory 'device'.")
+                .hasFileName("sys.c");
 
-            softly.assertThat(iterator.next())
-                    .hasPriority(Priority.NORMAL)
-                    .hasCategory("28155")
-                    .hasLineStart(137)
-                    .hasLineEnd(137)
-                    .hasMessage(
-                            "The function being assigned or passed should be a DRIVER_UNLOAD function:  Add the declaration 'DRIVER_UNLOAD OnUnload;' before the current first declaration of OnUnload.")
-                    .hasFileName("sys.c");
-        });
+        softly.assertThat(iterator.next())
+                .hasPriority(Priority.NORMAL)
+                .hasCategory("28155")
+                .hasLineStart(137)
+                .hasLineEnd(137)
+                .hasMessage(
+                        "The function being assigned or passed should be a DRIVER_UNLOAD function:  Add the declaration 'DRIVER_UNLOAD OnUnload;' before the current first declaration of OnUnload.")
+                .hasFileName("sys.c");
     }
 
     @Override
-    protected String getWarningsFile() {
-        return "PREfast.xml";
+    protected AbstractParser createParser() {
+        return new PREfastParser();
     }
 }
--- src/test/java/edu/hm/hafner/analysis/parser/PerlCriticParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/PerlCriticParserTest.java
@@ -4,9 +4,12 @@ import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
 
+import edu.hm.hafner.analysis.AbstractParser;
+import edu.hm.hafner.analysis.AbstractParserTest;
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.Issues;
 import edu.hm.hafner.analysis.Priority;
+import edu.hm.hafner.analysis.assertj.SoftAssertions;
 import static edu.hm.hafner.analysis.assertj.Assertions.*;
 import static edu.hm.hafner.analysis.assertj.SoftAssertions.*;
 
@@ -15,7 +18,12 @@ import static edu.hm.hafner.analysis.assertj.SoftAssertions.*;
  *
  * @author Mihail Menev, menev@hm.edu
  */
-public class PerlCriticParserTest extends ParserTester {
+public class PerlCriticParserTest extends AbstractParserTest {
+
+    PerlCriticParserTest() {
+        super("perlcritic.txt");
+    }
+
     /**
      * Parses a mixed log file with 105 perlcritic warnings and /var/log/ messages.
      */
@@ -26,46 +34,6 @@ public class PerlCriticParserTest extends ParserTester {
         assertThat(warnings).hasSize(105);
     }
 
-    /**
-     * Parses a file with three warnings.
-     */
-    @Test
-    public void testPerlCriticParserCreateWarning() {
-        Issues<Issue> warnings = parse("issue17792.txt");
-
-        assertThat(warnings).hasSize(3);
-
-        Iterator<Issue> iterator = warnings.iterator();
-
-        assertSoftly(softly -> {
-            softly.assertThat(iterator.next())
-                    .hasPriority(Priority.LOW)
-                    .hasCategory("33 of PBP")
-                    .hasLineStart(1)
-                    .hasLineEnd(1)
-                    .hasMessage("Code is not tidy")
-                    .hasFileName("perl/dir_handler.pl")
-                    .hasColumnStart(1);
-
-            softly.assertThat(iterator.next())
-                    .hasPriority(Priority.HIGH)
-                    .hasCategory("431 of PBP")
-                    .hasLineStart(10)
-                    .hasLineEnd(10)
-                    .hasMessage("Code before warnings are enabled")
-                    .hasFileName("perl/system.pl")
-                    .hasColumnStart(1);
-
-            softly.assertThat(iterator.next())
-                    .hasPriority(Priority.NORMAL)
-                    .hasCategory("Use IPC::Open3 instead")
-                    .hasLineStart(7)
-                    .hasLineEnd(7)
-                    .hasMessage("Backtick operator used")
-                    .hasFileName("perl/ch1/hello")
-                    .hasColumnStart(10);
-        });
-    }
 
     /**
      * Parses a file with three warnings without the filename in the warning.
@@ -78,40 +46,78 @@ public class PerlCriticParserTest extends ParserTester {
         Iterator<Issue> iterator = warnings.iterator();
 
         assertSoftly(softly -> {
-                    softly.assertThat(iterator.next())
-                            .hasPriority(Priority.LOW)
-                            .hasCategory("Don't use whitespace at the end of lines")
-                            .hasLineStart(18)
-                            .hasLineEnd(18)
-                            .hasMessage("Found \"\\N{SPACE}\" at the end of the line")
-                            .hasFileName("-")
-                            .hasColumnStart(77);
+            softly.assertThat(iterator.next())
+                    .hasPriority(Priority.LOW)
+                    .hasCategory("Don't use whitespace at the end of lines")
+                    .hasLineStart(18)
+                    .hasLineEnd(18)
+                    .hasMessage("Found \"\\N{SPACE}\" at the end of the line")
+                    .hasFileName("-")
+                    .hasColumnStart(77);
 
-                    softly.assertThat(iterator.next())
-                            .hasPriority(Priority.NORMAL)
-                            .hasCategory("240,241 of PBP")
-                            .hasLineStart(16)
-                            .hasLineEnd(16)
-                            .hasMessage("Regular expression without \"/s\" flag")
-                            .hasFileName("-")
-                            .hasColumnStart(28);
+            softly.assertThat(iterator.next())
+                    .hasPriority(Priority.NORMAL)
+                    .hasCategory("240,241 of PBP")
+                    .hasLineStart(16)
+                    .hasLineEnd(16)
+                    .hasMessage("Regular expression without \"/s\" flag")
+                    .hasFileName("-")
+                    .hasColumnStart(28);
 
-                    softly.assertThat(iterator.next())
-                            .hasPriority(Priority.HIGH)
-                            .hasCategory("202,204 of PBP")
-                            .hasLineStart(15)
-                            .hasLineEnd(15)
-                            .hasMessage("Bareword file handle opened")
-                            .hasFileName("-")
-                            .hasColumnStart(1);
-                });
+            softly.assertThat(iterator.next())
+                    .hasPriority(Priority.HIGH)
+                    .hasCategory("202,204 of PBP")
+                    .hasLineStart(15)
+                    .hasLineEnd(15)
+                    .hasMessage("Bareword file handle opened")
+                    .hasFileName("-")
+                    .hasColumnStart(1);
+        });
     }
 
-    private Issues<Issue> parse(final String fileName) {
-        return new PerlCriticParser().parse(openFile(fileName));
+
+    @Override
+    protected void assertThatIssuesArePresent(final Issues<Issue> issues, final SoftAssertions softly) {
+        Issues<Issue> warnings = parse("issue17792.txt");
+
+        assertThat(warnings).hasSize(3);
+
+        Iterator<Issue> iterator = warnings.iterator();
+
+        softly.assertThat(iterator.next())
+                .hasPriority(Priority.LOW)
+                .hasCategory("33 of PBP")
+                .hasLineStart(1)
+                .hasLineEnd(1)
+                .hasMessage("Code is not tidy")
+                .hasFileName("perl/dir_handler.pl")
+                .hasColumnStart(1);
+
+        softly.assertThat(iterator.next())
+                .hasPriority(Priority.HIGH)
+                .hasCategory("431 of PBP")
+                .hasLineStart(10)
+                .hasLineEnd(10)
+                .hasMessage("Code before warnings are enabled")
+                .hasFileName("perl/system.pl")
+                .hasColumnStart(1);
+
+        softly.assertThat(iterator.next())
+                .hasPriority(Priority.NORMAL)
+                .hasCategory("Use IPC::Open3 instead")
+                .hasLineStart(7)
+                .hasLineEnd(7)
+                .hasMessage("Backtick operator used")
+                .hasFileName("perl/ch1/hello")
+                .hasColumnStart(10);
+
     }
 
     @Override
+    protected AbstractParser createParser() {
+        return new PerlCriticParser();
+    }
+
     protected String getWarningsFile() {
         return "perlcritic.txt";
     }
