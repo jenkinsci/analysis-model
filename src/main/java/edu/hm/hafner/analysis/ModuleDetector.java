be5ad394eebf6e69fe36a1728f687d09be36c9c9
Ulli Hafner <ullrich.hafner@gmail.com>
2018-02-17 17:36:25 +0100

    Moved ModuleDetector to analysis-model.

---
+++ src/main/java/edu/hm/hafner/analysis/ModuleDetector.java
@@ -0,0 +1,327 @@
+package edu.hm.hafner.analysis;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import org.apache.commons.digester3.Digester;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.xml.sax.SAXException;
+
+/**
+ * Detects module names by parsing the name of a source file, the Maven pom.xml
+ * file or the ANT build.xml file.
+ *
+ * @author Ulli Hafner
+ * @author Christoph Laeubrich (support for OSGi-Bundles)
+ */
+public class ModuleDetector {
+    private static final String PLUS = ", ";
+    private static final String BACK_SLASH = "\\";
+    private static final String SLASH = "/";
+    private static final String ALL_DIRECTORIES = "**/";
+
+    private static final String BUNDLE_VENDOR = "Bundle-Vendor";
+    private static final String BUNDLE_SYMBOLIC_NAME = "Bundle-SymbolicName";
+    private static final String BUNDLE_NAME = "Bundle-Name";
+    private static final String REPLACEMENT_CHAR = "%";
+
+    static final String MAVEN_POM = "pom.xml";
+    static final String ANT_PROJECT = "build.xml";
+    static final String OSGI_BUNDLE = "META-INF/MANIFEST.MF";
+
+    private static final String PATTERN = ALL_DIRECTORIES + MAVEN_POM
+            + PLUS + ALL_DIRECTORIES + ANT_PROJECT
+            + PLUS + ALL_DIRECTORIES + OSGI_BUNDLE;
+
+    /** The factory to create input streams with. */
+    private final FileInputStreamFactory factory;
+    /** Maps file names to module names. */
+    private final Map<String, String> fileNameToModuleName;
+    /** Sorted list of file name prefixes. */
+    private final List<String> prefixes;
+
+    /**
+     * Creates a new instance of {@link ModuleDetector}.
+     *
+     * @param workspace
+     *            the workspace to scan for Maven pom.xml or ant build.xml files
+     * @param fileInputStreamFactory
+     *            factory to load files
+     */
+    public ModuleDetector(final File workspace, final FileInputStreamFactory fileInputStreamFactory) {
+        factory = fileInputStreamFactory;
+        fileNameToModuleName = createFilesToModuleMapping(workspace);
+        prefixes = new ArrayList<>(fileNameToModuleName.keySet());
+        Collections.sort(prefixes);
+    }
+
+    /**
+     * Returns a mapping of path prefixes to module names.
+     *
+     * @param workspace
+     *            the workspace to start scanning for files
+     * @return the mapping of path prefixes to module names
+     */
+    private Map<String, String> createFilesToModuleMapping(final File workspace) {
+        Map<String, String> mapping = new HashMap<>();
+
+        String[] projects = find(workspace);
+        for (String fileName : projects) {
+            if (fileName.endsWith(ANT_PROJECT)) {
+                addMapping(mapping, fileName, ANT_PROJECT, parseBuildXml(fileName));
+            }
+        }
+        for (String fileName : projects) {
+            if (fileName.endsWith(MAVEN_POM)) {
+                addMapping(mapping, fileName, MAVEN_POM, parsePom(fileName));
+            }
+        }
+        for (String fileName : projects) {
+            if (fileName.endsWith(OSGI_BUNDLE)) {
+                addMapping(mapping, fileName, OSGI_BUNDLE, parseManifest(fileName));
+            }
+        }
+
+        return mapping;
+    }
+
+    private void addMapping(final Map<String, String> mapping, final String fileName, final String suffix, final String moduleName) {
+        if (StringUtils.isNotBlank(moduleName)) {
+            mapping.put(StringUtils.substringBeforeLast(fileName, suffix), moduleName);
+        }
+    }
+
+    /**
+     * Uses the path prefixes of pom.xml or build.xml files to guess a module
+     * name for the specified file.
+     *
+     * @param originalFileName
+     *            file name to guess a module for, must be an absolute path
+     * @return a module name or an empty string
+     */
+    public String guessModuleName(final String originalFileName) {
+        String fullPath = originalFileName.replace('\\', '/');
+
+        String guessedModule = StringUtils.EMPTY;
+        for (String path : prefixes) {
+            if (fullPath.startsWith(path)) {
+                guessedModule = fileNameToModuleName.get(path);
+            }
+        }
+        return guessedModule;
+    }
+
+    /**
+     * Finds files of the matching pattern.
+     *
+     * @param path
+     *            root path to scan in
+     * @return the found files (as absolute paths)
+     */
+    private String[] find(final File path) {
+        String[] relativeFileNames = factory.find(path, PATTERN);
+        String[] absoluteFileNames = new String[relativeFileNames.length];
+
+        String absolutePath = normalizePath(path.getAbsolutePath());
+        for (int file = 0; file < absoluteFileNames.length; file++) {
+            String relativePath = normalizePath(relativeFileNames[file]);
+            if (relativePath.startsWith(SLASH)) {
+                absoluteFileNames[file] = relativePath;
+            }
+            else {
+                absoluteFileNames[file] = absolutePath + SLASH + relativePath;
+            }
+        }
+        return absoluteFileNames;
+    }
+
+    private String normalizePath(final String fileName) {
+        return fileName.replace(BACK_SLASH, SLASH);
+    }
+
+    /**
+     * Returns the project name stored in the build.xml.
+     *
+     * @param buildXml
+     *            Ant build.xml file name
+     * @return the project name or an empty string if the name could not be
+     *         resolved
+     */
+    private String parseBuildXml(final String buildXml) {
+        InputStream file = null;
+        try {
+            file = factory.create(buildXml);
+            Digester digester = new Digester();
+            digester.setValidating(false);
+            digester.setClassLoader(ModuleDetector.class.getClassLoader());
+
+            digester.push(new StringBuffer());
+            String xPath = "project";
+            digester.addCallMethod(xPath, "append", 1);
+            digester.addCallParam(xPath, 0, "name");
+
+            StringBuffer result = digester.parse(file);
+            return result.toString();
+        }
+        catch (IOException | SAXException exception) {
+            // ignore
+        }
+        finally {
+            IOUtils.closeQuietly(file);
+        }
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * Returns the project name stored in the POM.
+     *
+     * @param pom
+     *            Maven POM file name
+     * @return the project name or an empty string if the name could not be
+     *         resolved
+     */
+    private String parsePom(final String pom) {
+        String name = parsePomAttribute(pom, "name");
+        return StringUtils.defaultIfBlank(name, parsePomAttribute(pom, "artifactId"));
+    }
+
+    private String parsePomAttribute(final String pom, final String tagName) {
+        InputStream file = null;
+        try {
+            file = factory.create(pom);
+            Digester digester = new Digester();
+            digester.setValidating(false);
+            digester.setClassLoader(ModuleDetector.class.getClassLoader());
+
+            digester.push(new StringBuffer());
+            digester.addCallMethod("project/" + tagName, "append", 0);
+
+            StringBuffer result = digester.parse(file);
+            return result.toString();
+        }
+        catch (IOException | SAXException exception) {
+            // ignore
+        }
+        finally {
+            IOUtils.closeQuietly(file);
+        }
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * Scans a Manifest file for OSGi Bundle Information.
+     *
+     * @param manifestFile
+     *            file name of MANIFEST.MF
+     * @return the project name or an empty string if the name could not be
+     *         resolved
+     */
+    private String parseManifest(final String manifestFile) {
+        InputStream file = null;
+        try {
+            file = factory.create(manifestFile);
+            Manifest manifest = new Manifest(file);
+            Attributes attributes = manifest.getMainAttributes();
+            Properties properties = readProperties(StringUtils.substringBefore(manifestFile, OSGI_BUNDLE));
+            String name = getLocalizedValue(attributes, properties, BUNDLE_NAME);
+            if (StringUtils.isNotBlank(name)) {
+                return name;
+            }
+            return getSymbolicName(attributes, properties);
+        }
+        catch (IOException exception) {
+            // ignore
+        }
+        finally {
+            IOUtils.closeQuietly(file);
+        }
+        return StringUtils.EMPTY;
+    }
+
+    private String getLocalizedValue(final Attributes attributes, final Properties properties, final String bundleName) {
+        String value = attributes.getValue(bundleName);
+        if (StringUtils.startsWith(StringUtils.trim(value), REPLACEMENT_CHAR)) {
+            return properties.getProperty(StringUtils.substringAfter(value, REPLACEMENT_CHAR));
+        }
+        return value;
+    }
+
+    private Properties readProperties(final String path) {
+        Properties properties = new Properties();
+        readProperties(path, properties, "plugin.properties");
+        readProperties(path, properties, "OSGI-INF/l10n/bundle.properties");
+
+        return properties;
+    }
+
+    private void readProperties(final String path, final Properties properties, final String fileName) {
+        InputStream file = null;
+        try {
+            file = factory.create(path + SLASH + fileName);
+            if (file != null) {
+                properties.load(file);
+            }
+        }
+        catch (IOException exception) {
+            // ignore if properties are not present or not readable
+        }
+        finally {
+            IOUtils.closeQuietly(file);
+        }
+    }
+
+    private String getSymbolicName(final Attributes attributes, final Properties properties) {
+        String symbolicName = StringUtils.substringBefore(attributes.getValue(BUNDLE_SYMBOLIC_NAME), ";");
+        if (StringUtils.isNotBlank(symbolicName)) {
+            String vendor = getLocalizedValue(attributes, properties, BUNDLE_VENDOR);
+            if (StringUtils.isNotBlank(vendor)) {
+                return symbolicName + " (" + vendor + ")";
+            }
+            else {
+                return symbolicName;
+            }
+        }
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * Factory to create an {@link InputStream} from an absolute filename.
+     *
+     * @author Ulli Hafner
+     */
+    public interface FileInputStreamFactory {
+        /**
+         * Creates an {@link InputStream} from the specified filename.
+         *
+         * @param fileName
+         *            the file name
+         * @return the input stream
+         * @throws FileNotFoundException
+         *             if the file could not be found
+         */
+        InputStream create(String fileName) throws FileNotFoundException;
+
+        /**
+         * Returns all file names that match the specified pattern.
+         *
+         * @param root
+         *            root directory to start the search from
+         * @param pattern
+         *            the Ant pattern to search for
+         * @return the found file names
+         */
+        String[] find(File root, String pattern);
+    }
+}
+
---
+++ src/test/java/edu/hm/hafner/analysis/ModuleDetectorTest.java
@@ -0,0 +1,270 @@
+package edu.hm.hafner.analysis;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.analysis.ModuleDetector.FileInputStreamFactory;
+import edu.hm.hafner.util.ResourceTest;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyObject;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+/**
+ *  Tests the class {@link ModuleDetector}.
+ */
+public class ModuleDetectorTest extends ResourceTest {
+    private static final String EXPECTED_OSGI_MODULE = "de.faktorlogik.prototyp";
+    private static final String MANIFEST = "MANIFEST.MF";
+    private static final String MANIFEST_NAME = "MANIFEST-NAME.MF";
+    private static final File ROOT = new File("/tmp");
+    private static final String PREFIX = normalizeRoot();
+
+    private static String normalizeRoot() {
+        return ROOT.getAbsolutePath().replace("\\", "/") + "/";
+    }
+    private static final int NO_RESULT = 0;
+    private static final String PATH_PREFIX_MAVEN = "path/to/maven";
+    private static final String PATH_PREFIX_OSGI = "path/to/osgi";
+    private static final String PATH_PREFIX_ANT = "path/to/ant";
+    private static final String EXPECTED_MAVEN_MODULE = "ADT Business Logic";
+    private static final String EXPECTED_ANT_MODULE = "checkstyle";
+
+    private ModuleDetector createDetectorUnderTest(final String fileName, final String[] workspaceScanResult) throws FileNotFoundException {
+        return createDetectorUnderTest(createFactoryMock(fileName, workspaceScanResult));
+    }
+
+    private ModuleDetector createDetectorUnderTest(final FileInputStreamFactory factory) {
+        return new ModuleDetector(ROOT, factory);
+    }
+
+    @SuppressWarnings("OBL")
+    private FileInputStreamFactory createFactoryMock(final String fileName, final String[] workspaceScanResult) throws FileNotFoundException {
+        FileInputStreamFactory factory = mock(FileInputStreamFactory.class);
+        when(factory.create(anyString()))
+                .thenReturn(read(fileName), read(fileName));
+        when(factory.find(anyObject(), anyString())).thenReturn(workspaceScanResult);
+        return factory;
+    }
+
+    private InputStream read(final String fileName) {
+        return asInputStream(fileName);
+    }
+
+    /**
+     * Checks whether we could identify OSGi modules using the module mapping.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testOsgiModules() throws FileNotFoundException {
+        FileInputStreamFactory factory = mock(FileInputStreamFactory.class);
+        when(factory.create(anyString())).thenReturn(read(MANIFEST));
+        when(factory.find(anyObject(), anyString())).thenReturn(new String[]{PATH_PREFIX_OSGI + ModuleDetector.OSGI_BUNDLE});
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        verifyModuleName(detector, EXPECTED_OSGI_MODULE, PATH_PREFIX_OSGI + "/something.txt");
+        verifyModuleName(detector, EXPECTED_OSGI_MODULE, PATH_PREFIX_OSGI + "/in/between/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, "/path/to/something.txt");
+    }
+
+    /**
+     * Checks whether we could identify OSGi modules using the module mapping.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testOsgiModulesWithVendor() throws FileNotFoundException {
+        FileInputStreamFactory factory = createFactoryMock(MANIFEST, new String[] {PATH_PREFIX_OSGI + ModuleDetector.OSGI_BUNDLE});
+        when(factory.create(anyString())).thenReturn(read(MANIFEST)).thenReturn(read("l10n.properties"));
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        String expectedName = "de.faktorlogik.prototyp (My Vendor)";
+        verifyModuleName(detector, expectedName, PATH_PREFIX_OSGI + "/something.txt");
+        verifyModuleName(detector, expectedName, PATH_PREFIX_OSGI + "/in/between/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, "/path/to/something.txt");
+    }
+
+    /**
+     * Checks whether we could identify OSGi modules using the module mapping.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testOsgiModulesWithName() throws FileNotFoundException {
+        FileInputStreamFactory factory = createFactoryMock(MANIFEST_NAME, new String[] {PATH_PREFIX_OSGI + ModuleDetector.OSGI_BUNDLE});
+        when(factory.create(anyString())).thenReturn(read(MANIFEST_NAME)).thenReturn(read("l10n.properties"));
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        String expectedName = "My Bundle";
+        verifyModuleName(detector, expectedName, PATH_PREFIX_OSGI + "/something.txt");
+        verifyModuleName(detector, expectedName, PATH_PREFIX_OSGI + "/in/between/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, "/path/to/something.txt");
+    }
+
+    /**
+     * Checks whether we could identify Maven modules using the module mapping.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testPomModules() throws FileNotFoundException {
+        ModuleDetector detector = createDetectorUnderTest(ModuleDetector.MAVEN_POM,
+                new String[] {PATH_PREFIX_MAVEN + ModuleDetector.MAVEN_POM});
+
+        verifyModuleName(detector, EXPECTED_MAVEN_MODULE, PATH_PREFIX_MAVEN + "/something.txt");
+        verifyModuleName(detector, EXPECTED_MAVEN_MODULE, PATH_PREFIX_MAVEN + "/in/between/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, "/path/to/something.txt");
+    }
+
+    /**
+     * Checks whether we could identify Maven modules using the module mapping.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testPomWithoutName() throws FileNotFoundException {
+        ModuleDetector detector = createDetectorUnderTest("no-name-pom.xml",
+                new String[] {PATH_PREFIX_MAVEN + ModuleDetector.MAVEN_POM});
+
+        String artifactId = "com.avaloq.adt.core";
+        verifyModuleName(detector, artifactId, PATH_PREFIX_MAVEN + "/something.txt");
+        verifyModuleName(detector, artifactId, PATH_PREFIX_MAVEN + "/in/between/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, "/path/to/something.txt");
+    }
+
+    private void verifyModuleName(final ModuleDetector detector, final String expectedName, final String fileName) {
+        assertThat(detector.guessModuleName(PREFIX + fileName)).isEqualTo(expectedName);
+    }
+
+    /**
+     * Checks whether we could identify Ant projects using the module mapping.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testAntModules() throws FileNotFoundException {
+        ModuleDetector detector = createDetectorUnderTest(ModuleDetector.ANT_PROJECT,
+                new String[] {PATH_PREFIX_ANT + ModuleDetector.ANT_PROJECT});
+
+        verifyModuleName(detector, EXPECTED_ANT_MODULE, PATH_PREFIX_ANT + "/something.txt");
+        verifyModuleName(detector, EXPECTED_ANT_MODULE, PATH_PREFIX_ANT + "/in/between/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, "/path/to/something.txt");
+    }
+
+    /**
+     * Checks whether we ignore exceptions during parsing.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testNoPomNameOnException() throws FileNotFoundException {
+        FileInputStreamFactory factory = createDummyFactory();
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        verifyModuleName(detector, StringUtils.EMPTY, PATH_PREFIX_ANT + "/something.txt");
+        verifyModuleName(detector, StringUtils.EMPTY, PATH_PREFIX_MAVEN + "/something.txt");
+    }
+
+    private FileInputStreamFactory createDummyFactory() throws FileNotFoundException {
+        FileInputStreamFactory factory = mock(FileInputStreamFactory.class);
+        when(factory.create(anyString())).thenThrow(new FileNotFoundException());
+        when(factory.find(anyObject(), anyString())).thenReturn(new String[NO_RESULT]);
+        return factory;
+    }
+
+    /**
+     * Checks whether we could identify a maven module.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testMoreEntries() throws FileNotFoundException {
+        String ant = PATH_PREFIX_ANT + ModuleDetector.ANT_PROJECT;
+        String maven = PATH_PREFIX_MAVEN + ModuleDetector.MAVEN_POM;
+
+        FileInputStreamFactory factory = mock(FileInputStreamFactory.class);
+        when(factory.create(PREFIX + ant)).thenReturn(read(ModuleDetector.ANT_PROJECT));
+        when(factory.create(PREFIX + maven)).thenReturn(read(ModuleDetector.MAVEN_POM), read(ModuleDetector.MAVEN_POM));
+
+        when(factory.find(anyObject(), anyString())).thenReturn(new String[] {ant, maven});
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        verifyModuleName(detector, EXPECTED_ANT_MODULE, PATH_PREFIX_ANT + "/something.txt");
+        verifyModuleName(detector, EXPECTED_MAVEN_MODULE, PATH_PREFIX_MAVEN + "/something.txt");
+    }
+
+    /**
+     * Checks whether maven has precedence over ant.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testMavenHasPrecedenceOverAnt() throws FileNotFoundException {
+        String prefix = "/prefix/";
+        String ant = prefix + ModuleDetector.ANT_PROJECT;
+        String maven = prefix + ModuleDetector.MAVEN_POM;
+
+        verifyOrder(prefix, ant, maven, new String[] {ant, maven});
+        verifyOrder(prefix, ant, maven, new String[] {maven, ant});
+    }
+
+    private void verifyOrder(final String prefix, final String ant, final String maven, final String[] foundFiles)
+            throws FileNotFoundException {
+        FileInputStreamFactory factory = mock(FileInputStreamFactory.class);
+        when(factory.create(ant)).thenReturn(read(ModuleDetector.ANT_PROJECT));
+        when(factory.create(maven)).thenReturn(read(ModuleDetector.MAVEN_POM), read(ModuleDetector.MAVEN_POM));
+
+        when(factory.find(anyObject(), anyString())).thenReturn(foundFiles);
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        assertThat(detector.guessModuleName(prefix + "/something.txt")).isEqualTo(EXPECTED_MAVEN_MODULE);
+    }
+
+    /**
+     * Checks whether OSGi has precedence over maven and ant.
+     *
+     * @throws FileNotFoundException
+     *             should never happen
+     */
+    @Test
+    public void testOsgiHasPrecedenceOvermavenAndAnt() throws FileNotFoundException {
+        String prefix = "/prefix/";
+        String ant = prefix + ModuleDetector.ANT_PROJECT;
+        String maven = prefix + ModuleDetector.MAVEN_POM;
+        String osgi = prefix + ModuleDetector.OSGI_BUNDLE;
+
+        verifyOrder(prefix, ant, maven, osgi, new String[] {ant, maven, osgi});
+        verifyOrder(prefix, ant, maven, osgi, new String[] {ant, osgi, maven});
+        verifyOrder(prefix, ant, maven, osgi, new String[] {maven, ant, osgi});
+        verifyOrder(prefix, ant, maven, osgi, new String[] {maven, osgi, ant});
+        verifyOrder(prefix, ant, maven, osgi, new String[] {osgi, ant, maven});
+        verifyOrder(prefix, ant, maven, osgi, new String[] {osgi, maven, osgi});
+    }
+
+    private void verifyOrder(final String prefix, final String ant, final String maven, final String osgi, final String[] foundFiles)
+            throws FileNotFoundException {
+        FileInputStreamFactory factory = mock(FileInputStreamFactory.class);
+        when(factory.create(ant)).thenReturn(read(ModuleDetector.ANT_PROJECT));
+        when(factory.create(maven)).thenReturn(read(ModuleDetector.MAVEN_POM), read(ModuleDetector.MAVEN_POM));
+        when(factory.create(osgi)).thenReturn(read(MANIFEST));
+
+        when(factory.find(anyObject(), anyString())).thenReturn(foundFiles);
+        ModuleDetector detector = createDetectorUnderTest(factory);
+
+        assertThat(detector.guessModuleName(prefix + "/something.txt")).isEqualTo(EXPECTED_OSGI_MODULE);
+    }
+}
