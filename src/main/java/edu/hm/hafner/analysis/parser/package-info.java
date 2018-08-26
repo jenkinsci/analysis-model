fb96114dc37b8fc2be05ddae0e61aa8ef2386852
Ulli Hafner <ullrich.hafner@gmail.com>
2018-08-27 01:15:13 +0200

    Added package-info files.

---
+++ src/main/java/edu/hm/hafner/analysis/parser/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The available parsers. For each report a different parser is provided.
+ */
+@DefaultAnnotation(NonNull.class)
+package edu.hm.hafner.analysis.parser;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
---
+++ src/main/java/edu/hm/hafner/analysis/parser/violations/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Parsers that use the violations lib (https://github.com/tomasbjerre/violations-lib).
+ */
+@DefaultAnnotation(NonNull.class)
+package edu.hm.hafner.analysis.parser.violations;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
