33b6a70c287eb7ab435b61418af5f546f24dd2ff
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-19 15:24:55 +0200

    Added YamlLint support.

---
+++ src/main/java/edu/hm/hafner/analysis/parser/violations/YamlLintAdapter.java
@@ -0,0 +1,17 @@
+package edu.hm.hafner.analysis.parser.violations;
+
+import se.bjurr.violations.lib.parsers.YAMLlintParser;
+
+/**
+ * Parses YAMLLint results files.
+ *
+ * @author Ullrich Hafner
+ */
+public class YamlLintAdapter extends AbstractViolationAdapter {
+    private static final long serialVersionUID = 1233385439193697120L;
+
+    @Override
+    protected YAMLlintParser createParser() {
+        return new YAMLlintParser();
+    }
+}
---
+++ src/test/java/edu/hm/hafner/analysis/parser/violations/YamlLintAdapterTest.java
@@ -0,0 +1,35 @@
+package edu.hm.hafner.analysis.parser.violations;
+
+import edu.hm.hafner.analysis.AbstractParser;
+import edu.hm.hafner.analysis.AbstractParserTest;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.Severity;
+import edu.hm.hafner.analysis.assertj.SoftAssertions;
+
+/**
+ * Tests the class {@link YamlLintAdapter}.
+ *
+ * @author Ullrich Hafner
+ */
+class YamlLintAdapterTest extends AbstractParserTest {
+    YamlLintAdapterTest() {
+        super("yamllint.txt");
+    }
+
+    @Override
+    protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
+        softly.assertThat(report).hasSize(4);
+        softly.assertThat(report.get(0))
+                .hasMessage("missing starting space in comment")
+                .hasFileName("file.yml")
+                .hasLineStart(6)
+                .hasColumnStart(2)
+                .hasType("comments")
+                .hasSeverity(Severity.WARNING_NORMAL);
+    }
+
+    @Override
+    protected AbstractParser createParser() {
+        return new YamlLintAdapter();
+    }
+}
