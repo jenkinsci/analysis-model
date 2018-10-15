a1da32a3f4a23f71d3f3d5cbe3a5291a43719283
Jason Faust <jfaust@tsunamit.com>
2018-10-15 16:00:25 -0400

    Updates to Eclipse Compiler output parsing
    * Support for 'INFO' at priority WARNING_LOW
    * 'ERROR' is now at priority ERROR
    * Note for how markers are counted (different from XML output)
    * Skip leading type marker for log-in-log situations
    * Allow source code to be one or more lines long
    * Account for leading tab in column pointer line
    * Correct error in test input of JENKINS-21377

--- src/test/java/edu/hm/hafner/analysis/parser/EclipseParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/EclipseParserTest.java
@@ -58,8 +58,8 @@ public class EclipseParserTest extends AbstractIssueParserTest {
                     .hasSeverity(Severity.WARNING_NORMAL)
                     .hasLineStart(13)
                     .hasLineEnd(13)
-                    .hasColumnStart(15)
-                    .hasColumnEnd(15 + 13)
+                    .hasColumnStart(11)
+                    .hasColumnEnd(11 + 13)
                     .hasMessage("The method getOldValue() from the type SomeType is deprecated")
                     .hasFileName("/path/to/job/job-name/module/src/main/java/com/example/Example.java");
         });
@@ -174,5 +174,73 @@ public class EclipseParserTest extends AbstractIssueParserTest {
             number++;
         }
     }
+
+    /**
+     * Test for the info log level for the eclipse compiler.
+     */
+    @Test
+    void infoLogLevel() {
+        Report report = parse("eclipse-withinfo.txt");
+
+        assertThat(report).hasSize(6);
+
+        assertSoftly(softly -> {
+            softly.assertThat(report.get(0))
+                    .hasSeverity(Severity.ERROR)
+                    .hasLineStart(8)
+                    .hasLineEnd(8)
+                    .hasColumnStart(13)
+                    .hasColumnEnd(17)
+                    .hasFileName("C:/devenv/workspace/x/y/src/main/java/y/ECE.java")
+                    .hasMessage("Type mismatch: cannot convert from float to Integer");
+
+            softly.assertThat(report.get(1))
+                    .hasSeverity(Severity.ERROR)
+                    .hasLineStart(16)
+                    .hasLineEnd(16)
+                    .hasColumnStart(8)
+                    .hasColumnEnd(41)
+                    .hasFileName("C:/devenv/workspace/x/y/src/main/java/y/ECE.java")
+                    .hasMessage("Dead code");
+
+            softly.assertThat(report.get(2))
+                    .hasSeverity(Severity.WARNING_NORMAL)
+                    .hasLineStart(22)
+                    .hasLineEnd(22)
+                    .hasColumnStart(9)
+                    .hasColumnEnd(10)
+                    .hasFileName("C:/devenv/workspace/x/y/src/main/java/y/ECE.java")
+                    .hasMessage("The value of the local variable x is not used");
+
+            softly.assertThat(report.get(3))
+                    .hasSeverity(Severity.WARNING_NORMAL)
+                    .hasLineStart(27)
+                    .hasLineEnd(27)
+                    .hasColumnStart(8)
+                    .hasColumnEnd(41)
+                    .hasFileName("C:/devenv/workspace/x/y/src/main/java/y/ECE.java")
+                    .hasMessage(
+                            "Statement unnecessarily nested within else clause. The corresponding then clause does not complete normally");
+
+            softly.assertThat(report.get(4))
+                    .hasSeverity(Severity.WARNING_LOW)
+                    .hasLineStart(33)
+                    .hasLineEnd(33)
+                    .hasColumnStart(13)
+                    .hasColumnEnd(19)
+                    .hasFileName("C:/devenv/workspace/x/y/src/main/java/y/ECE.java")
+                    .hasMessage("Comparing identical expressions");
+
+            softly.assertThat(report.get(5))
+                    .hasSeverity(Severity.WARNING_LOW)
+                    .hasLineStart(35)
+                    .hasLineEnd(35)
+                    .hasColumnStart(1)
+                    .hasColumnEnd(96)
+                    .hasFileName("C:/devenv/workspace/x/y/src/main/java/y/ECE.java")
+                    .hasMessage("The allocated object is never used");
+        });
+    }
+
 }
 
