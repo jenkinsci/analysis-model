c5ca9a70cc6869379e393b45f813d5d58ee0fa40
Ulli Hafner <ullrich.hafner@gmail.com>
2018-12-04 22:43:16 +0100

    Added JavaDoc.

--- src/main/java/edu/hm/hafner/analysis/ReaderFactory.java
+++ src/main/java/edu/hm/hafner/analysis/ReaderFactory.java
@@ -37,6 +37,14 @@ public class ReaderFactory {
     private final Charset charset;
     private final String fileName;
 
+    /**
+     * Creates a new factory to read the specified file with a given charset.
+     *
+     * @param file
+     *         the file to open
+     * @param charset
+     *         the charset to use when reading the file
+     */
     public ReaderFactory(final Path file, final @CheckForNull Charset charset) {
         this.file = file;
         this.charset = charset;
@@ -44,11 +52,39 @@ public class ReaderFactory {
         fileName = file.toAbsolutePath().toString().replace('\\', '/');
     }
 
+    /**
+     * Creates a new {@link Reader} for the file.
+     *
+     * @return a reader
+     */
+    @MustBeClosed
+    public Reader create() {
+        try (InputStream inputStream = Files.newInputStream(file)) {
+            return createReader(inputStream);
+        }
+        catch (FileNotFoundException exception) {
+            throw new ParsingException(exception, "Can't find file: " + fileName);
+        }
+        catch (IOException | UncheckedIOException exception) {
+            throw new ParsingException(exception, "Can't scan file for issues: " + fileName);
+        }
+    }
+
+    /**
+     * Creates a new {@link BufferedReader} for the file.
+     * 
+     * @return a buffered reader
+     */
     @MustBeClosed
     public BufferedReader createBufferedReader() {
         return new BufferedReader(create());
     }
 
+    /**
+     * Creates a new {@link InputSource} for the file.
+     *
+     * @return a buffered reader
+     */
     @MustBeClosed
     public InputSource createInputSource() {
         return new InputSource(new ReaderInputStream(create(), getCharset()));
@@ -58,6 +94,12 @@ public class ReaderFactory {
         return ObjectUtils.defaultIfNull(charset, StandardCharsets.UTF_8);
     }
 
+    /**
+     * Reads the whole file into a {@link String}.
+     *
+     * @return the file content as string
+     * @throws ParsingException if the file could not be read
+     */
     public String readString() {
         try {
             return IOUtils.toString(create());
@@ -67,6 +109,12 @@ public class ReaderFactory {
         }
     }
 
+    /**
+     * Parses the whole file into a {@link Document}.
+     *
+     * @return the file content as document
+     * @throws ParsingException if the file could not be parsed
+     */
     public Document readDocument() {
         try {
             DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
@@ -78,23 +126,15 @@ public class ReaderFactory {
         }
     }
 
-    @MustBeClosed
-    public Reader create() {
-        try (InputStream inputStream = Files.newInputStream(file)) {
-            return createReader(inputStream, charset);
-        }
-        catch (FileNotFoundException exception) {
-            throw new ParsingException(exception, "Can't find file: " + fileName);
-        }
-        catch (IOException | UncheckedIOException exception) {
-            throw new ParsingException(exception, "Can't scan file for issues: " + fileName);
-        }
-    }
-
-    private Reader createReader(final InputStream inputStream, final Charset charset) {
+    private Reader createReader(final InputStream inputStream) {
         return new InputStreamReader(new BOMInputStream(inputStream), charset);
     }
 
+    /**
+     * Returns the absolute path of the file. The file name uses UNIX path separators.
+     * 
+     * @return the file name
+     */
     public String getFileName() {
         return fileName;
     }
