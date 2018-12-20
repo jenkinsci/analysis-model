01f15f1c8f47b2f9a3b582d6aacc6c018922c1d2
Ulli Hafner <ullrich.hafner@gmail.com>
2018-12-20 23:54:21 +0100

    Moved ModuleResolver from warnings plugin to analysis-model.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,12 +4,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased](https://github.com/jenkinsci/analysis-model/compare/v1.1.0...HEAD)
+
+## [1.1.0](https://github.com/jenkinsci/analysis-model/compare/v1.0.0...v1.1.0) - 2018-12-20
+
+### Added
+- Added ModuleResolver from Jenkins warnings plugin.
+
 ## 1.0.0 - 2018-12-20
 
 First public release.
 
 <!---
-## Unreleased
 ## 1.0.0 - year-month-day
 ### Added
 - One 
@@ -35,9 +41,5 @@ First public release.
 - One 
 - Two 
 
-# Git Log
-
-[Unreleased]: https://github.com/olivierlacan/keep-a-changelog/compare/v1.0.0...HEAD
-[1.0.0]: https://github.com/olivierlacan/keep-a-changelog/compare/v0.3.0...v1.0.0
 
 -->
---
+++ src/test/java/edu/hm/hafner/analysis/ModuleResolverTest.java
@@ -0,0 +1,41 @@
+package edu.hm.hafner.analysis;
+
+import org.junit.jupiter.api.Test;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import static edu.hm.hafner.analysis.assertj.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests the class {@link ModuleResolver}.
+ *
+ * @author Ullrich Hafner
+ */
+class ModuleResolverTest {
+    @Test
+    @SuppressFBWarnings("DMI")
+    void shouldAssignModuleName() {
+        Report report = new Report();
+        IssueBuilder builder = new IssueBuilder();
+        String fileName = "/file/with/warnings.txt";
+        builder.setFileName(fileName);
+        Issue noModule = builder.build();
+        report.add(noModule);
+
+        builder.setModuleName("module2");
+        Issue withModule = builder.build();
+        report.add(withModule);
+
+        ModuleDetector detector = mock(ModuleDetector.class);
+        when(detector.guessModuleName(fileName)).thenReturn("module1");
+
+        ModuleResolver resolver = new ModuleResolver();
+        resolver.run(report, detector);
+
+        assertThat(report.get(0)).hasModuleName("module1");
+        assertThat(report.get(1)).hasModuleName("module2");
+
+        assertThat(report.getInfoMessages()).contains("-> resolved module names for 1 issues");
+    }
+}
