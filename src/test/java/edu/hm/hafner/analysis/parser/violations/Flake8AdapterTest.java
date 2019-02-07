881b1cf1c365817e2bd571bc35d6e9095932aadc
Ulli Hafner <ullrich.hafner@gmail.com>
2019-02-07 13:33:21 +0100

    Added a test for flake 8 that exposes the './' prefix problem.

--- src/test/java/edu/hm/hafner/analysis/parser/violations/Flake8AdapterTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/violations/Flake8AdapterTest.java
@@ -1,12 +1,14 @@
 package edu.hm.hafner.analysis.parser.violations;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import edu.hm.hafner.analysis.AbstractParserTest;
 import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
-import static edu.hm.hafner.analysis.assertj.Assertions.*;
 import edu.hm.hafner.analysis.assertj.SoftAssertions;
+
+import static edu.hm.hafner.analysis.assertj.Assertions.*;
 
 /**
  * Tests the class {@link Flake8Adapter}.
@@ -49,5 +51,24 @@ class Flake8AdapterTest extends AbstractParserTest {
                 .hasColumnStart(1)
                 .hasType("E302")
                 .hasMessage("expected 2 blank lines, found 1");
+    }
+
+    @Test @Disabled("See issue https://github.com/tomasbjerre/violations-lib/issues/")
+    void shouldParseWithCurrentDirPrefix() {
+        Report report = parse("flake8.log");
+
+        assertThat(report).hasSize(2);
+        SoftAssertions.assertSoftly((softly) -> {
+            softly.assertThat(report.get(0)).hasFileName("./src/init.py")
+                    .hasLineStart(66)
+                    .hasColumnStart(121)
+                    .hasType("E501")
+                    .hasMessage("line too long (143 > 120 characters)");
+            softly.assertThat(report.get(1)).hasFileName("./src/init.py")
+                    .hasLineStart(254)
+                    .hasColumnStart(58)
+                    .hasType("W292")
+                    .hasMessage("no newline at end of file");
+        });
     }
 }
