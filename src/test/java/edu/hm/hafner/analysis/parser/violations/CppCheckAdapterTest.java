d0916118c7f43e602aa291f1ded31708c2265fbd
Ulli Hafner <ullrich.hafner@gmail.com>
2019-02-23 22:09:14 +0100

    [JENKINS-55733] Added a test case that reproduces the bug.

--- src/test/java/edu/hm/hafner/analysis/parser/violations/CppCheckAdapterTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/violations/CppCheckAdapterTest.java
@@ -1,9 +1,14 @@
 package edu.hm.hafner.analysis.parser.violations;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import edu.hm.hafner.analysis.AbstractParserTest;
 import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
 import edu.hm.hafner.analysis.assertj.SoftAssertions;
+
+import static edu.hm.hafner.analysis.assertj.Assertions.*;
 
 /**
  * Tests the class {@link CppCheckAdapter}.
@@ -19,14 +24,15 @@ class CppCheckAdapterTest extends AbstractParserTest {
     protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
         softly.assertThat(report).hasSize(3);
         softly.assertThat(report.get(0))
-                .hasMessage("The scope of the variable 'i' can be reduced. Warning: It can be unsafe to fix this message. "
-                        + "Be careful. Especially when there are inner loops. Here is an example where cppcheck will "
-                        + "write that the scope for 'i' can be reduced:&#xa;void f(int x)&#xa;{&#xa;    int i = 0;&#xa;    "
-                        + "if (x) {&#xa;        // it's safe to move 'int i = 0' here&#xa;        "
-                        + "for (int n = 0; n < 10; ++n) {&#xa;            "
-                        + "// it is possible but not safe to move 'int i = 0' here&#xa;            "
-                        + "do_something(&i);&#xa;        }&#xa;    }&#xa;}&#xa;"
-                        + "When you see this message it is always safe to reduce the variable scope 1 level.")
+                .hasMessage(
+                        "The scope of the variable 'i' can be reduced. Warning: It can be unsafe to fix this message. "
+                                + "Be careful. Especially when there are inner loops. Here is an example where cppcheck will "
+                                + "write that the scope for 'i' can be reduced:&#xa;void f(int x)&#xa;{&#xa;    int i = 0;&#xa;    "
+                                + "if (x) {&#xa;        // it's safe to move 'int i = 0' here&#xa;        "
+                                + "for (int n = 0; n < 10; ++n) {&#xa;            "
+                                + "// it is possible but not safe to move 'int i = 0' here&#xa;            "
+                                + "do_something(&i);&#xa;        }&#xa;    }&#xa;}&#xa;"
+                                + "When you see this message it is always safe to reduce the variable scope 1 level.")
                 .hasFileName("api.c")
                 .hasType("variableScope")
                 .hasLineStart(498)
@@ -44,6 +50,20 @@ class CppCheckAdapterTest extends AbstractParserTest {
                 .hasType("variableScope")
                 .hasLineStart(104)
                 .hasSeverity(Severity.WARNING_HIGH);
+    }
+
+    /** Verifies that the parser finds multiple locations (line ranges) for a given warning. */
+    @Test @Disabled("see JENKINS-55733")
+    void shouldFindMultipleLocations() {
+        Report report = parse("issue55733.xml");
+
+        assertThat(report).hasSize(2);
+
+        assertThat(report.get(0)).hasFileName(
+                "apps/cloud_composer/src/point_selectors/rectangular_frustum_selector.cpp")
+                .hasLineStart(53)
+                .hasMessage("Variable 'it' is reassigned a value before the old one has been used.")
+                .hasType("redundantAssignment");
     }
 
     @Override
