0d5ebdacbf7fa910baa87f823fbbb078e1896f4b
Ulli Hafner <ullrich.hafner@gmail.com>
2019-04-21 18:35:32 +0200

    Fixed PMD warnings - enabled Unused Imports check again.

--- src/main/java/edu/hm/hafner/analysis/parser/YuiCompressorParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/YuiCompressorParser.java
@@ -7,9 +7,7 @@ import java.util.regex.Pattern;
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueBuilder;
 import edu.hm.hafner.analysis.LookaheadParser;
-import edu.hm.hafner.analysis.ParsingException;
 import edu.hm.hafner.analysis.Severity;
-import edu.hm.hafner.analysis.RegexpDocumentParser;
 import edu.hm.hafner.util.LookaheadStream;
 
 /**
