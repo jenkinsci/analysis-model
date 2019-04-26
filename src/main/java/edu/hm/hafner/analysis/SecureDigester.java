be6000ff3221a847dc62f08f2903bcd19cbe129e
Ulli Hafner <ullrich.hafner@gmail.com>
2019-04-26 15:01:59 +0200

    [FIXED JENKINS-57192] Disable entity resolving for Digester as well.

--- src/main/java/edu/hm/hafner/analysis/SecureDigester.java
+++ src/main/java/edu/hm/hafner/analysis/SecureDigester.java
@@ -1,9 +1,13 @@
 package edu.hm.hafner.analysis;
 
+import javax.xml.parsers.SAXParserFactory;
+
 import org.apache.commons.digester3.Digester;
 import org.xml.sax.InputSource;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import static org.apache.xerces.impl.Constants.*;
 
 /**
  * A secure {@link Digester} implementation that does not resolve external entities.
@@ -20,16 +24,27 @@ public final class SecureDigester extends Digester {
         super();
 
         setClassLoader(classWithClassLoader.getClassLoader());
+
+        SAXParserFactory factory = getFactory();
+        setFeature(factory, XERCES_FEATURE_PREFIX, CREATE_ENTITY_REF_NODES_FEATURE, false);
+        setFeature(factory, XERCES_FEATURE_PREFIX, LOAD_DTD_GRAMMAR_FEATURE, false);
+        setFeature(factory, XERCES_FEATURE_PREFIX, LOAD_EXTERNAL_DTD_FEATURE, false);
+        setFeature(factory, SAX_FEATURE_PREFIX, DISALLOW_DOCTYPE_DECL_FEATURE, true);
+        setFeature(factory, SAX_FEATURE_PREFIX, EXTERNAL_GENERAL_ENTITIES_FEATURE, false);
+        setFeature(factory, SAX_FEATURE_PREFIX, EXTERNAL_PARAMETER_ENTITIES_FEATURE, false);
+        setFeature(factory, SAX_FEATURE_PREFIX, RESOLVE_DTD_URIS_FEATURE, false);
+        setFeature(factory, SAX_FEATURE_PREFIX, USE_ENTITY_RESOLVER2_FEATURE, false);
+        factory.setXIncludeAware(false);
         setValidating(false);
-        disableFeature("external-general-entities");
-        disableFeature("external-parameter-entities");
         setEntityResolver((publicId, systemId) -> new InputSource());
     }
 
-    @SuppressFBWarnings @SuppressWarnings("illegalcatch")
-    private void disableFeature(final String feature) {
+    @SuppressFBWarnings
+    @SuppressWarnings("illegalcatch")
+    private void setFeature(final SAXParserFactory factory, final String prefix, final String feature,
+            final boolean value) {
         try {
-            setFeature("http://xml.org/sax/features/" + feature, false);
+            factory.setFeature(prefix + feature, value);
         }
         catch (Exception ignored) {
             // ignore and continue
