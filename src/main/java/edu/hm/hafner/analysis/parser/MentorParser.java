521b702a3c9f1aa253341953139799b8465bc85f
Derrick Gibelyou <derrickgw@users.noreply.github.com>
2019-04-26 10:07:23 -0600

    Address code review changes.

--- src/main/java/edu/hm/hafner/analysis/parser/MentorParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/MentorParser.java
@@ -19,26 +19,25 @@ import java.util.regex.Pattern;
 public class MentorParser extends LookaheadParser {
 
     /**
-     * Matches the beginning of a Modelsim questa message "** [priority] : [The remainder of the message]".
-     * The first capture group is the priority, the second capture group is the rest of the message to be parsed later.
+     * Matches the beginning of a Modelsim/Questa message "** [priority] : [The remainder of the message]".
      */
-    private static final String MSG_REGEX = "\\*\\*\\s+(\\w+):\\s+(.*)";
+    private static final String MSG_REGEX = "\\*\\*\\s+(?<priority>\\w+):\\s+(?<message>.*)";
 
     /**
      * The first capture group captures the message type such as "vlog-###" or "vsim-###".
-     * The second group matches an optional filename in the form of filename.v(line-number).
+     * The second group matches an optional filename in the form of filename.sv(line-number).
      * The third group matches the rest of the message.
      */
-    private static final String VSIM_REGEX = "\\((v\\w+-\\d+)\\)(?: (\\S*)\\((\\d+)\\):)? (.*)";
-    private static final Pattern VSIM_PATTERN = Pattern.compile(VSIM_REGEX);
+    private static final Pattern VSIM_PATTERN = Pattern.compile(
+            "\\((?<category>v\\w+-\\d+)\\)(?: (?<filename>\\S*)\\((?<line>\\d+)\\):)? (?<message>.*)");
 
     /**
      * The first capture group matches the filename and line number.
      * The second capture group matches the warning type such as "vlog-###" or "vsim-###".
      * The third group matches the rest of the message.
      */
-    private static final String VLOG_REGEX = "at (\\S*)\\((\\d+)\\): \\((v\\w+-\\d+)\\) (.*)";
-    private static final Pattern VLOG_PATTERN = Pattern.compile(VLOG_REGEX);
+    private static final Pattern VLOG_PATTERN = Pattern.compile(
+            "at (?<filename>\\S*)\\((?<line>\\d+)\\): \\((?<category>v\\w+-\\d+)\\) (?<message>.*)");
     
     /**
      * The first capture group matches the timestamp for the message on the previous line.
@@ -47,8 +46,8 @@ public class MentorParser extends LookaheadParser {
      * The next capture group captures the path of the module.
      * The next capture groups capture the File name and Line number.
      */
-    private static final String TIME_FILE_REGEX = "# {4}Time: (\\d* \\ws)(?: {2}Iteration: \\d+)? {2}\\w*: (.\\S*)(?: File: (\\S+)(?: Line: (\\d+))?)?.*";
-    private static final Pattern TIME_FILE_PATTERN = Pattern.compile(TIME_FILE_REGEX);
+    private static final Pattern TIME_FILE_PATTERN = Pattern.compile(
+            "# {4}Time: (?<simtime>\\d* \\ws)(?: {2}Iteration: \\d+)? {2}\\w*: (?<module>.\\S*)(?: File: (?<filename>\\S+)(?: Line: (?<line>\\d+))?)?.*");
 
     
     /**
@@ -61,60 +60,78 @@ public class MentorParser extends LookaheadParser {
     @Override
     protected Optional<Issue> createIssue(final Matcher matcher, final LookaheadStream lookahead,
                                           final IssueBuilder builder) {
+        clearBuilder(builder);
 
+        builder.setSeverity(mapPriority(matcher.group("priority")));
+
+        String message = matcher.group("message");
+        if (message.contains("while parsing file")) {
+            parseVlogMessage(lookahead, builder, message);
+        } else {
+            parseVsimMessage(lookahead, builder, message);
+        }
+
+        return builder.buildOptional();
+    }
+
+    private void parseVlogMessage(LookaheadStream lookahead, IssueBuilder builder, String message) {
+        while (!lookahead.peekNext().startsWith("# ** at ")) {
+            lookahead.next();
+        }
+
+        // If we can refine the message, the do.
+        // Else just return what we got passed.
+        Matcher vlog = VLOG_PATTERN.matcher(lookahead.next().substring(5));
+        if (vlog.matches()) {
+            builder.setFileName(vlog.group("filename"));
+            builder.setLineStart(vlog.group("line"));
+            builder.setCategory(vlog.group("category"));
+            message = vlog.group("message");
+        }
+        builder.setDescription("");
+        builder.setMessage(message);
+    }
+
+    private void parseVsimMessage(LookaheadStream lookahead, IssueBuilder builder, String message) {
+        builder.setDescription(parseSimTime(lookahead, builder));
+
+        // If we can refine the message, the do.
+        // Else just return what we got passed.
+        Matcher vsim = VSIM_PATTERN.matcher(message);
+        if (vsim.matches()) {
+            builder.setCategory(vsim.group("category"));
+            builder.setFileName(vsim.group("filename"));
+            builder.setLineStart(vsim.group("line"));
+            message = vsim.group("message");
+        }
+        builder.setMessage(message);
+    }
+
+    private String parseSimTime(LookaheadStream lookahead, IssueBuilder builder) {
+        StringBuilder description = new StringBuilder();
+        String timeLine = "";
+        while (lookahead.hasNext() && !lookahead.peekNext().contains("# **")) {
+            timeLine = lookahead.next();
+            if (timeLine.startsWith("#    Time:")) {
+                break;
+            }
+            description.append("<br>");
+            description.append(timeLine);
+        }
+        Matcher tf = TIME_FILE_PATTERN.matcher(timeLine);
+        if (tf.matches()) {
+            builder.setModuleName(tf.group("module"));
+            builder.setFileName(tf.group("filename"));
+            builder.setLineStart(tf.group("line"));
+        }
+        return description.toString();
+    }
+
+    private void clearBuilder(IssueBuilder builder) {
         builder.setModuleName(null);
         builder.setFileName(null);
         builder.setLineStart(null);
         builder.setCategory(null);
-
-        String priority = matcher.group(1);
-        String message = matcher.group(2);
-
-        StringBuilder description = new StringBuilder();
-        String timeLine = "";
-
-        if (message.contains("while parsing file")) {
-            while (!lookahead.peekNext().startsWith("# ** at ")) {
-                lookahead.next();
-            }
-            Matcher at = VLOG_PATTERN.matcher(lookahead.next().substring(5));
-            if (at.matches()) {
-                builder.setFileName(at.group(1));
-                builder.setLineStart(at.group(2));
-                builder.setCategory(at.group(3));
-                message = at.group(4);
-            }
-
-        }
-        else {
-            while (lookahead.hasNext() && !lookahead.peekNext().contains("# **")) {
-                timeLine = lookahead.next();
-                if (timeLine.startsWith("#    Time:")) {
-                    break;
-                }
-                description.append("<br>");
-                description.append(timeLine);
-            }
-            Matcher tf = TIME_FILE_PATTERN.matcher(timeLine);
-            if (tf.matches()) {
-                builder.setModuleName(tf.group(2));
-                builder.setFileName(tf.group(3));
-                builder.setLineStart(tf.group(4));
-            }
-            Matcher vsim = VSIM_PATTERN.matcher(message);
-            if (vsim.matches()) {
-                builder.setCategory(vsim.group(1));
-                builder.setFileName(vsim.group(2));
-                builder.setLineStart(vsim.group(3));
-                message = vsim.group(4);
-            }
-        }
-
-        builder.setMessage(message);
-        builder.setDescription(description.toString());
-        builder.setOrigin("Modelsim");
-        builder.setSeverity(mapPriority(priority));
-        return builder.buildOptional();
     }
 
     @Override
@@ -128,14 +145,15 @@ public class MentorParser extends LookaheadParser {
             case "Error":
                 severity = Severity.ERROR;
                 break;
+            case "Critical Warning":
+                severity = Severity.WARNING_HIGH;
+                break;
             case "Warning":
                 severity = Severity.WARNING_NORMAL;
                 break;
             case "Note":
-                severity = Severity.WARNING_LOW;
-                break;
             default:
-                severity = Severity.WARNING_HIGH;
+                severity = Severity.WARNING_LOW;
                 break;
         }
         return severity;
