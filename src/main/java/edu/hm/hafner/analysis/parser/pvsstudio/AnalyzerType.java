7bedcee4295d9b0a7bea3f9eac5627bdd28f7ca3
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-07-17 12:58:41 +0300

    Fixed some errors and warning step 3

--- src/main/java/edu/hm/hafner/analysis/parser/pvsstudio/AnalyzerType.java
+++ src/main/java/edu/hm/hafner/analysis/parser/pvsstudio/AnalyzerType.java
@@ -14,95 +14,94 @@ public enum AnalyzerType {
     UNKNOWN;
 
     /**
-     * Diagnosis of 64-bit errors (Viva64, C++)
+     * Diagnosis of 64-bit errors (Viva64, C++).
      * https://www.viva64.com/en/w/#64CPP
      */
     public static final int VIVA64_CCPP_ERRORCODE_BEGIN = 100;
     /**
-     * Diagnosis of 64-bit errors (Viva64, C++)
+     * Diagnosis of 64-bit errors (Viva64, C++).
      * https://www.viva64.com/en/w/#64CPP
      */
     public static final int VIVA64_CCPP_ERRORCODE_END = 499;
-
     /**
-     * General Analysis (C++)
+     * General Analysis (C++).
      * https://www.viva64.com/en/w/#GeneralAnalysisCPP
      */
     public static final int GENERAL_CCPP_LOW_ERRORCODE_BEGIN = 500;
     /**
-     * General Analysis (C++)
+     * General Analysis (C++).
      * https://www.viva64.com/en/w/#GeneralAnalysisCPP
      */
     public static final int GENERAL_CCPP_LOW_ERRORCODE_END = 799;
 
     /**
-     * Diagnosis of micro-optimizations (C++)
+     * Diagnosis of micro-optimizations (C++).
      * https://www.viva64.com/en/w/#MicroOptimizationsCPP
      */
     public static final int OPTIMIZATION_CCPP_ERRORCODE_BEGIN = 800;
     /**
-     * Diagnosis of micro-optimizations (C++)
+     * Diagnosis of micro-optimizations (C++).
      * https://www.viva64.com/en/w/#MicroOptimizationsCPP
      */
     public static final int OPTIMIZATION_CCPP_ERRORCODE_END = 999;
-
     /**
-     * General Analysis (C++)
+     * General Analysis (C++).
      * https://www.viva64.com/en/w/
      */
     public static final int GENERAL_CCPP_HIGH_ERRORCODE_BEGIN = 1000;
     /**
-     * General Analysis (C++)
+     * General Analysis (C++).
      * https://www.viva64.com/en/w/
      */
     public static final int GENERAL_CCPP_HIGH_ERRORCODE_END = 1999;
-
     /**
-     * Customers Specific Requests (C++)
+     * Customers Specific Requests (C++).
      * https://www.viva64.com/en/w/#CustomersSpecificRequestsCPP
      */
     public static final int CUSTOMERSPECIFIC_CCPP_ERRORCODE_BEGIN = 2000;
     /**
-     * Customers Specific Requests (C++)
+     * Customers Specific Requests (C++).
      * https://www.viva64.com/en/w/#CustomersSpecificRequestsCPP
      */
     public static final int CUSTOMERSPECIFIC_CCPP_ERRORCODE_END = 2499;
-
     /**
-     * MISRA errors
+     * MISRA errors.
      * https://www.viva64.com/en/w/#MISRA
      */
     public static final int MISRA_CCPP_ERRORCODE_BEGIN = 2500;
     /**
-     * MISRA errors
+     * MISRA errors.
      * https://www.viva64.com/en/w/#MISRA
      */
     public static final int MISRA_CCPP_ERRORCODE_END = 2999;
-
     /**
-     * General Analysis (C#)
+     * General Analysis (C#).
      * https://www.viva64.com/en/w/#GeneralAnalysisCS
      */
     public static final int GENERAL_CS_ERRORCODE_BEGIN = 3000;
     /**
-     * General Analysis (C#)
+     * General Analysis (C#).
      * https://www.viva64.com/en/w/#GeneralAnalysisCS
      */
     public static final int GENERAL_CS_ERRORCODE_END = 3499;
 
     /**
-     * General Analysis (Java)
+     * General Analysis (Java).
      * https://www.viva64.com/en/w/#GeneralAnalysisJAVA
      */
     public static final int GENERAL_JAVA_ERRORCODE_BEGIN = 6000;
     /**
-     * General Analysis (Java)
+     * General Analysis (Java).
      * https://www.viva64.com/en/w/#GeneralAnalysisJAVA
      */
     public static final int GENERAL_JAVA_ERRORCODE_END = 6999;
 
     // errorCode is Vnnn
     // from DataTableConsts.cs
+    /**
+     * Get analayzer by error code
+     */
+    @SuppressWarnings("PMD")
     public static AnalyzerType getAnalyzerType(final String errorCodeStr)
     {
         if (errorCodeStr == null ||  errorCodeStr.length() <= 1)
@@ -115,36 +114,28 @@ public enum AnalyzerType {
 
             int errorCode = Integer.parseInt(sub);
 
-            if (errorCode >= VIVA64_CCPP_ERRORCODE_BEGIN && errorCode <= VIVA64_CCPP_ERRORCODE_END)
-            {
+            if (errorCode >= VIVA64_CCPP_ERRORCODE_BEGIN && errorCode <= VIVA64_CCPP_ERRORCODE_END) {
                 return AnalyzerType.VIVA_64;
             }
-            else if (errorCode >= GENERAL_CCPP_LOW_ERRORCODE_BEGIN && errorCode <= GENERAL_CCPP_LOW_ERRORCODE_END)
-            {
+            else if (errorCode >= GENERAL_CCPP_LOW_ERRORCODE_BEGIN && errorCode <= GENERAL_CCPP_LOW_ERRORCODE_END) {
                 return AnalyzerType.GENERAL;
             }
-            else if (errorCode >= OPTIMIZATION_CCPP_ERRORCODE_BEGIN && errorCode <= OPTIMIZATION_CCPP_ERRORCODE_END)
-            {
+            else if (errorCode >= OPTIMIZATION_CCPP_ERRORCODE_BEGIN && errorCode <= OPTIMIZATION_CCPP_ERRORCODE_END) {
                 return AnalyzerType.OPTIMIZATION;
             }
-            else if (errorCode >= GENERAL_CCPP_HIGH_ERRORCODE_BEGIN && errorCode <= GENERAL_CCPP_HIGH_ERRORCODE_END)
-            {
+            else if (errorCode >= GENERAL_CCPP_HIGH_ERRORCODE_BEGIN && errorCode <= GENERAL_CCPP_HIGH_ERRORCODE_END) {
                 return AnalyzerType.GENERAL;
             }
-            else if (errorCode >= CUSTOMERSPECIFIC_CCPP_ERRORCODE_BEGIN && errorCode <= CUSTOMERSPECIFIC_CCPP_ERRORCODE_END)
-            {
+            else if (errorCode >= CUSTOMERSPECIFIC_CCPP_ERRORCODE_BEGIN && errorCode <= CUSTOMERSPECIFIC_CCPP_ERRORCODE_END) {
                 return AnalyzerType.CUSTOMER_SPECIFIC;
             }
-            else if (errorCode >= MISRA_CCPP_ERRORCODE_BEGIN && errorCode <= MISRA_CCPP_ERRORCODE_END)
-            {
+            else if (errorCode >= MISRA_CCPP_ERRORCODE_BEGIN && errorCode <= MISRA_CCPP_ERRORCODE_END) {
                 return AnalyzerType.MISRA;
             }
-            else if (errorCode >= GENERAL_CS_ERRORCODE_BEGIN && errorCode <= GENERAL_CS_ERRORCODE_END)
-            {
+            else if (errorCode >= GENERAL_CS_ERRORCODE_BEGIN && errorCode <= GENERAL_CS_ERRORCODE_END) {
                 return AnalyzerType.GENERAL;
             }
-            else if (errorCode >= GENERAL_JAVA_ERRORCODE_BEGIN && errorCode <= GENERAL_JAVA_ERRORCODE_END)
-            {
+            else if (errorCode >= GENERAL_JAVA_ERRORCODE_BEGIN && errorCode <= GENERAL_JAVA_ERRORCODE_END) {
                 return AnalyzerType.GENERAL;
             }
         }
