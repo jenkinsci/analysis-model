16ff1bce5a8dc9f931e188e673d8b73c0b24fe28
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-07-17 12:38:51 +0300

    Fixed some errors and warning step 2

--- src/main/java/edu/hm/hafner/analysis/parser/pvsstudio/AnalyzerType.java
+++ src/main/java/edu/hm/hafner/analysis/parser/pvsstudio/AnalyzerType.java
@@ -13,28 +13,92 @@ public enum AnalyzerType {
     MISRA,
     UNKNOWN;
 
+    /**
+     * Diagnosis of 64-bit errors (Viva64, C++)
+     * https://www.viva64.com/en/w/#64CPP
+     */
     public static final int VIVA64_CCPP_ERRORCODE_BEGIN = 100;
+    /**
+     * Diagnosis of 64-bit errors (Viva64, C++)
+     * https://www.viva64.com/en/w/#64CPP
+     */
     public static final int VIVA64_CCPP_ERRORCODE_END = 499;
 
+    /**
+     * General Analysis (C++)
+     * https://www.viva64.com/en/w/#GeneralAnalysisCPP
+     */
     public static final int GENERAL_CCPP_LOW_ERRORCODE_BEGIN = 500;
+    /**
+     * General Analysis (C++)
+     * https://www.viva64.com/en/w/#GeneralAnalysisCPP
+     */
     public static final int GENERAL_CCPP_LOW_ERRORCODE_END = 799;
 
+    /**
+     * Diagnosis of micro-optimizations (C++)
+     * https://www.viva64.com/en/w/#MicroOptimizationsCPP
+     */
     public static final int OPTIMIZATION_CCPP_ERRORCODE_BEGIN = 800;
+    /**
+     * Diagnosis of micro-optimizations (C++)
+     * https://www.viva64.com/en/w/#MicroOptimizationsCPP
+     */
     public static final int OPTIMIZATION_CCPP_ERRORCODE_END = 999;
 
+    /**
+     * General Analysis (C++)
+     * https://www.viva64.com/en/w/
+     */
     public static final int GENERAL_CCPP_HIGH_ERRORCODE_BEGIN = 1000;
+    /**
+     * General Analysis (C++)
+     * https://www.viva64.com/en/w/
+     */
     public static final int GENERAL_CCPP_HIGH_ERRORCODE_END = 1999;
 
+    /**
+     * Customers Specific Requests (C++)
+     * https://www.viva64.com/en/w/#CustomersSpecificRequestsCPP
+     */
     public static final int CUSTOMERSPECIFIC_CCPP_ERRORCODE_BEGIN = 2000;
+    /**
+     * Customers Specific Requests (C++)
+     * https://www.viva64.com/en/w/#CustomersSpecificRequestsCPP
+     */
     public static final int CUSTOMERSPECIFIC_CCPP_ERRORCODE_END = 2499;
 
+    /**
+     * MISRA errors
+     * https://www.viva64.com/en/w/#MISRA
+     */
     public static final int MISRA_CCPP_ERRORCODE_BEGIN = 2500;
+    /**
+     * MISRA errors
+     * https://www.viva64.com/en/w/#MISRA
+     */
     public static final int MISRA_CCPP_ERRORCODE_END = 2999;
 
+    /**
+     * General Analysis (C#)
+     * https://www.viva64.com/en/w/#GeneralAnalysisCS
+     */
     public static final int GENERAL_CS_ERRORCODE_BEGIN = 3000;
+    /**
+     * General Analysis (C#)
+     * https://www.viva64.com/en/w/#GeneralAnalysisCS
+     */
     public static final int GENERAL_CS_ERRORCODE_END = 3499;
 
+    /**
+     * General Analysis (Java)
+     * https://www.viva64.com/en/w/#GeneralAnalysisJAVA
+     */
     public static final int GENERAL_JAVA_ERRORCODE_BEGIN = 6000;
+    /**
+     * General Analysis (Java)
+     * https://www.viva64.com/en/w/#GeneralAnalysisJAVA
+     */
     public static final int GENERAL_JAVA_ERRORCODE_END = 6999;
 
     // errorCode is Vnnn
@@ -84,8 +148,7 @@ public enum AnalyzerType {
                 return AnalyzerType.GENERAL;
             }
         }
-        catch (Exception ex)
-        {
+        catch (StringIndexOutOfBoundsException | NumberFormatException ex) {
             if ("External".equalsIgnoreCase(errorCodeStr)) {
                 return AnalyzerType.GENERAL;
             }
