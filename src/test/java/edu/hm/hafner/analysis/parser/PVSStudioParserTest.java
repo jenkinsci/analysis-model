84981b01e7f7fd882f6ae59c4cee7bdc7d9d1566
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-07-18 11:43:29 +0300

    Added more tests

--- src/test/java/edu/hm/hafner/analysis/parser/PVSStudioParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/PVSStudioParserTest.java
@@ -2,8 +2,6 @@ package edu.hm.hafner.analysis.parser;
 
 import java.util.Locale;
 
-import org.junit.jupiter.api.Test;
-
 import edu.hm.hafner.analysis.AbstractParserTest;
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueParser;
@@ -12,8 +10,6 @@ import edu.hm.hafner.analysis.Severity;
 import edu.hm.hafner.analysis.assertj.SoftAssertions;
 
 import edu.hm.hafner.analysis.parser.pvsstudio.PVSStudioParser;
-
-import static edu.hm.hafner.analysis.assertj.Assertions.*;
 
 /**
  * Tests the class {@link PVSStudioParser}.
@@ -29,20 +25,20 @@ class PVSStudioParserTest extends AbstractParserTest {
 
     @Override
     protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
-        softly.assertThat(report).hasSize(28);
+        softly.assertThat(report).hasSize(33);
         softly.assertThat(report.getFiles()).hasSize(2);
 
         softly.assertThat(report.stream().map(Issue::getSeverity).filter(s -> s == Severity.ERROR).count()).isEqualTo(1);
         softly.assertThat(report.stream().map(Issue::getSeverity).filter(s -> s == Severity.WARNING_HIGH).count()).isEqualTo(5);
-        softly.assertThat(report.stream().map(Issue::getSeverity).filter(s -> s == Severity.WARNING_NORMAL).count()).isEqualTo(19);
+        softly.assertThat(report.stream().map(Issue::getSeverity).filter(s -> s == Severity.WARNING_NORMAL).count()).isEqualTo(24);
         softly.assertThat(report.stream().map(Issue::getSeverity).filter(s -> s == Severity.WARNING_LOW).count()).isEqualTo(3);
 
-        softly.assertThat(report.stream().map(Issue::getType).filter("GENERAL Analysis"::equals).count()).isEqualTo(4);
-        softly.assertThat(report.stream().map(Issue::getType).filter("Micro-OPTIMIZATION"::equals).count()).isEqualTo(0);
+        softly.assertThat(report.stream().map(Issue::getType).filter("GENERAL Analysis"::equals).count()).isEqualTo(7);
+        softly.assertThat(report.stream().map(Issue::getType).filter("Micro-OPTIMIZATION"::equals).count()).isEqualTo(1);
         softly.assertThat(report.stream().map(Issue::getType).filter("Specific Requests"::equals).count()).isEqualTo(3);
         softly.assertThat(report.stream().map(Issue::getType).filter("64-bit"::equals).count()).isEqualTo(11);
         softly.assertThat(report.stream().map(Issue::getType).filter("Misra"::equals).count()).isEqualTo(9);
-        softly.assertThat(report.stream().map(Issue::getType).filter("Unknown"::equals).count()).isEqualTo(1);
+        softly.assertThat(report.stream().map(Issue::getType).filter("Unknown"::equals).count()).isEqualTo(2);
 
         softly.assertThat(report.get(0))
                 .hasSeverity(Severity.ERROR)
