5a8b26cf08d61373628d3d0e2bbe5b2977dbaec2
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-07-29 14:38:14 +0300

    Added correct test file and correct tests.

--- src/test/java/edu/hm/hafner/analysis/parser/PVSStudioParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/PVSStudioParserTest.java
@@ -26,7 +26,7 @@ class PVSStudioParserTest extends AbstractParserTest {
     @Override
     protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
         softly.assertThat(report).hasSize(33);
-        softly.assertThat(report.getFiles()).hasSize(2);
+        softly.assertThat(report.getFiles()).hasSize(1);
 
         softly.assertThat(report).hasSeverities(1, 5, 24, 3);
 
@@ -42,13 +42,13 @@ class PVSStudioParserTest extends AbstractParserTest {
                 .hasCategory("V002")
                 .hasLineStart(42)
                 .hasMessage(getFormedMessage("V002", "Some diagnostic messages may contain incorrect line number."))
-                .hasFileName("C:/Users/Komarov/IdeaProjects/warnings-ng-plugin/work/workspace/fgh/ABackup/Lib/CppLib/KitCpp.h");
+                .hasFileName("D:/SVN_VIVA64/SonarQube/sonar-pvs-studio/out/test/resources/TestReport.plog");
         softly.assertThat(report.get(1))
                 .hasSeverity(Severity.WARNING_HIGH)
                 .hasCategory("V106")
                 .hasLineStart(42)
                 .hasMessage(getFormedMessage("V106", "Implicit type conversion third argument '(lstrlenA(Source) + 1)' of function 'memmove' to memsize type."))
-                .hasFileName("C:/Users/Komarov/IdeaProjects/warnings-ng-plugin/work/workspace/fgh/ABackup/Lib/CppLib/KitCpp.h");
+                .hasFileName("D:/SVN_VIVA64/SonarQube/sonar-pvs-studio/out/test/resources/TestReport.plog");
     }
 
     private String getFormedMessage(final String type, final String messageFromFile) {
