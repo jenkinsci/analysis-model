2f0581ee96bf9f07d67808c6d0dd72d1e8f1e19a
Ulli Hafner <ullrich.hafner@gmail.com>
2019-10-01 11:39:00 +0200

    Mark parser as deprecated.

--- src/main/java/edu/hm/hafner/analysis/parser/violations/ErrorProneAdapter.java
+++ src/main/java/edu/hm/hafner/analysis/parser/violations/ErrorProneAdapter.java
@@ -1,12 +1,16 @@
 package edu.hm.hafner.analysis.parser.violations;
 
+import edu.hm.hafner.analysis.parser.ErrorProneParser;
+
 import se.bjurr.violations.lib.parsers.GoogleErrorProneParser;
 
 /**
  * Parses ErrorProne files.
  *
  * @author Ullrich Hafner
+ * @deprecated replaced by {@link ErrorProneParser}
  */
+@Deprecated
 public class ErrorProneAdapter extends AbstractViolationAdapter {
     private static final long serialVersionUID = -5800235784320729524L;
 
