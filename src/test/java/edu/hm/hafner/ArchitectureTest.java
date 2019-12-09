e854265a736ec130a53ce34158fb5fd0b8d06fae
Ulli Hafner <ullrich.hafner@gmail.com>
2019-12-09 23:19:29 +0100

    Use common base classes from codingstyle.

--- src/test/java/edu/hm/hafner/ArchitectureTest.java
+++ src/test/java/edu/hm/hafner/ArchitectureTest.java
@@ -1,22 +1,16 @@
 package edu.hm.hafner;
 
-import java.util.Arrays;
 import javax.xml.parsers.SAXParser;
 
 import org.apache.commons.digester3.Digester;
 import org.apache.commons.digester3.binder.DigesterLoader;
-import org.apache.commons.lang3.StringUtils;
 import org.xml.sax.XMLReader;
 
-import com.tngtech.archunit.base.DescribedPredicate;
-import com.tngtech.archunit.core.domain.JavaCall;
-import com.tngtech.archunit.core.domain.JavaModifier;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-import edu.hm.hafner.util.AccessRestrictedToTests;
-import edu.hm.hafner.util.VisibleForTesting;
+import edu.hm.hafner.util.ArchitectureRules;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
@@ -37,52 +31,15 @@ class ArchitectureTest {
                     .orShould().callConstructor(Digester.class, XMLReader.class)
                     .orShould().callMethod(DigesterLoader.class, "newDigester");
 
-    /** Test classes should not be public (Junit 5). */
     @ArchTest
-    static final ArchRule NO_PUBLIC_TEST_CLASSES =
-            noClasses().that().haveSimpleNameEndingWith("Test")
-                    .and().doNotHaveModifier(JavaModifier.ABSTRACT)
-                    .should().bePublic();
+    static final ArchRule NO_TEST_API_CALLED = ArchitectureRules.NO_TEST_API_CALLED;
 
-    /**
-     * Methods or constructors that are annotated with {@link VisibleForTesting} must not be called by other classes.
-     * These methods are meant to be {@code private}. Only test classes are allowed to call these methods.
-     */
     @ArchTest
-    static final ArchRule NO_TEST_API_CALLED =
-            noClasses().that().haveSimpleNameNotEndingWith("Test")
-                    .should().callCodeUnitWhere(new AccessRestrictedToTests());
+    static final ArchRule NO_PUBLIC_TEST_CLASSES = ArchitectureRules.NO_PUBLIC_TEST_CLASSES;
 
-    /** Prevents that classes use visible but forbidden API. */
     @ArchTest
-    static final ArchRule NO_FORBIDDEN_PACKAGE_ACCESSED =
-            noClasses()
-            .should().accessClassesThat().resideInAnyPackage(
-                    "org.apache.commons.lang..", "javax.xml.bind..", "javax.annotation..");
+    static final ArchRule NO_FORBIDDEN_PACKAGE_ACCESSED = ArchitectureRules.NO_FORBIDDEN_PACKAGE_ACCESSED;
 
-    /** Prevents that classes use visible but forbidden API. */
     @ArchTest
-    static final ArchRule NO_FORBIDDEN_CLASSES_CALLED
-            = noClasses()
-            .should().callCodeUnitWhere(new TargetIsForbiddenClass(
-                    "org.junit.jupiter.api.Assertions", "org.junit.Assert"));
-
-    /**
-     * Matches if a code unit of one of the registered classes has been called.
-     */
-    private static class TargetIsForbiddenClass extends DescribedPredicate<JavaCall<?>> {
-        private final String[] classes;
-
-        TargetIsForbiddenClass(final String... classes) {
-            super("forbidden class");
-
-            this.classes = Arrays.copyOf(classes, classes.length);
-        }
-
-        @Override
-        public boolean apply(final JavaCall<?> input) {
-            return StringUtils.containsAny(input.getTargetOwner().getFullName(), classes)
-                    && !input.getName().equals("assertTimeoutPreemptively");
-        }
-    }
+    static final ArchRule NO_FORBIDDEN_CLASSES_CALLED = ArchitectureRules.NO_FORBIDDEN_CLASSES_CALLED;
 }
