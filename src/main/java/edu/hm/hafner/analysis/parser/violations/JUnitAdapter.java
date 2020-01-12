cfcb45fa100b53830f3834a285eb0e927cf5e68b
Gyanesha <gyaneshaprajjwal@gmail.com>
2020-01-13 00:23:02 +0530

    Adding JUnitAdapter

---
+++ src/main/java/edu/hm/hafner/analysis/parser/violations/JUnitAdapter.java
@@ -0,0 +1,15 @@
+package edu.hm.hafner.analysis.parser.violations;
+
+import se.bjurr.violations.lib.parsers.JUnitParser;
+
+public class JUnitAdapter extends AbstractViolationAdapter {
+    private static final long serialVersionUID = -1595503635554896281L;
+
+    @Override
+    protected JUnitParser createParser() {
+        return new JUnitParser();
+    }
+
+
+
+}
