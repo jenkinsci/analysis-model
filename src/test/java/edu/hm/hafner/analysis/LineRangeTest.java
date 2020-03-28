51b81617bec603173aa844566a0cbea29f7e6a03
Simon Symhoven <simon.symhoven@hm.edu>
2020-03-28 21:29:17 +0100

    * added class puml file and sequence puml file for factory pattern

---
+++ src/test/java/edu/hm/hafner/analysis/LineRangeTest.java
@@ -0,0 +1,15 @@
+package edu.hm.hafner.analysis;
+
+import edu.hm.hafner.util.SerializableTest;
+
+/**
+ * Implementation of Serializable Factory test for {@link LineRange}.
+ *
+ * @author Simon Symhoven
+ */
+public class LineRangeTest extends SerializableTest<LineRange> {
+    @Override
+    protected LineRange createSerializable() {
+        return null;
+    }
+}
