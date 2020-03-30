77febe6c00ee079a7ac3215edf17024d0e0488c5
Bastian <bastian@cmbt.de>
2020-03-30 13:31:04 +0200

    Changed Test modifier and Nullable annotations v2

--- src/test/java/edu/hm/hafner/analysis/LineRangeTest.java
+++ src/test/java/edu/hm/hafner/analysis/LineRangeTest.java
@@ -5,7 +5,7 @@ import edu.hm.hafner.util.SerializableTest;
 /**
  * Konkreter SerializableTest für die Klasse LineRange.
  */
-public class LineRangeTest extends SerializableTest<LineRange> {
+class LineRangeTest extends SerializableTest<LineRange> {
     @Override
     protected LineRange createSerializable() {
         return new LineRange(0, 1);
