7823090d99c3b43aee81650a3ca0c058086728da
Simon Symhoven <simon.symhoven@hm.edu>
2020-04-09 11:06:21 +0200

    added javadoc for NullSafeCollections

--- src/main/java/edu/hm/hafner/analysis/NullSafeCollections.java
+++ src/main/java/edu/hm/hafner/analysis/NullSafeCollections.java
@@ -4,16 +4,37 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class NullSafeCollections<E> {
-    public static List nullSafeList(final List list) {
+/**
+ * Collection factory for NullSafeLists
+ * @author Simon Symhoven
+ */
+class NullSafeCollections {
+
+    /**
+     * constructor with specified list type
+     * @param list
+     *              which should add to the null safe list
+     * @return a null safe list of {@link NullSafeList}
+     */
+    static List nullSafeList(final List list) {
         return new NullSafeList<>(list);
     }
 
-    public static List nullSafeList() {
+    /**
+     * basic constructor
+     * @return a null safe list of {@link NullSafeList}
+     */
+    static List nullSafeList() {
         return new NullSafeList<>(new ArrayList());
     }
 
-    public static List nullSafeList(Collection c) {
+    /**
+     * constructor with collection to add
+     * @param c
+     *          Collection which sould add to the null safe list
+     * @return  a null safe list of {@link NullSafeList}
+     */
+    static List nullSafeList(Collection c) {
         return new NullSafeList<>(new ArrayList(c));
     }
 }
