a8c6c0673f9aeb2c5bdafc435f1017d5d04503bc
Marten Budelmann <budelman@hm.edu>
2020-04-14 13:37:30 +0200

    added javadoc and modified tests

--- src/main/java/edu/hm/hafner/analysis/NullSafeCollections.java
+++ src/main/java/edu/hm/hafner/analysis/NullSafeCollections.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Utility Klasse zum Erstellen von NullSafe Objekten.
+ * Utility Klasse zum Erstellen von NullSafe Instanzen.
  *
  * @param <T>
  *         Typparameter
@@ -14,10 +14,36 @@ import java.util.List;
  */
 public abstract class NullSafeCollections <T>{
 
+    /**
+     * Gibt eine neue NullSafeList zuruck.
+     *
+     * @param list
+     *         Die Delegate Liste fuer die NullSafeList.
+     *
+     * @param <T>
+     *         Typparameter
+     *
+     * @return Eine neue NullSafeList
+     */
     public static <T> List<T> nullSafeList(final List<T> list) {
         return new NullSafeList<>(list);
     }
 
+    /**
+     * Gibt eine neue NullSafeList zurueck mit gegebener minimaler Kapazitaet.
+     * Handelt es sich bei dem Delegate Objekt um eine ArrayList, wird die minimale Kapazität gesichert.
+     *
+     * @param list
+     *         Die Delegate Liste fuer die NullSafeList.
+     *
+     * @param initialCapacity
+     *         Die initiale Kapazitaet
+     *
+     * @param <T>
+     *         Typparameter
+     *
+     * @return Eine neue NullSafeList
+     */
     public static <T> List<T> nullSafeList(final List<T> list, final int initialCapacity) {
         if(list instanceof ArrayList<?>) {
             ((ArrayList<?>) list).ensureCapacity(initialCapacity);
@@ -25,6 +51,20 @@ public abstract class NullSafeCollections <T>{
         return new NullSafeList<>(list);
     }
 
+    /**
+     * Erstellt eine neue NullSafeList, welche die uebergebenen Werte enthaelt.
+     *
+     * @param list
+     *         Die Delegate Liste fuer die NullSafeList.
+     *
+     * @param collection
+     *         Werte fuer die NullSafeList
+     *
+     * @param <T>
+     *         Typparameter
+     *
+     * @return Eine neue NullSafeList
+     */
     public static <T> List<T> nullSafeList(final List<T> list, final Collection<? extends T> collection) {
         NullSafeList<T> nullSafeList = new NullSafeList<>(list);
         list.addAll(collection);
