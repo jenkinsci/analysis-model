eb0a0ae33ffb024f17564566dfc2eaf9a1ae3fbb
Daniel Soukup <d.soukup@hm.edu>
2020-04-24 23:04:45 +0200

    Blatt 1 abgespalten

---
+++ src/main/java/edu/hm/hafner/analysis/LineRangeBuilder.java
@@ -0,0 +1,29 @@
+package edu.hm.hafner.analysis;
+
+public class LineRangeBuilder {
+    private int start = -1;
+    private int end = -1;
+
+    public LineRangeBuilder(final int start, final int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public LineRangeBuilder() {
+
+    }
+
+    public LineRangeBuilder setStart(final int start) {
+        this.start = start;
+        return this;
+    }
+
+    public LineRangeBuilder setEnd(final int end) {
+        this.end = end;
+        return this;
+    }
+
+    public LineRange build(){
+        return new LineRange(start, end);
+    }
+}
