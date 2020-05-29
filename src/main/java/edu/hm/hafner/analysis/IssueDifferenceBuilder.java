a3b6473686fd588c7bafb25d4e9d4d01839c871e
Viet Phuoc Ho <v.ho@hm.edu>
2020-05-29 13:26:56 +0200

    added UML diagrams, tests and builder

--- src/main/java/edu/hm/hafner/analysis/IssueDifferenceBuilder.java
+++ src/main/java/edu/hm/hafner/analysis/IssueDifferenceBuilder.java
@@ -1,4 +1,60 @@
 package edu.hm.hafner.analysis;
-
+/**
+ * Builder for IssueDifference
+ *
+ * @author Viet Phuoc Ho (v.ho@hm.edu)
+ */
 public class IssueDifferenceBuilder {
+
+    /**
+     * The current Issue.
+     */
+    private Report currentIssues;
+
+    /**
+     * The ID of the Reference for this Issue.
+     */
+    private String referenceId;
+
+    /**
+     * The referenced Issue
+     */
+    private Report referenceIssues;
+
+    /**
+     * Setter for the current issues.
+     * @param currentIssue
+     *                  the current issue
+     */
+    public IssueDifferenceBuilder setCurrentIssues(final Report currentIssue) {
+        this.currentIssues = currentIssue;
+        return this;
+    }
+
+    /**
+     * Setter for the reference ID.
+     * @param referenceID
+     *                  of the issue
+     */
+    public IssueDifferenceBuilder setReferenceId(final String referenceID) {
+        this.referenceId = referenceID;
+        return this;
+    }
+
+    /**
+     * Setter for the ReferenceIssues.
+     * @param referenceIssues the reference Issues
+     */
+    public IssueDifferenceBuilder setReferenceIssues(final Report referenceIssues) {
+        this.referenceIssues = referenceIssues;
+        return this;
+    }
+
+    /**
+     * Builder for the IssueDifference
+     * @return new IssueDifference object.
+     */
+    IssueDifference build() {
+        return new IssueDifference(this.currentIssues, this.referenceId, this.referenceIssues);
+    }
 }
--- src/main/java/edu/hm/hafner/analysis/LineRangeBuilder.java
+++ src/main/java/edu/hm/hafner/analysis/LineRangeBuilder.java
@@ -1,4 +1,44 @@
 package edu.hm.hafner.analysis;
-
+/**
+ * Builder for LineRange.
+ *
+ * @author Viet Phuoc Ho (v.ho@hm.edu)
+ */
 public class LineRangeBuilder {
+
+    /**
+     * Setter for start value.
+     * @param start
+     *              Start value.
+     */
+    public void setStart(final int start) {
+        this.start = start;
+    }
+
+    /**
+     * Setter for end.
+     * @param end
+     *              End value.
+     */
+    public void setEnd(final int end) {
+        this.end = end;
+    }
+
+    /**
+     * The Start index of the line range.
+     */
+    private int start;
+
+    /**
+     * The End index of the line range.
+     */
+    private int end;
+
+    /**
+     * Builder for a lineRange.
+     * @return new Object of LineRange.
+     */
+    public LineRange build() {
+        return new LineRange(start,end);
+    }
 }
