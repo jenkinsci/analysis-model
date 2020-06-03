d4dde6a2582339a44b390ed1756aa9202978bb26
Benjamin Hartwig <b.hartwig@hm.edu>
2020-06-03 10:50:10 +0200

    Added Tests and Diagrams

---
+++ src/test/java/edu/hm/hafner/analysis/ArrayListTest.java
@@ -0,0 +1,17 @@
+package edu.hm.hafner.analysis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ArrayListTest extends ListTest{
+    @Override
+    protected List<Integer> create(final int numberOfInitialElements) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0;i < numberOfInitialElements;i++) {
+            list.add(i);
+        }
+
+        return list;
+    }
+}
---
+++ src/test/java/edu/hm/hafner/analysis/LineRangeTest.java
@@ -0,0 +1,10 @@
+package edu.hm.hafner.analysis;
+
+import edu.hm.hafner.util.SerializableTest;
+
+class LineRangeTest extends SerializableTest<LineRange> {
+    @Override
+    protected LineRange createSerializable() {
+        return new LineRange(15, 18);
+    }
+}
