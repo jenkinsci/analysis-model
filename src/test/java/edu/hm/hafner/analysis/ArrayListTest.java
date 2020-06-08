555c91f8d2554e9b1d739561f5ae9949939a86c9
Simon Schroeppel <simon.schroeppel@gmail.com>
2020-06-08 20:46:45 +0200

    Fix architecture violation

--- src/test/java/edu/hm/hafner/analysis/ArrayListTest.java
+++ src/test/java/edu/hm/hafner/analysis/ArrayListTest.java
@@ -3,7 +3,7 @@ package edu.hm.hafner.analysis;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArrayListTest extends ListTest{
+class ArrayListTest extends ListTest{
     @Override
     List<Integer> create(final int numberOfInitialElements) {
         List<Integer> list = new ArrayList<>();
