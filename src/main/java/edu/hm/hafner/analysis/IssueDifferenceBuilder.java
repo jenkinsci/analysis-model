18dbb48a1c648f14cdc0bfeaa7d1da35561fd91d
11Mike11 <M.Schober2@gmx.de>
2020-06-09 10:40:35 +0200

    reduced accessability of IssueDifferenceBuilder and LineRangeBuilder

--- src/main/java/edu/hm/hafner/analysis/IssueDifferenceBuilder.java
+++ src/main/java/edu/hm/hafner/analysis/IssueDifferenceBuilder.java
@@ -8,17 +8,17 @@ public class IssueDifferenceBuilder {
     private Report referenceIssues;
     private String referenceID;
 
-    public IssueDifferenceBuilder setCurrentIssues(final Report currentIssues) {
+    IssueDifferenceBuilder setCurrentIssues(final Report currentIssues) {
         this.currentIssues = currentIssues;
         return this;
     }
 
-    public IssueDifferenceBuilder setReferenceIssues(final Report referenceIssues) {
+    IssueDifferenceBuilder setReferenceIssues(final Report referenceIssues) {
         this.referenceIssues = referenceIssues;
         return this;
     }
 
-    public IssueDifferenceBuilder setReferenceID(final String referenceID) {
+    IssueDifferenceBuilder setReferenceID(final String referenceID) {
         this.referenceID = referenceID;
         return this;
     }
@@ -26,7 +26,7 @@ public class IssueDifferenceBuilder {
      * Eine neue IssueDifference.
      * @return IsseDifference
      */
-    public IssueDifference build() {
+    IssueDifference build() {
         return new IssueDifference(currentIssues, referenceID, referenceIssues);
     }
 }
