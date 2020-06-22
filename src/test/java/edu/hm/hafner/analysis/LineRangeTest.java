5986befaf8cd0ff82ae5ab04da373fda90b141af
kariustobias <tobias.karius@hm.edu>
2020-06-22 14:25:19 +0200

    implemented UML Diagrams for Task 1.3.2 and 1.3.3 + implemented method createSerializable() in LineRangeTest.java

--- src/test/java/edu/hm/hafner/analysis/LineRangeTest.java
+++ src/test/java/edu/hm/hafner/analysis/LineRangeTest.java
@@ -2,9 +2,14 @@ package edu.hm.hafner.analysis;
 
 import edu.hm.hafner.util.SerializableTest;
 
+/**
+ * Tests, if class LineRange is Serializable {@link LineRange}.
+ *
+ * @author Tobias Karius
+ */
 public class LineRangeTest extends SerializableTest<LineRange> {
     @Override
     protected LineRange createSerializable() {
-        return null;
+        return new LineRange(1, 2);
     }
 }
