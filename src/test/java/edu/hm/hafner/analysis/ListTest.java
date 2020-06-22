2727d1889911c8d3b918292d6265dee644195195
JohannesJaeger <mail@jaeger-johannes.de>
2020-06-23 00:02:50 +0200

    Add list test klass

---
+++ src/test/java/edu/hm/hafner/analysis/ListTest.java
@@ -0,0 +1,63 @@
+package edu.hm.hafner.analysis;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+abstract class ListTest {
+
+    abstract List<Integer> create(int numberOfInitialElements);
+
+    @Test
+    void testEmpty() {
+        final List<Integer> list = create(0);
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    void testNotEmpty() {
+        final List<Integer> list = create(1);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    void testSize() {
+        final List<Integer> list = create(10);
+        assertEquals(list.size(), 10);
+    }
+
+    @Test
+    void testAdd() {
+        final List<Integer> list = create(0);
+        assertEquals(list.size(), 0);
+        list.add(2);
+        assertEquals(list.size(), 1);
+        assertEquals(list.get(0), 2);
+    }
+
+    @Test
+    void testRemove() {
+        final List<Integer> list = create(0);
+        list.add(2);
+        assertEquals(list.get(0), 2);
+        assertEquals(list.remove(0), 2);
+        assertEquals(list.size(), 0);
+        assertTrue(list.isEmpty());
+    }
+    
+    @Test
+    void testContainsTrue() {
+        final List<Integer> list = create(0);
+        list.add(100);
+        assertTrue(list.contains(100));
+    }
+    
+    @Test
+    void testContainsFalse() {
+        final List<Integer> list = create(0);
+        list.add(100);
+        assertTrue(list.contains(0));
+    }
+
+}
