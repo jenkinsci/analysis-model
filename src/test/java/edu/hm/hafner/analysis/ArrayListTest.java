b105ee76da8650b7c8feb0efb9ff9d4b880a79bb
Johannes Jäger <40130846+JohannesJaeger@users.noreply.github.com>
2020-06-23 12:10:39 +0200

    Fix javadoc

--- src/test/java/edu/hm/hafner/analysis/ArrayListTest.java
+++ src/test/java/edu/hm/hafner/analysis/ArrayListTest.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+/**
+ * Tests the class {@link ArrayList}.
+ */
 class ArrayListTest extends ListTest {
 
     @Override
