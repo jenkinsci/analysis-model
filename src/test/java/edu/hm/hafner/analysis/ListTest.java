57b5afcb1439798635df584298142409946ca14b
Johannes Jäger <40130846+JohannesJaeger@users.noreply.github.com>
2020-06-23 12:58:20 +0200

    Update ListTest.java

--- src/test/java/edu/hm/hafner/analysis/ListTest.java
+++ src/test/java/edu/hm/hafner/analysis/ListTest.java
@@ -3,13 +3,11 @@ package edu.hm.hafner.analysis;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Abstract Tests for the implementation of the interface {@link List}.
+ * @author Johannes Jäger 
  */
 abstract class ListTest {
 
@@ -18,52 +16,52 @@ abstract class ListTest {
     @Test
     void testEmpty() {
         final List<Integer> list = create(0);
-        assertTrue(list.isEmpty());
+        assertThat(list.isEmpty()).isTrue();
     }
 
     @Test
     void testNotEmpty() {
         final List<Integer> list = create(1);
-        assertFalse(list.isEmpty());
+        assertThat(list.isEmpty()).isFalse();
     }
 
     @Test
     void testSize() {
         final List<Integer> list = create(10);
-        assertEquals(list.size(), 10);
+        assertThat(list.size()).isEqualTo(10);
     }
 
     @Test
     void testAdd() {
         final List<Integer> list = create(0);
-        assertEquals(list.size(), 0);
+        assertThat(list.size()).isEqualTo(0);
         list.add(2);
-        assertEquals(list.size(), 1);
-        assertEquals(list.get(0), 2);
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.get(0)).isEqualTo(2);
     }
 
     @Test
     void testRemove() {
         final List<Integer> list = create(0);
         list.add(2);
-        assertEquals(list.get(0), 2);
-        assertEquals(list.remove(0), 2);
-        assertEquals(list.size(), 0);
-        assertTrue(list.isEmpty());
+        assertThat(list.get(0)).isEqualTo(2);
+        assertThat(list.remove(0)).isEqualTo(2);
+        assertThat(list.size()).isEqualTo(0);
+        assertThat(list.isEmpty()).isTrue();
     }
     
     @Test
     void testContainsTrue() {
         final List<Integer> list = create(0);
         list.add(100);
-        assertTrue(list.contains(100));
+        assertThat(list.contains(100)).isTrue();
     }
     
     @Test
     void testContainsFalse() {
         final List<Integer> list = create(0);
         list.add(100);
-        assertFalse(list.contains(0));
+        assertThat(list.contains(0)).isFalse();
     }
 
 }
