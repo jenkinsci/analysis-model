23fc97edc8b6e97c2c91685d2292fa31bdafcb3f
Andreas Mandel <andreas.mandel@gmail.com>
2020-07-04 13:40:32 +0200

    Adding support to scan clair-scanner json data.

---
+++ src/main/java/edu/hm/hafner/analysis/parser/ClairParser.java
@@ -0,0 +1,119 @@
+package edu.hm.hafner.analysis.parser;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import edu.hm.hafner.analysis.Issue;
+import edu.hm.hafner.analysis.IssueBuilder;
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.ParsingException;
+import edu.hm.hafner.analysis.ReaderFactory;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.Severity;
+
+/**
+ * A parser for dockerlint json output.
+ * <p>
+ * See <a href='https://github.com/arminc/clair-scanner'>clair-scanner</a> for project details.
+ *
+ * @author Andreas Mandel
+ */
+public class ClairParser extends IssueParser {
+    private static final long serialVersionUID = 42L;
+
+    String image;
+
+    @Override
+    public boolean accepts(final ReaderFactory readerFactory) {
+        return readerFactory.getFileName().endsWith(".json");
+    }
+
+    @Override
+    public Report parse(final ReaderFactory readerFactory) throws ParsingException {
+        Report report = new Report();
+        try (Reader reader = readerFactory.create()) {
+            JSONObject jsonReport = (JSONObject) new JSONTokener(reader).nextValue();
+            image = optStringIgnoreCase(jsonReport,"image");
+            JSONArray vulnerabilities = optJsonArrayIgnoreCase(jsonReport, "vulnerabilities");
+            for (Object vulnerability : vulnerabilities) {
+                if (vulnerability instanceof JSONObject) {
+                    report.add(convertToIssue((JSONObject) vulnerability));
+                }
+            }
+        } catch (IOException | JSONException | ClassCastException e) {
+            throw new ParsingException(e);
+        }
+        return report;
+    }
+
+    private Issue convertToIssue(final JSONObject jsonIssue) {
+        StringBuilder message = new StringBuilder();
+        appendIfNotEmpty(jsonIssue, message, "featurename", "");
+        appendIfNotEmpty(jsonIssue, message, "featureversion", ":");
+        appendIfNotEmpty(jsonIssue, message, "description", "");
+        appendIfNotEmpty(jsonIssue, message, "fixedby", "Fixed by ");
+        appendIfNotEmpty(jsonIssue, message, "link", "see ");
+        return new IssueBuilder()
+                .setMessage(message.toString())
+                .setCategory(optStringIgnoreCase(jsonIssue, "vulnerability"))
+                .setSeverity(toSeverity(optStringIgnoreCase(jsonIssue, "severity")))
+                .setType(optStringIgnoreCase(jsonIssue, "namespace"))
+                .setFileName(image).build();
+    }
+
+    private void appendIfNotEmpty(final JSONObject issue, final StringBuilder message, final String key,
+            final String head) {
+        String text = optStringIgnoreCase(issue, key);
+        if (text != null && !text.isEmpty()) {
+            if (message.length() > 0 && !head.startsWith(":")) {
+                message.append(' ');
+            }
+            message.append(head).append(text);
+        }
+    }
+
+    private Severity toSeverity(final String level) {
+        switch (String.valueOf(level).toLowerCase(Locale.ENGLISH)) {
+            case "defcon1":
+                return Severity.ERROR;
+            case "critical":
+                return Severity.WARNING_HIGH;
+            case "high":
+                return Severity.WARNING_NORMAL;
+            default:
+                return Severity.WARNING_LOW;
+        }
+    }
+
+    private JSONArray optJsonArrayIgnoreCase(JSONObject json, final String searchKey) {
+        Object result = optIgnoreCase(json, searchKey);
+        return result instanceof JSONArray ? (JSONArray) result : null;
+    }
+
+    private String optStringIgnoreCase(JSONObject json, final String searchKey) {
+        Object result = optIgnoreCase(json, searchKey);
+        return result instanceof String ? (String) result : null;
+    }
+
+    private Object optIgnoreCase(JSONObject json, final String searchKey) {
+        Object result = json.opt(searchKey);
+        if (result == null) {
+            for (String key : json.keySet()) {
+                if (key.equalsIgnoreCase(searchKey)) {
+                    result = json.opt(key);
+                    if (result != null) {
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/edu/hm/hafner/analysis/parser/ClairParserTest.java
@@ -0,0 +1,74 @@
+package edu.hm.hafner.analysis.parser;
+
+import java.nio.file.FileSystems;
+
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.analysis.AbstractParserTest;
+import edu.hm.hafner.analysis.FileReaderFactory;
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.ParsingException;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.Severity;
+import edu.hm.hafner.analysis.assertions.SoftAssertions;
+
+import static edu.hm.hafner.analysis.assertions.Assertions.*;
+
+class ClairParserTest extends AbstractParserTest {
+
+    ClairParserTest() {
+        super("clair.json");
+    }
+
+    @Override
+    protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
+        softly.assertThat(report).hasSize(112);
+        softly.assertThat(report.get(1)).hasSeverity(Severity.WARNING_LOW);
+        softly.assertThat(report.get(0))
+                .hasFileName("registry.example.com/project/project-docker-was/develop:10.0.230")
+                .hasCategory("CVE-2020-13630")
+                .hasType("ubuntu:18.04")
+                .hasMessage(
+                    "sqlite3:3.22.0-1ubuntu0.1 ext/fts3/fts3.c in SQLite before 3.32.0 has a use-after-free "
+                        + "in fts3EvalNextRow, related to the snippet feature. Fixed by 3.22.0-1ubuntu0.4 "
+                        + "see http://people.ubuntu.com/~ubuntu-security/cve/CVE-2020-13630");
+    }
+
+    @Override
+    protected IssueParser createParser() {
+        return new ClairParser();
+    }
+
+    @Test
+    void accepts() {
+        assertThat(new ClairParser().accepts(
+                new FileReaderFactory(FileSystems.getDefault().getPath("clair.json")))).isTrue();
+        assertThat(new ClairParser().accepts(
+                new FileReaderFactory(FileSystems.getDefault().getPath("clair.xml")))).isFalse();
+    }
+
+    @Test
+    void unusualInput() {
+        Report report = parse("clair-unusual.json");
+        assertThat(report).hasSize(5);
+        assertThat(report.get(0))
+                .hasSeverity(Severity.WARNING_LOW)
+                .hasMessage(
+                        "sqlite3 Fixed by 3.22.0-1ubuntu0.4");
+        assertThat(report.get(1))
+                .hasSeverity(Severity.WARNING_NORMAL)
+                .hasMessage(
+                        "libidn2");
+        assertThat(report.get(2))
+                .hasSeverity(Severity.ERROR);
+        assertThat(report.get(3))
+                .hasMessage(
+                        "libgcrypt20 see http://people.ubuntu.com/~ubuntu-security/cve/CVE-2019-13627");
+    }
+
+    @Test
+    void brokenInput() {
+        assertThatThrownBy(() -> parse("eclipse.txt"))
+                .isInstanceOf(ParsingException.class);
+    }
+}
