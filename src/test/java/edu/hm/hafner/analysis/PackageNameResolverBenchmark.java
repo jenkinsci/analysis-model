cc0a23e1864ae9d74e3983b3b3743aa537a8e12d
Ulli Hafner <ullrich.hafner@gmail.com>
2020-07-05 16:50:53 +0200

    Improve Benchmark initialization (State).

--- src/test/java/edu/hm/hafner/analysis/PackageNameResolverBenchmark.java
+++ src/test/java/edu/hm/hafner/analysis/PackageNameResolverBenchmark.java
@@ -7,7 +7,11 @@ import java.nio.charset.StandardCharsets;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 
 import edu.hm.hafner.analysis.PackageDetectors.FileSystem;
 
@@ -24,20 +28,16 @@ public class PackageNameResolverBenchmark extends AbstractBenchmark {
 
     /**
      * Benchmarking the {@link PackageNameResolver} with 1000 Issues.
+     *
+     * @param state
+     *         a {@link BenchmarkState} object containing the report
      */
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @Fork(value = 5, warmups = 5)
-    public void benchmark1000IssuesTest() throws IOException {
-        Report report = createIssues();
-        for (int i = 0; i < 1000; i++) {
-            report.add(new IssueBuilder().setFileName(FILE_WITH_PACKAGE + i)
-                    .setPackageName("existing")
-                    .build());
-        }
-
+    public void benchmark1000IssuesTest(final BenchmarkState state) throws IOException {
         PackageNameResolver resolver = new PackageNameResolver(createFileSystemStub());
-        resolver.run(report, StandardCharsets.UTF_8);
+        resolver.run(state.getReport(), StandardCharsets.UTF_8);
     }
 
     private FileSystem createFileSystemStub() throws IOException {
@@ -47,7 +47,28 @@ public class PackageNameResolverBenchmark extends AbstractBenchmark {
         return fileSystemStub;
     }
 
-    private Report createIssues() {
-        return new Report();
+    /**
+     * State for the benchmark containing all preconfigured and necessary objects.
+     */
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        private Report report;
+
+        public Report getReport() {
+            return report;
+        }
+
+        /**
+         * Initializes the reports.
+         */
+        @Setup(Level.Iteration)
+        public void doSetup() {
+            report = new Report();
+            for (int i = 0; i < 1000; i++) {
+                report.add(new IssueBuilder().setFileName(FILE_WITH_PACKAGE + i)
+                        .setPackageName("existing")
+                        .build());
+            }
+        }
     }
 }
