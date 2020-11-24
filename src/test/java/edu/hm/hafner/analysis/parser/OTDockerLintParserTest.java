dd18b44478d9e08251a0e89be08197282d33f76d
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-11-24 20:25:51 +0530

    Added dockerlinter parser analysis
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

---
+++ src/test/java/edu/hm/hafner/analysis/parser/OTDockerLintParserTest.java
@@ -0,0 +1,75 @@
+package edu.hm.hafner.analysis.parser;
+
+import java.nio.file.FileSystems;
+
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.analysis.AbstractParserTest;
+import edu.hm.hafner.analysis.FileReaderFactory;
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.ParsingException;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.Severity;
+import edu.hm.hafner.analysis.assertions.SoftAssertions;
+
+import static edu.hm.hafner.analysis.assertions.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Tests the class {@link OTDockerLintParser}.
+ *
+ * @author Abhishek Dubey
+ */
+
+class OTDockerLintParserTest extends AbstractParserTest {
+
+    OTDockerLintParserTest() {
+        super("ot-docker-linter.json");
+    }
+
+    @Override
+    protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
+        assertThat(report).hasSize(1);
+        softly.assertThat(report.get(0))
+                .hasModuleName("WORKDIR spsp/")
+                .hasCategory("DL3000")
+                .hasDescription("Use absolute WORKDIR.")
+                .hasLineStart(3)
+                .hasFileName("testing/Dockerfile.testing")
+                .hasSeverity(Severity.ERROR);
+    }
+
+    @Override
+    protected IssueParser createParser() {
+        return new OTDockerLintParser();
+    }
+
+    @Test
+    void accepts() {
+        assertThat(new OTDockerLintParser().accepts(
+                new FileReaderFactory(FileSystems.getDefault().getPath("lint.json")))).isTrue();
+        assertThat(new OTDockerLintParser().accepts(
+                new FileReaderFactory(FileSystems.getDefault().getPath("foo.txt")))).isFalse();
+    }
+
+    @Test
+    void unusualInput() {
+        Report report = parse("ot-docker-linter-unsual.json");
+        assertThat(report).hasSize(1);
+        assertThat(report.get(0))
+                .hasSeverity(Severity.ERROR);
+        assertThat(report.get(0))
+                .hasSeverity(Severity.ERROR)
+                .hasCategory("DL3000");
+        assertThat(report.get(0))
+                .hasFileName("testing/Dockerfile.testing")
+                .hasDescription("Use absolute WORKDIR.")
+                .hasLineStart(3);
+    }
+
+    @Test
+    void brokenInput() {
+        assertThatThrownBy(() -> parse("eclipse.txt"))
+                .isInstanceOf(ParsingException.class);
+    }
+}
