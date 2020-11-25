1ad8aa045b2aae57f6b6d8eb97971c72fb5b370d
Ulli Hafner <ullrich.hafner@gmail.com>
2020-11-25 19:18:43 +0100

    Remove test from API.

--- src/test/java/edu/hm/hafner/analysis/IssueParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/IssueParserTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.*;
  *
  * @author Ullrich Hafner
  */
-public class IssueParserTest {
+class IssueParserTest {
     @Test
     void shouldCompareWithoutConsideringCase() {
         assertThat(IssueParser.equalsIgnoreCase("a", "b")).isFalse();
