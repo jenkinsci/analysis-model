faf7ccae1fefd99a81485b08c6ab7296997b93b7
Moravek Ladislav <ladislav.moravek@elektrobit.com>
2021-02-03 10:32:15 +0100

    [JENKINS-64657] created DoxygenParser and tests

---
+++ src/main/java/edu/hm/hafner/analysis/parser/DoxygenParser.java
@@ -0,0 +1,16 @@
+package edu.hm.hafner.analysis.parser;
+
+public class DoxygenParser extends Gcc4CompilerParser {
+    private static final long serialVersionUID = 8760302999081711502L;
+
+    private static final String DOXYGEN_WARNING_PATTERN =
+            "(?:(.+?):(\\d+):(?:(\\d+):)?)? ?([wW]arning|.*[Ee]rror): (.*)$";
+
+    /**
+     * Creates a new instance of {@link DoxygenParser}.
+     */
+    public DoxygenParser() {
+        super(DOXYGEN_WARNING_PATTERN);
+    }
+
+}
