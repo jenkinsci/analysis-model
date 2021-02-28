9b6507d224cabdc532404edb02dab73019f169a5
Ulli Hafner <ullrich.hafner@gmail.com>
2021-02-28 23:04:13 +0100

    Add CheckStyle messages.

--- src/main/java/edu/hm/hafner/analysis/registry/CheckstyleDescriptor.java
+++ src/main/java/edu/hm/hafner/analysis/registry/CheckstyleDescriptor.java
@@ -1,7 +1,10 @@
 package edu.hm.hafner.analysis.registry;
 
+import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueParser;
 import edu.hm.hafner.analysis.parser.checkstyle.CheckStyleParser;
+import edu.hm.hafner.analysis.parser.checkstyle.CheckStyleRules;
+import edu.hm.hafner.util.Deferred;
 
 /**
  * A descriptor for the CheckStyle warnings.
@@ -11,6 +14,8 @@ import edu.hm.hafner.analysis.parser.checkstyle.CheckStyleParser;
 class CheckstyleDescriptor extends ParserDescriptor {
     private static final String ID = "checkstyle";
     private static final String NAME = "CheckStyle";
+
+    private final Deferred<CheckStyleRules> messages = new Deferred<>(CheckStyleRules::new);
 
     CheckstyleDescriptor() {
         super(ID, NAME);
@@ -29,5 +34,10 @@ class CheckstyleDescriptor extends ParserDescriptor {
     @Override
     public String getUrl() {
         return "https://checkstyle.org";
+    }
+
+    @Override
+    public String getDescription(final Issue issue) {
+        return messages.get().getDescription(issue.getType());
     }
 }
