9dda0d8ab59c728e13ee5561a236f988d53f9985
Ulli Hafner <ullrich.hafner@gmail.com>
2021-12-03 23:31:37 +0100

    [JENKINS-67296] Ignore JSON files without results.

--- src/test/java/edu/hm/hafner/analysis/parser/TrivyParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/TrivyParserTest.java
@@ -1,7 +1,5 @@
 package edu.hm.hafner.analysis.parser;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import org.junit.jupiter.api.Test;
 
 import edu.hm.hafner.analysis.AbstractParserTest;
@@ -9,14 +7,14 @@ import edu.hm.hafner.analysis.IssueParser;
 import edu.hm.hafner.analysis.ParsingException;
 import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
-import edu.hm.hafner.analysis.assertions.Assertions;
 import edu.hm.hafner.analysis.assertions.SoftAssertions;
+
+import static edu.hm.hafner.analysis.assertions.Assertions.*;
 
 /**
  * Tests the class {@link TrivyParser}.
  */
 class TrivyParserTest extends AbstractParserTest {
-
     TrivyParserTest() {
         super("trivy_result.json");
     }
@@ -36,13 +34,20 @@ class TrivyParserTest extends AbstractParserTest {
     void parseResultsForSchemaVersion2() {
         Report report = parse("trivy_result_0.20.0.json");
 
-        Assertions.assertThat(report).hasSize(4);
+        assertThat(report).hasSize(4);
 
-        Assertions.assertThat(report.get(0))
+        assertThat(report.get(0))
                 .hasSeverity(Severity.WARNING_LOW)
                 .hasType("CVE-2017-6519")
                 .hasCategory("redhat")
                 .hasMessage("avahi: Multicast DNS responds to unicast queries outside of local network");
+    }
+
+    @Test
+    void shouldHandleEmptyResultsJenkins67296() {
+        Report report = parse("issue67296.json");
+
+        assertThat(report).isEmpty();
     }
 
     @Test
