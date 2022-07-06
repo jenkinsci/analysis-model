f733a7fdacc0997415eab066dff10d0963eb7326
Dominik Jantschar <Dominik.jantschar@adesso.de>
2022-07-06 21:52:46 +0200

    Added additional information to a parsed issue from revapi

---
+++ src/main/java/edu/hm/hafner/analysis/RevapiInfoExtension.java
@@ -0,0 +1,83 @@
+package edu.hm.hafner.analysis;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+
+/**
+ * Stores the additional information of a parsed issue.
+ * (Severities, issueName, oldFile, newFile)
+ */
+public class RevapiInfoExtension implements Serializable {
+
+    private static final long serialVersionUID = 6058160289391492934L;
+
+    private final Map<String, String> severities = new HashMap<>();
+
+    private String issueName = StringUtils.EMPTY;
+    private String oldFile = StringUtils.EMPTY;
+    private String newFile = StringUtils.EMPTY;
+
+    public RevapiInfoExtension(@CheckForNull final String code) {
+        setCode(code);
+    }
+
+    /**
+     * Sets the name of the issue.
+     * @param name of the issue
+     */
+    public void setCode(@CheckForNull final String name) {
+        if (StringUtils.isBlank(this.issueName)) {
+            this.issueName = StringUtils.defaultString(name);
+        }
+    }
+
+    public Map<String, String> getSeverities(){
+        return this.severities;
+    }
+
+    public String getIssueName() {
+        return issueName;
+    }
+
+    public String getOldFile() {
+        return oldFile;
+    }
+
+    public String getNewFile() {
+        return newFile;
+    }
+
+    /**
+     * Sets all different severities. This can be: Source, Binary, Semantic, Others.
+     * @param severities of the issue
+     */
+    public void setSeverities(@CheckForNull final Map<String, String> severities) {
+
+        if (severities != null && !severities.isEmpty()) {
+            for (String severity : severities.keySet()) {
+                this.severities.put(severity, severities.get(severity));
+            }
+        }
+    }
+
+    /**
+     * Sets the fileName in which the new change occurred.
+     * @param newFile in which the change occurred
+     */
+    public void setNewFile(@CheckForNull final String newFile) {
+            this.newFile = StringUtils.defaultString(newFile);
+    }
+
+    /**
+     * Sets the oldFile which is affected by the issue.
+     * @param oldFile in which the change occurred
+     */
+    public void setOldFile(@CheckForNull final String oldFile) {
+            this.oldFile = StringUtils.defaultString(oldFile);
+    }
+
+}
--- src/main/java/edu/hm/hafner/analysis/parser/RevApiParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/RevApiParser.java
@@ -2,7 +2,10 @@ package edu.hm.hafner.analysis.parser;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 
 
@@ -11,6 +14,7 @@ import org.json.JSONObject;
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueBuilder;
 import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.RevapiInfoExtension;
 import edu.hm.hafner.analysis.Severity;
 
 /**
@@ -34,12 +38,35 @@ public class RevApiParser extends JsonIssueParser {
         builder.setSeverity(evaluateSeverity(jsonIssue.getJSONArray("classification")));
         builder.setDescription(getDescription(jsonIssue));
         addAttachments(jsonIssue.getJSONArray("attachments"), builder);
+        builder.setAdditionalProperties(convertToGroup(jsonIssue));
         return builder.build();
+    }
+
+
+    private RevapiInfoExtension convertToGroup(final JSONObject jsonIssue) {
+        RevapiInfoExtension group = new RevapiInfoExtension(jsonIssue.getString("name"));
+        Object newChange = jsonIssue.get("new");
+        Object oldChange = jsonIssue.get("old");
+        Map<String, String> allSeverities = new HashMap<>();
+        JSONArray severities = jsonIssue.getJSONArray("classification");
+        for (Object severity : severities) {
+            if (severity instanceof JSONObject) {
+                allSeverities.put(((JSONObject) severity).getString("compatibility"), ((JSONObject) severity).getString("severity"));
+            }
+        }
+        group.setSeverities(allSeverities);
+        if (newChange instanceof JSONObject && !((JSONObject) newChange).isNull("new")) {
+            group.setNewFile(StringUtils.defaultString(((JSONObject) newChange).getString("new")));
+        }
+        if (oldChange instanceof JSONObject && !((JSONObject) oldChange).isNull("old")) {
+            group.setOldFile(StringUtils.defaultString(((JSONObject) oldChange).getString("old")));
+        }
+        return group;
     }
 
     private void addAttachments(final JSONArray attachments, final IssueBuilder builder) {
         String packageName = attachments.getJSONObject(0).getString("value");
-        String classQualifiedName = attachments.getJSONObject(1).getString("value");
+        //String classQualifiedName = attachments.getJSONObject(1).getString("value");
         String classSimpleName = attachments.getJSONObject(2).getString("value");
         String elementKind = attachments.getJSONObject(3).getString("value");
 
@@ -69,6 +96,7 @@ public class RevApiParser extends JsonIssueParser {
                 return Severity.WARNING_LOW;
             case "BREAKING":
                 return Severity.WARNING_HIGH;
+            case "POTENTIALLY_BREAKING":
             default:
                 return Severity.WARNING_NORMAL;
         }
@@ -88,9 +116,10 @@ public class RevApiParser extends JsonIssueParser {
         }
 
         return MessageFormat.format(
-                "<p><div><b>File</b>: {0}</div><div><b>Description:</b> {1}</div><div><b>Change type:</b> {2}",
+                "<p><div><b>File</b>: {0}</div><div><b>Description:</b> {1} {2}</div><div><b>Change type:</b> {3}",
                 jsonIssue.getJSONArray("attachments").getJSONObject(1).getString("value"),
                 jsonIssue.getString("description"),
+                jsonIssue.getString("name"),
                 jsonIssue.getString("code")) + severityDescription;
 
     }
