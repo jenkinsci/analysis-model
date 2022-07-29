191f93b86b03657aa7b07ae3e4291b4d692b0125
Mulgish <juri.duval@gmail.com>
2022-07-29 23:44:28 +0100

    Adding Veracode Pipeline Scanner support.

---
+++ src/main/java/edu/hm/hafner/analysis/registry/VeracodePipelineScannerDescriptor.java
@@ -0,0 +1,34 @@
+package edu.hm.hafner.analysis.registry;
+
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.parser.VeracodePipelineScannerParser;
+
+import static j2html.TagCreator.*;
+
+/**
+ * A descriptor for Veracode Pipeline Scanner.
+ *
+ * @author Juri Duval
+ */
+class VeracodePipelineScannerDescriptor extends ParserDescriptor {
+    private static final String ID = "veracode-pipeline-scanner";
+    private static final String NAME = "Veracode Pipeline Scanner";
+
+    VeracodePipelineScannerDescriptor() {
+        super(ID, NAME);
+    }
+
+    @Override
+    public IssueParser createParser(final Option... options) {
+        return new VeracodePipelineScannerParser();
+    }
+
+    @Override
+    public String getHelp() {
+        return join(text("Use commandline"),
+                code("java -jar pipeline-scan.jar --json_output=true --json_output_file=results.json"),
+                text(", see"),
+                a("Veracode Pipeline Scanner").withHref("https://docs.veracode.com/r/c_about_pipeline_scan"),
+                text("for usage details.")).render();
+    }
+}
---
+++ src/test/java/edu/hm/hafner/analysis/parser/VeracodePipelineScannerParserTest.java
@@ -0,0 +1,78 @@
+package edu.hm.hafner.analysis.parser;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.analysis.AbstractParserTest;
+import edu.hm.hafner.analysis.Issue;
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.ParsingException;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.Severity;
+import edu.hm.hafner.analysis.assertions.SoftAssertions;
+
+import static edu.hm.hafner.analysis.assertions.Assertions.*;
+
+/**
+ * Tests the class {@link AquaScannerParser}.
+ */
+class VeracodePipelineScannerParserTest extends AbstractParserTest {
+
+    VeracodePipelineScannerParserTest() {
+        super("veracode_pipeline_scanner_result.json");
+    }
+
+    @Override
+    protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
+        softly.assertThat(report).hasSize(5);
+
+        softly.assertThat(report.get(0))
+                .hasSeverity(Severity.WARNING_HIGH)
+                .hasMessage("org.slf4j.Logger.info")
+                .hasFileName("com/sample/LoggingFilter.java")
+                .hasType("Improper Output Neutralization for Logs")
+                .hasLineStart(28);
+        softly.assertThat(report.get(1))
+                .hasSeverity(Severity.WARNING_NORMAL)
+                .hasMessage("set")
+                .hasFileName("react/dist/esm/data.js")
+                .hasType("Use of Hard-coded Password")
+                .hasLineStart(25);
+        softly.assertThat(report.get(2))
+                .hasSeverity(Severity.WARNING_LOW)
+                .hasMessage("management:endpoint:health:show-details:")
+                .hasFileName("BOOT-INF/classes/application.yml")
+                .hasType("Information Exposure Through Sent Data")
+                .hasLineStart(1);
+        softly.assertThat(report.get(3))
+                .hasSeverity(Severity.WARNING_LOW)
+                .hasMessage("nosourcefile")
+                .hasFileName("-")
+                .hasType("No source_file present")
+                .hasLineStart(0);
+        softly.assertThat(report.get(4))
+                .hasSeverity(Severity.WARNING_LOW)
+                .hasMessage("nofiles")
+                .hasFileName("-")
+                .hasType("No files present")
+                .hasLineStart(0);
+    }
+
+    @Test
+    void shouldHandleEmptyResultsJenkins67296() {
+        Report report = parse("issue67296.json");
+
+        assertThat(report).isEmpty();
+    }
+
+    @Test
+    void brokenInput() {
+        assertThatThrownBy(() -> parse("eclipse.txt")).isInstanceOf(ParsingException.class);
+    }
+
+    @Override
+    protected IssueParser createParser() {
+        return new VeracodePipelineScannerParser();
+    }
+}
