aceb1c52e6b3acecd8f02f9ae1b2316e1f334fb7
Eva Habeeb Allah <eva.allah@lilium.com>
2022-11-30 10:47:47 +0100

    Code edits based on feedback from reviewer

--- src/main/java/edu/hm/hafner/analysis/parser/PolyspaceParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/PolyspaceParser.java
@@ -1,101 +1,105 @@
 package edu.hm.hafner.analysis.parser;
 
-import java.util.Optional;
-import java.util.regex.Matcher;
+import java.io.UncheckedIOException;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
-import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueBuilder;
-import edu.hm.hafner.analysis.LookaheadParser;
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.ParsingException;
+import edu.hm.hafner.analysis.ReaderFactory;
+import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
-import edu.hm.hafner.util.LookaheadStream;
-
-import static edu.hm.hafner.util.IntegerParser.*;
+import static edu.hm.hafner.util.StringContainsUtils.*;
 
 /**
  * A parser for Polyspace Bug Finder and Code Prover results.
+ * Used for .csv files generated from Bugfinder and CodeProver tools
  *
  * @author Eva Habeeb
  */
-public class PolyspaceParser extends LookaheadParser {
+public class PolyspaceParser extends IssueParser {
 
     private static final long serialVersionUID = -1251248150596418714L;
-
-    private static final String WARNING_PATTERN = "^([0-9]+)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)"
-            + "\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)\\t([^\\t]*)"
-            + "\\t([^\\t]*)\\t([^\\t]*)(\\t([^\\t]*))?";
 
     /**
      * Creates a new instance of {@link PolyspaceParser}.
      */
     public PolyspaceParser() {
-        super(WARNING_PATTERN);
+        super();
     }
 
     @Override
-    protected Optional<Issue> createIssue(final Matcher matcher, final LookaheadStream lookahead,
-            final IssueBuilder builder) {
-        int lineNumber;
-        int colNumber;
-        String cwe = matcher.group(13);
-        if (cwe.contains("CWE")) {
-            lineNumber = parseInt(matcher.group(15));
-            colNumber = parseInt(matcher.group(16));
+    public Report parse(final ReaderFactory reader) throws ParsingException {
+        try (Stream<String> lines = reader.readStream()) {
+            return parse(lines);
         }
-        else {
-            lineNumber = parseInt(matcher.group(14));
-            colNumber = parseInt(matcher.group(15));
+        catch (UncheckedIOException e) {
+            throw new ParsingException(e);
         }
-
-        return builder.setFileName(matcher.group(9))
-                .setCategory(matcher.group(3))
-                .setDescription(matcher.group(2))
-                .setMessage("Check: " + matcher.group(6) + " " + matcher.group(7))
-                .setModuleName(matcher.group(8))
-                .setColumnStart(colNumber)
-                .setLineStart(lineNumber)
-                .setSeverity(mapPriority(matcher))
-                .buildOptional();
     }
 
-    private Severity mapPriority(final Matcher matcher) {
+    private Report parse(final Stream<String> lines) {
 
-        Severity priority = null;
+        try (IssueBuilder builder = new IssueBuilder()) {
+            int lineNumber;
+            int colNumber;
+            int limit;
+            Report report = new Report();
+            Iterator<String> lineIterator = lines.iterator();
 
-        if (equalsIgnoreCase(matcher.group(11), "Unset")) {
+            while (lineIterator.hasNext()) {
+                String line = lineIterator.next();
+                //System.out.println(line);
+                /** Checks whether "CWE" field is found, which defines the difference between
+                 a BugFinder file and a CodeProver report */
+                if (line.contains("CWE")) {
+                    // BugFinder result file has 16 columns
+                    limit = 16;
+                    lineNumber = 14;
+                    colNumber = 15;
+                }
+                else {
+                    // CodePRover file has 15 columns
+                    limit = 15;
+                    lineNumber = 13;
+                    colNumber = 14;
+                }
 
-            if (equalsIgnoreCase(matcher.group(2), "Defect")) {
-                priority = Severity.WARNING_HIGH;
+                String[] arrOfStr = line.split("\\t", limit);
+                builder.setFileName(arrOfStr[8]);
+                builder.setCategory(arrOfStr[2]);
+                builder.setDescription(arrOfStr[1]);
+                builder.setMessage("Check: " + arrOfStr[5] + " " + arrOfStr[6]);
+                builder.setModuleName(arrOfStr[7]);
+                builder.setColumnStart(arrOfStr[colNumber]);
+                builder.setLineStart(arrOfStr[lineNumber]);
+                builder.setSeverity(mapPriority(arrOfStr));
+
+                report.add(builder.build());
             }
-            else {
-                if (equalsIgnoreCase(matcher.group(4), "Red")) {
-                    priority = Severity.WARNING_HIGH;
-                }
-                else if (equalsIgnoreCase(matcher.group(4), "Orange")) {
-                    priority = Severity.WARNING_NORMAL;
-                }
-                else if (equalsIgnoreCase(matcher.group(4), "Gray")) {
-                    priority = Severity.WARNING_LOW;
-                }
-                else if (equalsIgnoreCase(matcher.group(4), "Green")) {
-                    priority = Severity.WARNING_LOW;
-                }
-                else if (equalsIgnoreCase(matcher.group(4), "Not Applicable")) {
-                    priority = Severity.WARNING_NORMAL;
-                }
-            }
-
+            return report;
         }
-        else if (equalsIgnoreCase(matcher.group(11), "High")) {
+    }
+
+    private Severity mapPriority(final String[] arrOfStr) {
+
+        Severity priority = Severity.WARNING_NORMAL;
+
+        if ((equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[1], "Defect"))
+                || (equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Red"))
+                || (equalsIgnoreCase(arrOfStr[10], "High"))) {
             priority = Severity.WARNING_HIGH;
         }
-        else if (equalsIgnoreCase(matcher.group(11), "Medium")) {
+        else if ((equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Orange"))
+                || (equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Not Applicable"))
+                || (equalsIgnoreCase(arrOfStr[10], "Medium"))) {
             priority = Severity.WARNING_NORMAL;
         }
-        else if (equalsIgnoreCase(matcher.group(11), "Low")) {
+        else if ((equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Gray"))
+                || (equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Green"))
+                || (equalsIgnoreCase(arrOfStr[10], "Low"))) {
             priority = Severity.WARNING_LOW;
-        }
-        else {
-            priority = Severity.WARNING_NORMAL;
         }
         return priority;
     }
--- src/main/java/edu/hm/hafner/analysis/registry/PolyspaceDescriptor.java
+++ src/main/java/edu/hm/hafner/analysis/registry/PolyspaceDescriptor.java
@@ -21,4 +21,19 @@ public class PolyspaceDescriptor extends ParserDescriptor {
     public IssueParser createParser(final Option... options) {
         return new PolyspaceParser();
     }
+
+    @Override
+    public String getUrl() {
+        return "https://www.mathworks.com/products/polyspace.html";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Reads reports of Polyspace Static Analysis Tool by MathWorks. "
+                + "Used for BugFinder and CodeProver result files.<br/>"
+                + "Bugfinder: <a href='https://www.mathworks.com/help/bugfinder/ref/polyspaceresultsexport.html?s_tid=srchtitle_polyspace-results-export_1'> </a>"
+                + "CodeProver: <a href='https://www.mathworks.com/help/codeprover/ref/polyspaceresultsexport.html?s_tid=srchtitle_polyspace-results-export_2'> </a>"
+                + "Report can be generated with command: "
+                + "polyspace-results-export -format csv -results-dir <RESULTS> -output-name <CSVFILE> -key <KEY>";
+    }
 }
