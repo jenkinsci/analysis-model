ae7ab9a5116251f5196df220da61dc80f03931aa
Eva Habeeb Allah <eva.allah@lilium.com>
2022-12-01 11:29:16 +0100

    Code edits based on feedback from reviewer - 2

--- src/main/java/edu/hm/hafner/analysis/parser/PolyspaceParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/PolyspaceParser.java
@@ -10,7 +10,7 @@ import edu.hm.hafner.analysis.ParsingException;
 import edu.hm.hafner.analysis.ReaderFactory;
 import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
-import static edu.hm.hafner.util.StringContainsUtils.*;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /**
  * A parser for Polyspace Bug Finder and Code Prover results.
@@ -21,6 +21,9 @@ import static edu.hm.hafner.util.StringContainsUtils.*;
 public class PolyspaceParser extends IssueParser {
 
     private static final long serialVersionUID = -1251248150596418714L;
+    private static final int severityIndex = 10;
+    private static final int colorIndex = 3;
+    private static final int familyIndex = 1;
 
     /**
      * Creates a new instance of {@link PolyspaceParser}.
@@ -64,15 +67,15 @@ public class PolyspaceParser extends IssueParser {
                     colNumber = 14;
                 }
 
-                String[] arrOfStr = line.split("\\t", limit);
-                builder.setFileName(arrOfStr[8]);
-                builder.setCategory(arrOfStr[2]);
-                builder.setDescription(arrOfStr[1]);
-                builder.setMessage("Check: " + arrOfStr[5] + " " + arrOfStr[6]);
-                builder.setModuleName(arrOfStr[7]);
-                builder.setColumnStart(arrOfStr[colNumber]);
-                builder.setLineStart(arrOfStr[lineNumber]);
-                builder.setSeverity(mapPriority(arrOfStr));
+                String[] attributes = line.split("\\t", limit);
+                builder.setFileName(attributes[8]);
+                builder.setCategory(attributes[2]);
+                builder.setDescription(attributes[1]);
+                builder.setMessage("Check: " + attributes[5] + " " + attributes[6]);
+                builder.setModuleName(attributes[7]);
+                builder.setColumnStart(attributes[colNumber]);
+                builder.setLineStart(attributes[lineNumber]);
+                builder.setSeverity(mapPriority(attributes));
 
                 report.add(builder.build());
             }
@@ -80,23 +83,33 @@ public class PolyspaceParser extends IssueParser {
         }
     }
 
-    private Severity mapPriority(final String[] arrOfStr) {
+    @SuppressWarnings(value = "PMD.UseVarargs")
+    private Severity mapPriority(final String[] attributes) {
 
         Severity priority = Severity.WARNING_NORMAL;
 
-        if (equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[1], "Defect")
-                || equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Red")
-                || equalsIgnoreCase(arrOfStr[10], "High")) {
+        if (equalsIgnoreCase(attributes[severityIndex], "Unset")) {
+
+            if (equalsIgnoreCase(attributes[familyIndex], "Defect") ||
+                    equalsIgnoreCase(attributes[colorIndex], "Red")) {
+                priority = Severity.WARNING_HIGH;
+            }
+            else if (equalsIgnoreCase(attributes[colorIndex], "Orange") ||
+                    equalsIgnoreCase(attributes[colorIndex], "Not Applicable")) {
+                priority = Severity.WARNING_NORMAL;
+            }
+            else if (equalsIgnoreCase(attributes[colorIndex], "Gray") ||
+                    equalsIgnoreCase(attributes[colorIndex], "Green")) {
+                priority = Severity.WARNING_LOW;
+            }
+        }
+        else if (equalsIgnoreCase(attributes[severityIndex], "High")) {
             priority = Severity.WARNING_HIGH;
         }
-        else if (equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Orange")
-                || equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Not Applicable")
-                || (equalsIgnoreCase(arrOfStr[10], "Medium"))) {
+        else if (equalsIgnoreCase(attributes[severityIndex], "Medium")) {
             priority = Severity.WARNING_NORMAL;
         }
-        else if (equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Gray")
-                || equalsIgnoreCase(arrOfStr[10], "Unset") && containsAnyIgnoreCase(arrOfStr[3], "Green")
-                || equalsIgnoreCase(arrOfStr[10], "Low")) {
+        else if (equalsIgnoreCase(attributes[severityIndex], "Low")) {
             priority = Severity.WARNING_LOW;
         }
         return priority;
