55bd527a43f9c0bdcea279890afb4c206209915f
Ulli Hafner <ullrich.hafner@gmail.com>
2022-12-07 22:06:54 +0100

    Simplify code.

--- src/main/java/edu/hm/hafner/analysis/parser/PolyspaceParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/PolyspaceParser.java
@@ -10,6 +10,8 @@ import edu.hm.hafner.analysis.ReaderFactory;
 import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
 
+import static org.apache.commons.lang3.StringUtils.*;
+
 /**
  * A parser for Polyspace Bug Finder and Code Prover results.
  * Used for .csv files generated from Bugfinder and CodeProver tools
@@ -17,8 +19,8 @@ import edu.hm.hafner.analysis.Severity;
  * @author Eva Habeeb
  */
 public class PolyspaceParser extends IssueParser {
-
     private static final long serialVersionUID = -1251248150596418714L;
+
     static final int SEVERITY_INDEX = 10;
     static final int COLOR_INDEX = 3;
     static final int FAMILY_INDEX = 1;
@@ -35,7 +37,6 @@ public class PolyspaceParser extends IssueParser {
     }
 
     private Report parse(final Stream<String> lines) {
-
         try (IssueBuilder builder = new IssueBuilder()) {
             int lineNumber;
             int colNumber;
@@ -61,8 +62,7 @@ public class PolyspaceParser extends IssueParser {
                 }
 
                 String[] attributes = line.split("\\t", limit);
-                if (equalsIgnoreCase(attributes[9], "Unreviewed") || equalsIgnoreCase(attributes[9], "To investigate")
-                        || equalsIgnoreCase(attributes[9], "To fix") || equalsIgnoreCase(attributes[9], "Other")) {
+                if (containsAnyIgnoreCase(attributes[9], "Unreviewed", "To investigate", "To fix", "Other")) {
                     builder.setFileName(attributes[8]);
                     builder.setCategory(attributes[2]);
                     builder.setDescription(attributes[1]);
@@ -82,33 +82,27 @@ public class PolyspaceParser extends IssueParser {
 
     @SuppressWarnings({"PMD.UseVarargs", "PMD.CyclomaticComplexity" })
     private Severity mapPriority(final String[] attributes) {
-
-        Severity priority = Severity.WARNING_NORMAL;
-
         if (equalsIgnoreCase(attributes[SEVERITY_INDEX], "Unset")) {
-
             if (equalsIgnoreCase(attributes[FAMILY_INDEX], "Defect")
                     || equalsIgnoreCase(attributes[COLOR_INDEX], "Red")) {
-                priority = Severity.WARNING_HIGH;
+                return Severity.WARNING_HIGH;
             }
-            else if (equalsIgnoreCase(attributes[COLOR_INDEX], "Orange")
-                    || equalsIgnoreCase(attributes[COLOR_INDEX], "Not Applicable")) {
-                priority = Severity.WARNING_NORMAL;
+            else if (containsAnyIgnoreCase(attributes[COLOR_INDEX], "Orange", "Not Applicable")) {
+                return Severity.WARNING_NORMAL;
             }
-            else if (equalsIgnoreCase(attributes[COLOR_INDEX], "Gray")
-                    || equalsIgnoreCase(attributes[COLOR_INDEX], "Green")) {
-                priority = Severity.WARNING_LOW;
+            else if (containsAnyIgnoreCase(attributes[COLOR_INDEX], "Gray", "Green")) {
+                return Severity.WARNING_LOW;
             }
         }
         else if (equalsIgnoreCase(attributes[SEVERITY_INDEX], "High")) {
-            priority = Severity.WARNING_HIGH;
+            return Severity.WARNING_HIGH;
         }
         else if (equalsIgnoreCase(attributes[SEVERITY_INDEX], "Medium")) {
-            priority = Severity.WARNING_NORMAL;
+            return Severity.WARNING_NORMAL;
         }
         else if (equalsIgnoreCase(attributes[SEVERITY_INDEX], "Low")) {
-            priority = Severity.WARNING_LOW;
+            return Severity.WARNING_LOW;
         }
-        return priority;
+        return Severity.WARNING_NORMAL;
     }
 }
