49f69c718f1206246dd37d3ea44fb9aaaa8eb4fe
Eva Habeeb Allah <eva.allah@lilium.com>
2022-12-12 15:19:18 +0100

    Parser logic edit, changes based on Code Covergae Check

--- src/main/java/edu/hm/hafner/analysis/parser/SimulinkCheckParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/SimulinkCheckParser.java
@@ -2,7 +2,6 @@ package edu.hm.hafner.analysis.parser;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,18 +19,17 @@ import edu.hm.hafner.analysis.Severity;
  *
  * @author Eva Habeeb
  */
-public class SimulinkCheckParser extends IssuePropertiesParser {
+public class SimulinkCheckParser extends IssuePropertiesParser{
 
     private static final long serialVersionUID = -8099258658775128275L;
 
     @Override
-    @SuppressWarnings({"CheckStyle.JavaNCSSCheck", "PMD.NcssCount", "PMD.CyclomaticComplexity", "PMD.CognitiveComplexity", "PMD.NPathComplexity" })
-    public Report parse(final ReaderFactory readerFactory) throws ParsingException {
+    @SuppressWarnings({"PMD.NcssCount", "PMD.CyclomaticComplexity", "PMD.CognitiveComplexity", "PMD.NPathComplexity" })
+    public Report parse(ReaderFactory readerFactory) throws ParsingException {
         String name = readerFactory.getFileName();
         File file = new File(name);
 
         try (IssueBuilder issueBuilder = new IssueBuilder()) {
-
             //Parse html document using Jsoup
             Document document = Jsoup.parse(file, "utf-8");
             Elements systemElement = document.select("div.ReportContent");
@@ -39,124 +37,79 @@ public class SimulinkCheckParser extends IssuePropertiesParser {
             Report report = new Report();
 
             //Select Warning items from the whole html document
-            Elements warningsElements = document.select("div.WarningCheck");
-            for (Element w : warningsElements) {
-                Elements heading = w.select("span.CheckHeading");
-                String warningTxt = heading.text();
-                issueBuilder.setSeverity(Severity.WARNING_NORMAL);
-                issueBuilder.setCategory("Warning");
-
-                if (warningTxt.contains("SW")) {
-                    String[] fileName = warningTxt.split(":", 2);
-                    issueBuilder.setModuleName(fileName[0]);
-                    issueBuilder.setDescription(fileName[1]);
-                }
-                else if (w.parent().id().contains("SW")) {
-                    String parentTxt = w.parent().id();
-                    int i = parentTxt.indexOf("SW");
-                    parentTxt = parentTxt.substring(i);
-
-                    issueBuilder.setModuleName(parentTxt);
-                    issueBuilder.setDescription(warningTxt);
-                }
-                else {
-                    issueBuilder.setDescription(warningTxt);
-                    issueBuilder.setAdditionalProperties(w.parent().id());
-                }
-                issueBuilder.setFileName(system);
-                report.add(issueBuilder.build());
-            }
-
+            parseIssue(report, document, issueBuilder, system, "div.WarningCheck");
             //Select Failed items from the whole html document
-            Elements failedElements = document.select("div.FailedCheck");
-            for (Element f : failedElements) {
-                Elements heading = f.select("span.CheckHeading");
-                String failedTxt = heading.text();
-                issueBuilder.setSeverity(Severity.ERROR);
-                issueBuilder.setCategory("Failed");
-
-                if (failedTxt.contains("SW")) {
-                    String[] fileName = failedTxt.split(":", 2);
-                    issueBuilder.setModuleName(fileName[0]);
-                    issueBuilder.setDescription(fileName[1]);
-                }
-                else if (f.parent().id().contains("SW")) {
-                    String parentTxt = f.parent().id();
-                    int i = parentTxt.indexOf("SW");
-                    parentTxt = parentTxt.substring(i);
-
-                    issueBuilder.setModuleName(parentTxt);
-                    issueBuilder.setDescription(failedTxt);
-                }
-                else {
-                    issueBuilder.setDescription(failedTxt);
-                    issueBuilder.setAdditionalProperties(f.parent().id());
-                }
-                issueBuilder.setFileName(system);
-                report.add(issueBuilder.build());
-            }
-
+            parseIssue(report, document, issueBuilder, system, "div.FailedCheck");
             //Select Not Run items from the whole html document
-            Elements notRunElements = document.select("div.NotRunCheck");
-            for (Element n : notRunElements) {
-                Elements heading = n.select("span.CheckHeading");
-                String notRunTxt = heading.text();
-                issueBuilder.setSeverity(Severity.WARNING_HIGH);
-                issueBuilder.setCategory("Not Run");
-
-                if (notRunTxt.contains("SW")) {
-                    String[] fileName = notRunTxt.split(":", 2);
-                    issueBuilder.setModuleName(fileName[0]);
-                    issueBuilder.setDescription(fileName[1]);
-                }
-                else if (n.parent().id().contains("SW")) {
-                    String parentTxt = n.parent().id();
-                    int i = parentTxt.indexOf("SW");
-                    parentTxt = parentTxt.substring(i);
-
-                    issueBuilder.setModuleName(parentTxt);
-                    issueBuilder.setDescription(notRunTxt);
-                }
-                else {
-                    issueBuilder.setDescription(notRunTxt);
-                    issueBuilder.setAdditionalProperties(n.parent().id());
-                }
-                issueBuilder.setFileName(system);
-                report.add(issueBuilder.build());
-            }
-
+            parseIssue(report, document, issueBuilder, system, "div.NotRunCheck");
             //Select Incomplete items from the whole html document
-            Elements incompleteElements = document.select("div.IncompleteCheck");
-            for (Element p : incompleteElements) {
-                Elements heading = p.select("span.CheckHeading");
-                String incompleteTxt = heading.text();
-                issueBuilder.setSeverity(Severity.WARNING_LOW);
-                issueBuilder.setCategory("Incomplete");
+            parseIssue(report, document, issueBuilder, system, "div.IncompleteCheck");
 
-                if (incompleteTxt.contains("SW")) {
-                    String[] fileName = incompleteTxt.split(":", 2);
-                    issueBuilder.setModuleName(fileName[0]);
-                    issueBuilder.setDescription(fileName[1]);
-                }
-                else if (p.parent().id().contains("SW")) {
-                    String parentTxt = p.parent().id();
-                    int i = parentTxt.indexOf("SW");
-                    parentTxt = parentTxt.substring(i);
-
-                    issueBuilder.setModuleName(parentTxt);
-                    issueBuilder.setDescription(incompleteTxt);
-                }
-                else {
-                    issueBuilder.setDescription(incompleteTxt);
-                    issueBuilder.setAdditionalProperties(p.parent().id());
-                }
-                issueBuilder.setFileName(system);
-                report.add(issueBuilder.build());
-            }
             return report;
-        } // end try
+        }
         catch (IOException e) {
             throw new ParsingException(e);
+        }
+    }
+    public void parseIssue(Report report, Document document, IssueBuilder issueBuilder, String system, String check) {
+        Elements heading;
+        String issueTxt;
+        String headingElement;
+        String[] headingSplit;
+        int strLen;
+        String module;
+
+        Elements issueElements = document.select(check);
+        for (Element w : issueElements) {
+            heading = w.select("span.CheckHeading");
+            issueTxt = heading.text();
+            setSeverity(check, issueBuilder);
+
+            headingElement = heading.first().id();
+            headingSplit = headingElement.split("\\.");
+            strLen = headingSplit.length;
+
+            if (issueTxt.contains("SW")) {
+                String[] fileName = issueTxt.split(":", 2);
+                module = headingSplit[strLen - 1];
+                issueBuilder.setModuleName(fileName[0] + "." + module);
+                issueBuilder.setDescription(fileName[1]);
+            }
+            else if (w.parent().id().contains("SW")) {
+                String parentTxt = w.parent().id();
+                int i = parentTxt.indexOf("SW");
+                parentTxt = parentTxt.substring(i);
+                module = headingSplit[strLen - 1];
+                issueBuilder.setModuleName(parentTxt + "." + module);
+                issueBuilder.setDescription(issueTxt);
+            }
+            else {
+                module = headingSplit[strLen - 1] + "." + headingSplit[strLen - 2];
+                issueBuilder.setModuleName(module);
+                issueBuilder.setDescription(issueTxt);
+                issueBuilder.setAdditionalProperties(w.parent().id());
+            }
+            issueBuilder.setFileName(system);
+            report.add(issueBuilder.build());
+        }
+    }
+    public void setSeverity(String check, IssueBuilder issueBuilder ) {
+
+        if (check.equalsIgnoreCase("div.WarningCheck")) {
+            issueBuilder.setSeverity(Severity.WARNING_NORMAL);
+            issueBuilder.setCategory("Warning");
+        }
+        else if (check.equalsIgnoreCase("div.FailedCheck")) {
+            issueBuilder.setSeverity(Severity.ERROR);
+            issueBuilder.setCategory("Failed");
+        }
+        else if (check.equalsIgnoreCase("div.NotRunCheck")) {
+            issueBuilder.setSeverity(Severity.WARNING_HIGH);
+            issueBuilder.setCategory("Not Run");
+        }
+        else if (check.equalsIgnoreCase("div.IncompleteCheck")) {
+            issueBuilder.setSeverity(Severity.WARNING_LOW);
+            issueBuilder.setCategory("Incomplete");
         }
     }
 }
--- src/test/java/edu/hm/hafner/analysis/parser/SimulinkCheckParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/SimulinkCheckParserTest.java
@@ -7,9 +7,7 @@ import edu.hm.hafner.analysis.assertions.SoftAssertions;
 
 class SimulinkCheckParserTest extends AbstractParserTest {
 
-    SimulinkCheckParserTest()  {
-        super("Simulink.html");
-    }
+    SimulinkCheckParserTest(){ super("Simulink.html"); }
 
     @Override
     protected SimulinkCheckParser createParser() {
@@ -24,18 +22,37 @@ class SimulinkCheckParserTest extends AbstractParserTest {
                 .hasCategory("Warning")
                 .hasDescription("Define the names to avoid")
                 .hasSeverity(Severity.WARNING_NORMAL);
+        softly.assertThat(report.get(1))
+                .hasModuleName("SW01-1996.UseOfCase");
+        softly.assertThat(report.get(68))
+                .hasModuleName("SW01-125.runVisualizationChecks");
+
         softly.assertThat(report.get(500))
                 .hasCategory("Not Run")
-                .hasDescription("Check if model uses row major algorithms")
+                .hasDescription("Check model mask parameters")
                 .hasSeverity(Severity.WARNING_HIGH);
+        softly.assertThat(report.get(333))
+                .hasModuleName("SW01-181.LUTRangeCheckCode");
+        softly.assertThat(report.get(439))
+                .hasModuleName("SW01-441.runClockChecks");
+
         softly.assertThat(report.get(302))
                 .hasCategory("Failed")
                 .hasDescription("Check model for Stateflow messages")
                 .hasSeverity(Severity.ERROR);
+        softly.assertThat(report.get(305))
+                .hasModuleName("SW02-142.MaskParamInfCheck");
+        softly.assertThat(report.get(307))
+                .hasModuleName("SW02-061.EventBlockCheck");
+
         softly.assertThat(report.get(1490))
                 .hasCategory("Incomplete")
-                .hasDescription("Identify questionable code instrumentation (data I/O)")
+                .hasDescription("Identify blocks generating inefficient algorithms")
                 .hasSeverity(Severity.WARNING_LOW);
+        softly.assertThat(report.get(1491))
+                .hasModuleName("SW02-493.CodeGenSettings");
+        softly.assertThat(report.get(429))
+                .hasModuleName("SW02-430.runHDLRecipChecks");
 
     }
 }
