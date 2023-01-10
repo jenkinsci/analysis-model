009734d9a7cf78fa83ea72ef96f51e4065cdf4db
Eva Habeeb Allah <eva.allah@lilium.com>
2023-01-10 14:34:37 +0100

    Simulink Check Parser

--- src/main/java/edu/hm/hafner/analysis/parser/SimulinkCheckParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/SimulinkCheckParser.java
@@ -1,10 +1,15 @@
 package edu.hm.hafner.analysis.parser;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.apache.commons.io.input.ReaderInputStream;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Safelist;
 import org.jsoup.select.Elements;
 
 import edu.hm.hafner.analysis.IssueBuilder;
@@ -22,32 +27,48 @@ import edu.hm.hafner.analysis.Severity;
 public class SimulinkCheckParser extends IssuePropertiesParser {
 
     private static final long serialVersionUID = -8099258658775128275L;
+    static final String WARNING = "div.WarningCheck";
+    static final String FAILED = "div.FailedCheck";
+    static final String NOTRUN = "div.NotRunCheck";
+    static final String INCOMPLETE = "div.IncompleteCheck";
 
     @Override
     public Report parse(final ReaderFactory readerFactory) throws ParsingException {
-        String name = readerFactory.getFileName();
-        File file = new File(name);
+        Reader reader = readerFactory.create();
+        InputStream targetStream = new ReaderInputStream(reader, readerFactory.getCharset());
 
         try (IssueBuilder issueBuilder = new IssueBuilder()) {
+            //Create a safe-list with allowed tags and attributes to prevent XSS attacks
+            Safelist safelist = new Safelist();
+            safelist = safelist.basic().addTags("div");
+            safelist = safelist.addAttributes("div","class","id");
+            safelist = safelist.addAttributes("span","class","id");
+
             //Parse html document using Jsoup
-            Document document = Jsoup.parse(file, "utf-8");
+            Document dirtyDoc = Jsoup.parse(targetStream, String.valueOf(readerFactory.getCharset()), "");
+            //Clean the parsed HTML document
+            Document document = new Cleaner(safelist).clean(dirtyDoc);
+
             Elements systemElement = document.select("div.ReportContent");
             String system = systemElement.first().id();
             Report report = new Report();
 
             //Select Warning items from the whole html document
-            parseIssue(report, document, issueBuilder, system, "div.WarningCheck");
+            parseIssue(report, document, issueBuilder, system, WARNING);
             //Select Failed items from the whole html document
-            parseIssue(report, document, issueBuilder, system, "div.FailedCheck");
+            parseIssue(report, document, issueBuilder, system, FAILED);
             //Select Not Run items from the whole html document
-            parseIssue(report, document, issueBuilder, system, "div.NotRunCheck");
+            parseIssue(report, document, issueBuilder, system, NOTRUN);
             //Select Incomplete items from the whole html document
-            parseIssue(report, document, issueBuilder, system, "div.IncompleteCheck");
+            parseIssue(report, document, issueBuilder, system, INCOMPLETE);
 
             return report;
         }
         catch (IOException e) {
             throw new ParsingException(e);
+        }
+        catch (NullPointerException p) {
+            throw new ParsingException(p);
         }
     }
 
@@ -55,16 +76,12 @@ public class SimulinkCheckParser extends IssuePropertiesParser {
 
         Elements issueElements = document.select(check);
         for (Element w : issueElements) {
-            Elements heading;
-            heading = w.select("span.CheckHeading");
-            String issueTxt;
-            issueTxt = heading.text();
+            Elements heading = w.select("span.CheckHeading");
+            String issueTxt = heading.text();
             setSeverity(check, issueBuilder);
 
-            String headingElement;
-            headingElement = heading.first().id();
-            String[] headingSplit;
-            headingSplit = headingElement.split("\\.");
+            String headingElement = heading.first().id();
+            String[] headingSplit = headingElement.split("\\.");
             int strLen;
             strLen = headingSplit.length;
             String module;
