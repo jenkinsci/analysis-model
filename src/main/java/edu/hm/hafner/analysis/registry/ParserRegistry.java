0399419b6d775919967c8db947188534b1a92e4d
dtbaum <dim.tbaum@gmail.com>
2023-07-27 08:41:34 +0200

    Removed tabulator

--- src/main/java/edu/hm/hafner/analysis/registry/ParserRegistry.java
+++ src/main/java/edu/hm/hafner/analysis/registry/ParserRegistry.java
@@ -92,7 +92,7 @@ public class ParserRegistry {
             new GnuFortranDescriptor(),
             new GoLintDescriptor(),
             new GoVetDescriptor(),
-	    new GrypeDescriptor(),
+            new GrypeDescriptor(),
             new HadoLintDescriptor(),
             new IarCstatDescriptor(),
             new IarDescriptor(),
