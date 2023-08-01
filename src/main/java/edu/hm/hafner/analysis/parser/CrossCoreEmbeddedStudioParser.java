67b381a6ad9cfae465c5070f442e2c5d01dd101e
Arthur MINGASSON <arthur.mingasson@devialet.com>
2023-08-01 13:42:03 +0200

    Update regex to have named capturing groups

--- src/main/java/edu/hm/hafner/analysis/parser/CrossCoreEmbeddedStudioParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/CrossCoreEmbeddedStudioParser.java
@@ -24,12 +24,7 @@ public class CrossCoreEmbeddedStudioParser extends LookaheadParser {
      *  - Errors are not parsed, only warnings
      */
     private static final String CCES_WARNING_PATTERN =
-            "^\"(.+?)\", line (\\d+).*(cc\\d+).*warning:(.*)";
-
-    private static final Integer MATCHER_FILE = 1;
-    private static final Integer MATCHER_LINE = 2;
-    private static final Integer MATCHER_CATEGORY = 3;
-    private static final Integer MATCHER_MESSAGE_BEGIN = 4;
+            "^\"(?<file>.+?)\", line (?<line>\\d+).*(?<category>cc\\d+).*warning:(?<messageBegin>.*)";
 
     /**
      * Creates a new instance of {@link CrossCoreEmbeddedStudioParser}.
@@ -42,7 +37,7 @@ public class CrossCoreEmbeddedStudioParser extends LookaheadParser {
     protected Optional<Issue> createIssue(final Matcher matcher, final LookaheadStream lookahead,
             final IssueBuilder builder) {
 
-        StringBuilder message = new StringBuilder(matcher.group(MATCHER_MESSAGE_BEGIN).trim());
+        StringBuilder message = new StringBuilder(matcher.group("messageBegin").trim());
 
         // always grab the second line
         if (lookahead.hasNext()) {
@@ -50,10 +45,10 @@ public class CrossCoreEmbeddedStudioParser extends LookaheadParser {
             message.append(lookahead.next().trim());
         }
 
-        return builder.setFileName(matcher.group(MATCHER_FILE))
-                .setLineStart(matcher.group(MATCHER_LINE))
+        return builder.setFileName(matcher.group("file"))
+                .setLineStart(matcher.group("line"))
                 .setSeverity(Severity.WARNING_NORMAL)
-                .setCategory(matcher.group(MATCHER_CATEGORY))
+                .setCategory(matcher.group("category"))
                 .setMessage(message.toString())
                 .buildOptional();
     }
