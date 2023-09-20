cbffebf19be9fe404f7876ce36b220c0e4b4b8d8
Tony Ciavarella <bitrunnner@gmail.com>
2023-09-19 20:28:04 -0400

    add information to ValgrindDescriptor

--- src/main/java/edu/hm/hafner/analysis/registry/ValgrindDescriptor.java
+++ src/main/java/edu/hm/hafner/analysis/registry/ValgrindDescriptor.java
@@ -3,9 +3,10 @@ package edu.hm.hafner.analysis.registry;
 import edu.hm.hafner.analysis.IssueParser;
 import edu.hm.hafner.analysis.parser.violations.ValgrindAdapter;
 
+import static j2html.TagCreator.*;
+
 /**
  * A descriptor for Valgrind.
- * See https://valgrind.org for more information about Valgrind.
  */
 public class ValgrindDescriptor extends ParserDescriptor {
     private static final String ID = "valgrind";
@@ -22,6 +23,16 @@ public class ValgrindDescriptor extends ParserDescriptor {
 
     @Override
     public String getHelp() {
-        return "Use option --xml=yes";
+        return join(text("Use options"),
+                code("--xml=yes --xml-file=valgrind_report.xml --child-silent-after-fork=yes"),
+                text(", see the"),
+                a("Valgrind User Manual").withHref("https://valgrind.org/docs/manual/manual-core.html"),
+                text("for usage details.")).render();
     }
+
+    @Override
+    public String getUrl() { return "https://valgrind.org"; }
+
+    @Override
+    public String getIconUrl() { return "https://valgrind.org/images/valgrind-link3.png"; }
 }
