ded7287f0468a9ba56a16f2825b65f71e3dc86bd
Johan Wigert <45201794+jwigert@users.noreply.github.com>
2023-10-19 08:39:13 +0200

    restructure tests

--- src/test/java/edu/hm/hafner/analysis/parser/GrypeParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/GrypeParserTest.java
@@ -5,6 +5,7 @@ import edu.hm.hafner.analysis.IssueParser;
 import edu.hm.hafner.analysis.Report;
 import edu.hm.hafner.analysis.Severity;
 import edu.hm.hafner.analysis.assertions.SoftAssertions;
+import org.junit.jupiter.api.Test;
 import static j2html.TagCreator.a;
 import static j2html.TagCreator.p;
 
@@ -38,6 +39,35 @@ class GrypeParserTest extends AbstractParserTest {
                 .hasDescription(p().with(a()
                         .withHref("https://nvd.nist.gov/vuln/detail/CVE-2016-8745")
                         .withText("https://nvd.nist.gov/vuln/detail/CVE-2016-8745")).render());
+    }
+
+    @Test
+    void assertThatVulnerabilityWithoutDescriptionCanBeParsed() {
+        var fileWithIssues = "grype-report-wo-description.json";
+        var report = super.parse(fileWithIssues);
+
+        try (var softly = new SoftAssertions()) {
+            softly.assertThat(report).hasSize(20).hasDuplicatesSize(13);
+            softly.assertThat(report.get(0))
+                    .hasFileName("/usr/local/bin/environment-to-ini")
+                    .hasSeverity(Severity.ERROR)
+                    .hasCategory("Critical")
+                    .hasType("GHSA-pg38-r834-g45j")
+                    .hasMessage("Improper Privilege Management in Gitea")
+                    .hasDescription(p().with(a()
+                            .withHref("https://github.com/advisories/GHSA-pg38-r834-g45j")
+                            .withText("https://github.com/advisories/GHSA-pg38-r834-g45j")).render());
+
+            softly.assertThat(report.get(13))
+                    .hasFileName("/lib/apk/db/installed")
+                    .hasSeverity(Severity.WARNING_HIGH)
+                    .hasCategory("High")
+                    .hasType("CVE-2023-38039")
+                    .hasMessage("Unknown")
+                    .hasDescription(p().with(a()
+                            .withHref("http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2023-38039")
+                            .withText("http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2023-38039")).render());
+        }
     }
 
     @Override
