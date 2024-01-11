d973fd9131cfc0e8fcc84895014ad7be6963dea0
Safa Yilmaz <yilmaz.safa17@gmail.com>
2024-01-11 16:15:11 +0100

    changes for test cases
    changes for test cases

--- src/test/java/edu/hm/hafner/analysis/parser/EmbeddedEngineerParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/EmbeddedEngineerParserTest.java
@@ -49,6 +49,31 @@ class EmbeddedEngineerParserTest extends AbstractParserTest {
                 .hasCategory("Out parameters")
                 .hasDescription("Out parameters 'Model_ptr_2345') are not supported. Please use 'return' or 'inout' parameters; {98CF1FE6-EC9C-43f1-e476-40EFCD63cA8D}")
                 .hasSeverity(Severity.WARNING_NORMAL);
+        softly.assertThat(report.get(9))
+                .hasCategory("Error loading plugins from")
+                .hasDescription("Error loading plugins from C:\\file1\\idc\\sample_ext.x64.dll File name: "
+                + "'file:///C:\\AA\\BB\\System.Text.Json.dll' ---> System.NotSupportedException: "
+                + "An attempt was made to load an assembly from a network location which would have caused the "
+                + "assembly to be sandboxed in previous versions of the .NET Framework. "
+                + "This release of the .NET Framework does not enable CAS policy by default, "
+                + "so this load may be dangerous. If this load is not intended to sandbox the assembly,"
+                + " please enable the loadFromRemoteSources switch. "
+                + "See http://go.microsoft.com/fwlink/?LinkId=155569 for more information.   "
+                + " at System.Reflection.RuntimeAssembly._nLoad(AssemblyName fileName, String codeBase,"
+                + " Evidence assemblySecurity, RuntimeAssembly locationHint, StackCrawlMark& stackMark, "
+                + "IntPtr pPrivHostBinder, Boolean throwOnFileNotFound, Boolean forIntrospection, "
+                + "Boolean suppressSecurityChecks)    at System.Reflection.RuntimeAssembly."
+                + "InternalLoadAssemblyName(AssemblyName assemblyRef, Evidence assemblySecurity, "
+                + "RuntimeAssembly reqAssembly, StackCrawlMark& stackMark, IntPtr pPrivHostBinder, "
+                + "Boolean throwOnFileNotFound, Boolean forIntrospection, Boolean suppressSecurityChecks)    "
+                + "at System.Reflection.RuntimeAssembly.InternalLoadFrom(String assemblyFile, "
+                + "Evidence securityEvidence, Byte[] hashValue, AssemblyHashAlgorithm hashAlgorithm, "
+                + "Boolean forIntrospection, Boolean suppressSecurityChecks, StackCrawlMark& stackMark)   "
+                + " at System.Reflection.Assembly.LoadFrom(String assemblyFile)    "
+                + "at A.A`1.a(String , Predicate`1 , Object[] )    "
+                + "at A.A`1.A(String , Predicate`1 , Object[] )")
+                .hasSeverity(Severity.ERROR);
+
     }
 }
 
