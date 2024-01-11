a5bc9aac1b1e8103612fcc6cd3cd738d9ae05163
Ulli Hafner <ullrich.hafner@gmail.com>
2024-01-11 18:29:46 +0100

    Make assertion independent of order.

--- src/test/java/edu/hm/hafner/analysis/parser/violations/AnsibleLaterAdapterTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/violations/AnsibleLaterAdapterTest.java
@@ -5,6 +5,8 @@ import edu.hm.hafner.analysis.Severity;
 import edu.hm.hafner.analysis.assertions.SoftAssertions;
 import edu.hm.hafner.analysis.registry.AbstractParserTest;
 
+import static edu.hm.hafner.analysis.assertions.Assertions.*;
+
 /**
  * Tests the class {@link CodeNarcAdapter}.
  *
@@ -18,18 +20,20 @@ class AnsibleLaterAdapterTest extends AbstractParserTest {
     @Override
     protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
         softly.assertThat(report).hasSize(2);
-        softly.assertThat(report.get(0))
-                .hasMessage("[ANSIBLE0004] Standard 'YAML should use consistent number of spaces around variables' not met: simple_role.yml:7: no suitable numbers of spaces (min: 1 max: 1)")
-                .hasFileName("simple_role.yml")
-                .hasType("ANSIBLE0004")
-                .hasLineStart(7)
-                .hasSeverity(Severity.WARNING_HIGH);
-        softly.assertThat(report.get(1))
-                .hasMessage("[ANSIBLE9998] Best practice 'Standards version should be pinned' not met: simple_role.yml: Standards version not set. Using latest standards version 0.2")
-                .hasFileName("simple_role.yml")
-                .hasType("ANSIBLE9998")
-                .hasLineStart(0)
-                .hasSeverity(Severity.WARNING_NORMAL);
+        softly.assertThat(report.get())
+                .anySatisfy(i -> assertThat(i)
+                        .hasMessage(
+                                "[ANSIBLE0004] Standard 'YAML should use consistent number of spaces around variables' not met: simple_role.yml:7: no suitable numbers of spaces (min: 1 max: 1)")
+                        .hasFileName("simple_role.yml")
+                        .hasType("ANSIBLE0004")
+                        .hasLineStart(7)
+                        .hasSeverity(Severity.WARNING_HIGH))
+                .anySatisfy(i -> assertThat(i)
+                        .hasMessage("[ANSIBLE9998] Best practice 'Standards version should be pinned' not met: simple_role.yml: Standards version not set. Using latest standards version 0.2")
+                        .hasFileName("simple_role.yml")
+                        .hasType("ANSIBLE9998")
+                        .hasLineStart(0)
+                        .hasSeverity(Severity.WARNING_NORMAL));
     }
 
     @Override
