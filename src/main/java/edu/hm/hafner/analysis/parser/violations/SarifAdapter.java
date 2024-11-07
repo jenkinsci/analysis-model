8dfcf3c9f817de56263c0f5313833759e2d46b80
Ulli Hafner <ullrich.hafner@gmail.com>
2024-11-07 11:08:22 +0100

    Fix handling of Windows paths on unix.

--- src/main/java/edu/hm/hafner/analysis/parser/violations/SarifAdapter.java
+++ src/main/java/edu/hm/hafner/analysis/parser/violations/SarifAdapter.java
@@ -1,13 +1,13 @@
 package edu.hm.hafner.analysis.parser.violations;
 
-import edu.hm.hafner.analysis.IssueBuilder;
-import se.bjurr.violations.lib.model.Violation;
-import se.bjurr.violations.lib.parsers.SarifParser;
-
-import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.InvalidPathException;
+import java.util.regex.Pattern;
+
+import edu.hm.hafner.analysis.IssueBuilder;
+
+import se.bjurr.violations.lib.model.Violation;
+import se.bjurr.violations.lib.parsers.SarifParser;
 
 /**
  * Parses SARIF files.
@@ -17,28 +17,28 @@ import java.nio.file.InvalidPathException;
 public class SarifAdapter extends AbstractViolationAdapter {
     private static final long serialVersionUID = -5699747899173867285L;
 
+    private static final Pattern WINDOWS_PATH_ON_UNIX = Pattern.compile("^/[a-zA-Z]:.*");
+
     @Override
     SarifParser createParser() {
         return new SarifParser();
     }
 
     @Override
-    void updateIssueBuilder(final Violation violation, final IssueBuilder builder) {
-        super.updateIssueBuilder(violation, builder);
-        builder.setFileName(convertFileUriSchemeToPath(violation.getFile()));
+    void extractAdditionalProperties(final IssueBuilder builder, final Violation violation) {
+        try {
+            var fileName = violation.getFile();
+            var uri = new URI(fileName);
+            builder.setFileName(removePrefix(uri.getPath()));
+        }
+        catch (URISyntaxException exception) {
+            // ignore
+        }
     }
 
-    String convertFileUriSchemeToPath(final String fileName) {
-        if (!fileName.startsWith("file:")) {
-            return fileName;
-        }
-
-        try {
-            File file = new File(new URI(fileName));
-            return file.toPath().toString();
-        } 
-        catch (URISyntaxException | InvalidPathException ignored) {
-            // ignore
+    private String removePrefix(final String fileName) {
+        if (WINDOWS_PATH_ON_UNIX.matcher(fileName).matches()) {
+            return fileName.substring(1);
         }
         return fileName;
     }
