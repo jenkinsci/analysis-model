6f246899a1ee2fbc12950ea4b270adeab1d801dd
Ulli Hafner <ullrich.hafner@gmail.com>
2025-03-24 08:35:14 +0100

    Remove helper class `MsBuildPattern`

--- src/main/java/edu/hm/hafner/analysis/parser/MsBuildParser.java
+++ src/main/java/edu/hm/hafner/analysis/parser/MsBuildParser.java
@@ -1,19 +1,18 @@
 package edu.hm.hafner.analysis.parser;
 
-import java.io.Serial;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import edu.hm.hafner.analysis.MsBuildPattern;
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueBuilder;
 import edu.hm.hafner.analysis.LookaheadParser;
 import edu.hm.hafner.analysis.Severity;
 import edu.hm.hafner.util.LookaheadStream;
+
+import java.io.Serial;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A parser for the MSBuild/PcLint compiler warnings.
@@ -24,17 +23,55 @@ public class MsBuildParser extends LookaheadParser {
     @Serial
     private static final long serialVersionUID = -2141974437420906595L;
 
+    /** Pattern for command line warnings in MSBuild output. */
+    public static final String COMMAND_LINE_WARNING_PATTERN =
+            "(?:^(?:.*)Command line warning ([A-Za-z0-9]+):\\s*(.*)\\s*\\[(.*)\\])"; // Group 1 - 3
+
+    /** Pattern for optional line number in MSBuild output. */
+    public static final String OPTIONAL_LINE_NUMBER_PATTERN =
+            "(?:\\s*(?:\\d+|\\d+:\\d+)>)?"; // Optional Group
+
+    /** Pattern for different line column combinations in MSBuild output. */
+    private static final String LINE_COLUMN_PATTERN =
+            "\\((?:(\\d+),(\\d+),(\\d+),(\\d+)|(\\d+)(?:-(\\d+))?(?:,(\\d+)(?:-(\\d+))?)?)\\)"; // Group 5 - 12
+
+    /** Pattern for file name in MSBuild output. */
+    public static final String FILE_NAME_PATTERN =
+            "(?:(?:(?:(.*?)" // Group 4
+                    + LINE_COLUMN_PATTERN
+                    + "|.*LINK)\\s*:|(.*):)"; // Group 13
+
+    /** Pattern for severity type in MSBuild output. */
+    public static final String SEVERITY_PATTERN =
+            "\\s*([A-z-_]*\\s(?:[Nn]ote|[Ii]nfo|[Ww]arning|(?:fatal\\s*)?[Ee]rror))[^A-Za-z0-9]\\s*:?\\s*"; // Group 14
+
+    /** Pattern for category of the issue in MSBuild output. */
+    public static final String CATEGORY_PATTERN =
+            "([A-Za-z0-9\\-_]+)?\\s*:\\s"; // Group 15
+
+    /** Pattern for type of the issue in MSBuild output. */
+    public static final String TYPE_PATTERN =
+            "(?:\\s*([A-Za-z0-9.]+)\\s*:)?\\s*"; // Group 16
+
+    /** Pattern for message in MSBuild output. */
+    public static final String MESSAGE_PATTERN =
+            "(.*?)"; // Group 17
+
+    /** Pattern for project directory in MSBuild output. */
+    public static final String PROJECT_DIR_PATTERN =
+            "(?: \\[([^\\]]*)[/\\\\][^\\]\\\\]+\\])?"; // Group 18
+
     private static final String MS_BUILD_WARNING_PATTERN
-            = MsBuildPattern.COMMAND_LINE_WARNING_PATTERN
+            = COMMAND_LINE_WARNING_PATTERN
             + "|"
             + ANT_TASK + "(?:"
-            + MsBuildPattern.OPTIONAL_LINE_NUMBER_PATTERN
-            + MsBuildPattern.FILE_NAME_PATTERN
-            + MsBuildPattern.SEVERITY_PATTERN
-            + MsBuildPattern.CATEGORY_PATTERN
-            + MsBuildPattern.TYPE_PATTERN
-            + MsBuildPattern.MESSAGE_PATTERN
-            + MsBuildPattern.PROJECT_DIR_PATTERN
+            + OPTIONAL_LINE_NUMBER_PATTERN
+            + FILE_NAME_PATTERN
+            + SEVERITY_PATTERN
+            + CATEGORY_PATTERN
+            + TYPE_PATTERN
+            + MESSAGE_PATTERN
+            + PROJECT_DIR_PATTERN
             + "))$";
 
     private static final Pattern IGNORED_TOOLS_PATTERN = Pattern.compile("(?!.exe)(\\.[^.]+)$");
