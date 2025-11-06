9b531c323dea0066ea35cbbc2661c20f8245ea90
Akash Manna <akash.manna.mymail@gmail.com>
2025-11-07 02:36:37 +0530

    [JENKINS-75743] Add help text for Gcc and Gcc4 parsers
    Fixes https://issues.jenkins.io/browse/JENKINS-75743

--- src/main/java/edu/hm/hafner/analysis/registry/GccDescriptor.java
+++ src/main/java/edu/hm/hafner/analysis/registry/GccDescriptor.java
@@ -4,13 +4,18 @@ import edu.hm.hafner.analysis.IssueParser;
 import edu.hm.hafner.analysis.parser.GccParser;
 
 /**
- * A descriptor for GNU C Compiler 3 (gcc).
+ * A descriptor for legacy GNU C Compiler (gcc) versions older than GCC 4.
+ *
+ * <p>
+ * This parser is designed for very old GCC compilers (pre-GCC 4) that use a different output format. 
+ * For modern GCC versions (4 and newer, including GCC 5–15), use the "gcc" parser instead.
+ * </p>
  *
  * @author Lorenz Munsch
  */
 class GccDescriptor extends ParserDescriptor {
     private static final String ID = "gcc3";
-    private static final String NAME = "GNU C Compiler 3 (gcc)";
+    private static final String NAME = "GNU C Compiler (Legacy, pre-GCC 4)";
 
     GccDescriptor() {
         super(ID, NAME);
@@ -19,5 +24,14 @@ class GccDescriptor extends ParserDescriptor {
     @Override
     public IssueParser create(final Option... options) {
         return new GccParser();
+    }
+
+    @Override
+    public String getHelp() {
+        return "<p>Parses warnings and errors from legacy GCC compilers (versions older than GCC 4). "
+                + "This parser uses an older, simpler warning format.</p>"
+                + "<p>For modern GCC versions (GCC 4 and newer, including GCC 5–15), use the 'gcc' parser instead, "
+                + "which supports the newer format with additional context like:</p>"
+                + "<p><code>file.c:10:5: warning: unused variable 'x' [-Wunused-variable]</code></p>";
     }
 }
