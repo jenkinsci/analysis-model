70e90a542963affcd81b05e99d70175da220ff28
Akash Manna <akash.manna.mymail@gmail.com>
2025-11-09 20:34:45 +0530

    [JENKINS-64614] clang-tidy tool reports warnings generated by gcc

--- src/test/java/edu/hm/hafner/analysis/parser/ClangTidyParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/ClangTidyParserTest.java
@@ -170,4 +170,65 @@ class ClangTidyParserTest extends AbstractParserTest {
                 .hasCategory("google-explicit-constructor")
                 .hasSeverity(Severity.WARNING_NORMAL);
     }
+
+    /**
+     * Verifies that clang-tidy parser filters out GCC warnings when processing cmake build output.
+     * When running clang-tidy via cmake with CMAKE_CXX_CLANG_TIDY, both clang-tidy and GCC warnings
+     * are generated in the same output. The parser should only include clang-tidy warnings with
+     * [check-name] pattern and exclude GCC warnings with [-W...] pattern.
+     *
+     * @see <a href="https://issues.jenkins.io/browse/JENKINS-64614">Issue 64614</a>
+     */
+    @Test
+    void issue64614() {
+        var report = parse("issue64614.txt");
+
+        // Should only match 5 clang-tidy warnings, filtering out 4 GCC warnings
+        assertThat(report).hasSize(5);
+
+        assertThat(report.get(0))
+                .hasLineStart(42)
+                .hasColumnStart(10)
+                .hasFileName("/home/user/project/src/main.cpp")
+                .hasMessage("implicit conversion changes signedness: 'int' to 'uint32_t' (aka 'unsigned int')")
+                .hasType(WARNING_TYPE)
+                .hasCategory("clang-diagnostic-sign-conversion")
+                .hasSeverity(Severity.WARNING_NORMAL);
+
+        assertThat(report.get(1))
+                .hasLineStart(15)
+                .hasColumnStart(5)
+                .hasFileName("/home/user/project/src/utils.cpp")
+                .hasMessage("single-argument constructors must be marked explicit to avoid unintentional implicit conversions")
+                .hasType(WARNING_TYPE)
+                .hasCategory("google-explicit-constructor")
+                .hasSeverity(Severity.WARNING_NORMAL);
+
+        assertThat(report.get(2))
+                .hasLineStart(89)
+                .hasColumnStart(20)
+                .hasFileName("/home/user/project/include/helper.h")
+                .hasMessage("suggest braces around initialization of subobject")
+                .hasType(WARNING_TYPE)
+                .hasCategory("clang-diagnostic-missing-braces")
+                .hasSeverity(Severity.WARNING_NORMAL);
+
+        assertThat(report.get(3))
+                .hasLineStart(200)
+                .hasColumnStart(30)
+                .hasFileName("/home/user/project/src/config.cpp")
+                .hasMessage("initializer for member 'data' is redundant")
+                .hasType(WARNING_TYPE)
+                .hasCategory("readability-redundant-member-init")
+                .hasSeverity(Severity.WARNING_NORMAL);
+
+        assertThat(report.get(4))
+                .hasLineStart(55)
+                .hasColumnStart(8)
+                .hasFileName("/home/user/project/src/algorithm.cpp")
+                .hasMessage("use emplace_back instead of push_back")
+                .hasType(WARNING_TYPE)
+                .hasCategory("modernize-use-emplace")
+                .hasSeverity(Severity.WARNING_NORMAL);
+    }
 }
