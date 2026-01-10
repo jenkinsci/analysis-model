2515dc8e30339ff054f81791910dd29de913aa02
Ulli Hafner <ullrich.hafner@gmail.com>
2026-01-10 16:05:13 +0100

    Remove duplicate assertion

--- src/test/java/edu/hm/hafner/analysis/parser/ScalacParserTest.java
+++ src/test/java/edu/hm/hafner/analysis/parser/ScalacParserTest.java
@@ -21,7 +21,6 @@ class ScalacParserTest extends AbstractParserTest {
     protected void assertThatIssuesArePresent(final Report report, final SoftAssertions softly) {
         softly.assertThat(report).hasSize(3);
 
-        softly.assertThat(report).hasSize(3);
         assertThatReportHasSeverities(report, 0, 1, 2, 0);
         softly.assertThat(report.get(0))
                 .hasSeverity(Severity.WARNING_NORMAL)
