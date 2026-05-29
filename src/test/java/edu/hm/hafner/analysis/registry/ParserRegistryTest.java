16d84934d626fe4a1ef435b2b28acb9afc13c85d
Ulli Hafner <ullrich.hafner@gmail.com>
2026-05-29 21:45:33 +0200

    Fix number of vulnerabilty parsers

--- src/test/java/edu/hm/hafner/analysis/registry/ParserRegistryTest.java
+++ src/test/java/edu/hm/hafner/analysis/registry/ParserRegistryTest.java
@@ -24,7 +24,7 @@ class ParserRegistryTest extends ResourceTest {
     // please check if you are using the correct type and increment the corresponding count
     private static final long WARNING_PARSERS_COUNT = 152L;
     private static final long BUG_PARSERS_COUNT = 3L;
-    private static final long VULNERABILITY_PARSERS_COUNT = 16L;
+    private static final long VULNERABILITY_PARSERS_COUNT = 17L;
     private static final long DUPLICATION_PARSERS_COUNT = 3L;
 
     private static final String SPOTBUGS = "spotbugs";
