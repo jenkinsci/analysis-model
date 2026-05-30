3a0d49a807c9fe3c201a51786244587f22670ce8
Akash Manna <akash.manna.mymail@gmail.com>
2026-05-30 21:29:40 +0530

    Parser for FOSSA issue reports in JSON format

---
+++ src/main/java/edu/hm/hafner/analysis/registry/FossaDescriptor.java
@@ -0,0 +1,45 @@
+package edu.hm.hafner.analysis.registry;
+
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.Report.IssueType;
+import edu.hm.hafner.analysis.parser.FossaParser;
+
+/**
+ * A descriptor for FOSSA issue reports.
+ *
+ * @author Akash Manna
+ */
+class FossaDescriptor extends ParserDescriptor {
+    private static final String ID = "fossa";
+    private static final String NAME = "FOSSA";
+
+    FossaDescriptor() {
+        super(ID, NAME);
+    }
+
+    @Override
+    public IssueType getType() {
+        return IssueType.VULNERABILITY;
+    }
+
+    @Override
+    public String getUrl() {
+        return "https://docs.fossa.com/";
+    }
+
+    @Override
+    public String getPattern() {
+        return "**/fossa-report.json";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Use commandline <code>fossa test --format json &gt; fossa-report.json</code> to generate JSON output.<br/>"
+                + "See <a href='https://docs.fossa.com/'>FOSSA documentation</a> for usage details.";
+    }
+
+    @Override
+    protected IssueParser create(final Option... options) {
+        return new FossaParser();
+    }
+}
