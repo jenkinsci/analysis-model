14e6320a2b2613560001dccb09c51de8556b3471
Akash Manna <akash.manna.mymail@gmail.com>
2026-06-10 12:37:35 +0530

    Add a parser for `Talisman` security scanner JSON reports (#1530)

---
+++ src/main/java/edu/hm/hafner/analysis/parser/TalismanParser.java
@@ -0,0 +1,70 @@
+package edu.hm.hafner.analysis.parser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import edu.hm.hafner.analysis.IssueBuilder;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.Severity;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+
+import java.io.Serial;
+
+/**
+ * A parser for Talisman security scanner JSON reports.
+ *
+ * @author Akash Manna
+ * @see <a href="https://github.com/thoughtworks/talisman">Talisman on GitHub</a>
+ */
+public class TalismanParser extends JsonIssueParser {
+    @Serial
+    private static final long serialVersionUID = 8469918575898239045L;
+
+    private static final String RESULTS_KEY = "results";
+    private static final String FILENAME_KEY = "filename";
+    private static final String FAILURE_LIST_KEY = "failure_list";
+    private static final String WARNING_LIST_KEY = "warning_list";
+    private static final String TYPE_KEY = "type";
+    private static final String MESSAGE_KEY = "message";
+    private static final String SEVERITY_KEY = "severity";
+
+    @Override
+    protected void parseJsonObject(final Report report, final JSONObject jsonReport, final IssueBuilder issueBuilder) {
+        if (jsonReport.has(RESULTS_KEY)) {
+            var results = jsonReport.getJSONArray(RESULTS_KEY);
+            for (int i = 0; i < results.length(); i++) {
+                var fileResult = results.getJSONObject(i);
+                parseFileResult(report, fileResult, issueBuilder);
+            }
+        }
+    }
+
+    private void parseFileResult(final Report report, final JSONObject fileResult, final IssueBuilder issueBuilder) {
+        var filename = fileResult.optString(FILENAME_KEY, "-");
+
+        parseDetailList(report, fileResult.optJSONArray(FAILURE_LIST_KEY), filename, issueBuilder, false);
+        parseDetailList(report, fileResult.optJSONArray(WARNING_LIST_KEY), filename, issueBuilder, true);
+    }
+
+    private void parseDetailList(final Report report, @CheckForNull final JSONArray detailList,
+            final String filename, final IssueBuilder issueBuilder, final boolean isWarning) {
+        if (detailList == null) {
+            return;
+        }
+
+        for (int i = 0; i < detailList.length(); i++) {
+            var detail = detailList.getJSONObject(i);
+            if (isWarning) {
+                issueBuilder.setSeverity(Severity.WARNING_LOW);
+            }
+            else {
+                issueBuilder.guessSeverity(detail.optString(SEVERITY_KEY, ""));
+            }
+            report.add(issueBuilder
+                    .setFileName(filename)
+                    .setType(detail.optString(TYPE_KEY, "-"))
+                    .setMessage(detail.optString(MESSAGE_KEY, ""))
+                    .buildAndClean());
+        }
+    }
+}
---
+++ src/main/java/edu/hm/hafner/analysis/registry/TalismanDescriptor.java
@@ -0,0 +1,51 @@
+package edu.hm.hafner.analysis.registry;
+
+import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.Report.IssueType;
+import edu.hm.hafner.analysis.parser.TalismanParser;
+
+/**
+ * A descriptor for Talisman security scanner JSON reports.
+ *
+ * @author Akash Manna
+ * @see <a href="https://github.com/thoughtworks/talisman">Talisman on GitHub</a>
+ */
+class TalismanDescriptor extends ParserDescriptor {
+    private static final String ID = "talisman";
+    private static final String NAME = "Talisman";
+
+    TalismanDescriptor() {
+        super(ID, NAME);
+    }
+
+    @Override
+    public IssueType getType() {
+        return IssueType.VULNERABILITY;
+    }
+
+    @Override
+    public IssueParser create(final Option... options) {
+        return new TalismanParser();
+    }
+
+    @Override
+    public String getPattern() {
+        return "**/talisman-report.json";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Use commandline <code>talisman --scanWithHtml</code> to generate a report that includes a JSON file.<br/>"
+                + "See <a href='https://github.com/thoughtworks/talisman'>Talisman on GitHub</a> for usage details.";
+    }
+
+    @Override
+    public String getUrl() {
+        return "https://github.com/thoughtworks/talisman";
+    }
+
+    @Override
+    public String getIconUrl() {
+        return "https://raw.githubusercontent.com/jaydeepc/talisman-html-report/master/img/talisman.png";
+    }
+}
